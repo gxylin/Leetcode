c9b06cb2df8f9e2804d5d75fb3a458efaa5be2d9
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-13 12:03:55 -0600

    Update 1 Two Sum.java

--- Java/1 Two Sum.java
+++ Java/1 Two Sum.java
@@ -11,12 +11,14 @@ return [0, 1].
 
 --------------------------------------------
 Method 1: HashMap
+Time complexity: O(n)
+Space complexity: O(n)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length == 0){
-            return null;
-        }
         int[] result = new int[2];
+        if (nums == null || nums.length == 0){
+            return result;
+        }
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++){
             if (map.containsKey(target - nums[i])){
