25b9e34601365bd69f2e214fdf0eb931ddb0e4d0
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-14 11:18:46 -0600

    Create Valid Anagram.java

---
+++ Java/Valid Anagram.java
@@ -0,0 +1,67 @@
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+Method 1:
+Conver to alphabet array with the size of 26
+Time complexity: O(n)
+Space complexity: O(n)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null){
+            return false;
+        }
+        if (s.length() == 0 && t.length() == 0){
+            return true;
+        }
+        final int MAX_CHAR = 26;
+        int[] alphabet = new int[MAX_CHAR];
+        for (int i = 0; i < s.length(); i++){
+            alphabet[s.charAt(i) - 'a']++; 
+        }
+        for (int i = 0; i < t.length(); i++){
+            alphabet[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < MAX_CHAR; i++){
+            if (alphabet[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+Method 2:
+Use sort and compare
+Time complexity: O(nlogn)
+Space complexity: O(n)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null){
+            return false;
+        }
+        if (s.length() == 0 && t.length() == 0){
+            return true;
+        }
+        char[] sArray = s.toCharArray();
+        Arrays.sort(sArray);
+        String s_new = String.valueOf(sArray);
+        
+        char[] tArray = t.toCharArray();
+        Arrays.sort(tArray);
+        String t_new = String.valueOf(tArray);
+        
+        if (s_new.equals(t_new)){
+            return true;
+        }
+        return false;
+    }
+}
