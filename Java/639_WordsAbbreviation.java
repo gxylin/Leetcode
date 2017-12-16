5905550842c4b1cb23c5694f401d303c97dec345
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-15 20:26:36 -0600

    Update 639_WordsAbbreviation.java

--- Java/639_WordsAbbreviation.java
+++ Java/639_WordsAbbreviation.java
@@ -1,9 +1,11 @@
-Given an array of n distinct non-empty strings, you need to generate minimal possible abbreviations for every word following rules below.
+Given an array of n distinct non-empty strings, you need to generate minimal possible abbreviations for every 
+word following rules below.
 
 Begin with the first character and then the number of characters abbreviated, which followed by the last character.
-If there are any conflict, that is more than one words share the same abbreviation, a longer prefix is used instead of only the first character until making the map from word to abbreviation become unique. In other words, a final abbreviation cannot map to more than one original words.
+If there are any conflict, that is more than one words share the same abbreviation, 
+a longer prefix is used instead of only the first character until making the map from word to abbreviation become unique. 
+In other words, a final abbreviation cannot map to more than one original words. 
 If the abbreviation doesn't make the word shorter, then keep it as original.
- Notice
 
 Both n and the length of each word will not exceed 400.
 The length of each word is greater than 1.
