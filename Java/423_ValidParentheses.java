72c7faf51991aeee9f65f3c8ee995d41f065d388
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-19 18:31:03 -0600

    Create 423_ValidParentheses.java

---
+++ Java/423_ValidParentheses.java
@@ -0,0 +1,30 @@
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+Have you met this question in a real interview? Yes
+Example
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+public class Solution {
+    /*
+     * @param s: A string
+     * @return: whether the string is a valid parentheses
+     */
+    public boolean isValidParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(']', '[');
+        map.put('}', '{');
+        map.put(')', '(');
+        char[] sc = s.toCharArray();
+        for (int i = 0; i < sc.length; i++){
+            if (sc[i] == '(' || sc[i] == '[' || sc[i] == '{'){
+                stack.push(sc[i]);
+            }else{
+                if (stack.isEmpty() || map.get(sc[i]) != stack.pop()){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
