9f8f7b782400ea013dab72fae786213e4d9887a1
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-19 18:11:36 -0600

    Create 76_LongestIncreasingSubsequence.java

---
+++ Java/76_LongestIncreasingSubsequence.java
@@ -0,0 +1,43 @@
+Given a sequence of integers, find the longest increasing subsequence (LIS).
+
+You code should return the length of the LIS.
+
+Have you met this question in a real interview? Yes
+Clarification
+What's the definition of longest increasing subsequence?
+
+The longest increasing subsequence problem is to find a subsequence of a given sequence in which the subsequence's elements are in sorted order, lowest to highest, and in which the subsequence is as long as possible. This subsequence is not necessarily contiguous, or unique.
+
+https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+
+Example
+For [5, 4, 1, 2, 3], the LIS is [1, 2, 3], return 3
+For [4, 2, 4, 5, 3, 7], the LIS is [2, 4, 5, 7], return 4
+
+public class Solution {
+    /*
+     * @param nums: An integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence(int[] nums) {
+        int[] f = new int[nums.length];
+        
+        for (int i = 0; i < nums.length; i++){
+            f[i] = 1;
+        }
+        
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < i; j++){
+                if (nums[j] < nums[i]){
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+        }
+        
+        int result = 0;
+        for (int i = 0; i < nums.length; i++){
+            result = Math.max(result, f[i]);
+        }
+        return result;
+    }
+}
