7626611b9ef8e242a019db43235e63d7a083595f
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-27 22:08:19 -0500

    Create 47_PermutationsII.java

---
+++ Java/47_PermutationsII.java
@@ -0,0 +1,40 @@
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0){
+            return result;
+        }
+        Arrays.sort(nums);
+        boolean[] marked = new boolean[nums.length];
+        dfs(result, new ArrayList<Integer>(), nums, marked);
+        return result;
+    }
+    private void dfs(List<List<Integer>> result, List<Integer> item, int[] nums, boolean[] marked){
+        if (item.size() == nums.length){
+            result.add(new ArrayList<Integer>(item));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++){
+            if (i != 0 && nums[i] == nums[i-1] && !marked[i-1]){
+                continue;
+            }
+            if (!marked[i]){
+                item.add(nums[i]);
+                marked[i] = true;
+                dfs(result, item, nums, marked);
+                item.remove(item.size() - 1);
+                marked[i] = false;
+            }
+        }
+    }
+}
