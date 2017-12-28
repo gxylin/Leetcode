a52b7ea8db0f800847707589be0b4d85bffaf4f7
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-28 18:52:52 -0500

    Update 94_BinaryTreeInorderTraversal.java

--- Java/94_BinaryTreeInorderTraversal.java
+++ Java/94_BinaryTreeInorderTraversal.java
@@ -11,21 +11,55 @@ return [1,3,2].
 
 Note: Recursive solution is trivial, could you do it iteratively?
 
+
+Method 1: divide & conquer
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-        TreeNode curt = root;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        while (curt != null || !stack.isEmpty()){
-            while (curt != null){
-                stack.push(curt);
-                curt = curt.left;
-            }
-            curt = stack.pop();
-            result.add(curt.val);
-            curt = curt.right;
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
         }
+        List<Integer> left = inorderTraversal(root.left);
+        List<Integer> right = inorderTraversal(root.right);
+
+        result.addAll(left);
+        result.add(root.val);
+        result.addAll(right);
         return result;
     }
 }
 
+Method 2: recursion
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(result, root);
+        return result;
+    }
+    private void helper(List<Integer> result, TreeNode node){
+        if (node == null){
+            return;
+        }
+        helper(result, node.left);
+        result.add(node.val);
+        helper(result, node.right);
+    }
+}
