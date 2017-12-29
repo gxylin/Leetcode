4b59671906cc871d56bb165b270acf83ac855235
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-28 23:21:06 -0500

    Create 257_BinaryTreePaths.java

---
+++ Java/257_BinaryTreePaths.java
@@ -0,0 +1,44 @@
+Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        if (root.left == null && root.right == null){
+            result.add(Integer.toString(root.val));
+            return result;
+        }
+        List<String> left = binaryTreePaths(root.left);
+        List<String> right = binaryTreePaths(root.right);
+        
+        for (String str : left){
+            result.add(root.val + "->" + str);
+        }
+        for (String str : right){
+            result.add(root.val + "->" + str);
+        }
+        return result;
+    }
+}
