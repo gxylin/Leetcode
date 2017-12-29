90eabcb01da770f8f0872090c88a07778850595c
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-29 11:35:21 -0500

    Create 378_ConvertBinarySearchTree toDoublyLinkedList.java

---
+++ Java/378_ConvertBinarySearchTree toDoublyLinkedList.java
@@ -0,0 +1,66 @@
+onvert a binary search tree to doubly linked list with in-order traversal.
+
+Have you met this question in a real interview? Yes
+Example
+Given a binary search tree:
+
+    4
+   / \
+  2   5
+ / \
+1   3
+return 1<->2<->3<->4<->5
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for Doubly-ListNode.
+ * public class DoublyListNode {
+ *     int val;
+ *     DoublyListNode next, prev;
+ *     DoublyListNode(int val) {
+ *         this.val = val;
+ *         this.next = this.prev = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    public DoublyListNode bstToDoublyList(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        Queue<Integer> queue = new LinkedList<Integer>();
+        traverse(root, queue);
+        
+        DoublyListNode dummy = new DoublyListNode(0);
+        DoublyListNode head = dummy;
+        while (!queue.isEmpty()){
+            DoublyListNode temp = new DoublyListNode(queue.poll());
+            head.next = temp;
+            temp.prev = head;
+            head = head.next;
+        }
+        return dummy.next;
+    }
+    private void traverse(TreeNode root, Queue<Integer> queue){
+        if (root == null){
+            return;
+        }
+        traverse(root.left, queue);
+        queue.offer(root.val);
+        traverse(root.right,queue);
+    }
+}
