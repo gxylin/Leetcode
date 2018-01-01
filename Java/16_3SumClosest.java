7186f47f9d7acbaafbd892abdf643c7f49aeff06
optimisea <34330974+optimisea@users.noreply.github.com>
2017-12-31 19:39:03 -0500

    Create 16_3SumClosest.java

---
+++ Java/16_3SumClosest.java
@@ -0,0 +1,37 @@
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, 
+target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+    
+    
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+       Arrays.sort(nums);
+        int best = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - 2; i++){
+            if (i > 0 && nums[i-1] == nums[i]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == target){
+                    return sum;
+                }else if (sum < target){
+                    left++;
+                }else{
+                    right--;
+                }
+                if (Math.abs(sum - target) < best){
+                    best = Math.abs(sum- target);
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+}
