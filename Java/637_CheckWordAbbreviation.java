3b17593ef86cf9155fa8c8d59bff5d41c4868b35
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-05 00:27:27 -0600

    Update 637_CheckWordAbbreviation.java

--- Java/637_CheckWordAbbreviation.java
+++ Java/637_CheckWordAbbreviation.java
@@ -5,7 +5,8 @@ A string such as "word" contains only the following valid abbreviations:
 ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
  Notice
 
-Notice that only the above abbreviations are valid abbreviations of the string word. Any other string is not a valid abbreviation of word.
+Notice that only the above abbreviations are valid abbreviations of the string word. Any other string is not a 
+valid abbreviation of word.
 
 Have you met this question in a real interview? Yes
 Example
