facb82d08b66cae0732ab5463599b3d0278c0948
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-06 22:14:19 -0600

    Update 27_RemoveElement.java

--- Java/27_RemoveElement.java
+++ Java/27_RemoveElement.java
@@ -15,10 +15,7 @@ class Solution {
         int index = 0;
         for (int i = 0; i < nums.length; i++){
             if (nums[i] != val){
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index] = temp;
-                index++;
+                nums[index++] = nums[i];
             }
         }
         return index;
