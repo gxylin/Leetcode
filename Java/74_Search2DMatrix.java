b0a9369b3ae19caa7d061a8163e29607db1e9e09
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-09 22:27:55 -0600

    Update 74_Search2DMatrix.java

--- Java/74_Search2DMatrix.java
+++ Java/74_Search2DMatrix.java
@@ -13,7 +13,7 @@ Consider the following matrix:
 ]
 Given target = 3, return true.
 
-Method: 
+Method 1: O(logm + logn)
 first binary search => target row
 second binary search => target col
 
@@ -60,5 +60,39 @@ class Solution {
             return true;
         }
         return false;
+    }
+}
+
+Method 2:
+Time complexity O(log(mn) = logm + logn)
+ // we can view this matrix as an sorted array matrix[0, m *n - 1], given a index in array
+// we can use (index / n) to get row index and use (index % n) to get column index
+// then use binary search to find the target
+ class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int start = 0;
+        int end = m * n - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            int r = mid / n;
+            int c = mid % n;
+            if (matrix[r][c] == target){
+                return true;
+            }else if (matrix[r][c] < target){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        if (matrix[start/n][start%n] == target || matrix[end/n][end%n] == target){
+            return true;
+        }
+        return false;
+        
     }
 }
