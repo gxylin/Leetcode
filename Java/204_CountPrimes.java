3ab5c4158ed13580f9181eab1e39f38f901a62c8
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-12 23:11:40 -0600

    Create 204_CountPrimes.java

---
+++ Java/204_CountPrimes.java
@@ -0,0 +1,20 @@
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+class Solution {
+    public int countPrimes(int n) {
+        int ans = 0;
+        boolean[] notPrime = new boolean[n];
+        for (int i = 2; i < n; i++){
+            if (!notPrime[i]){
+                notPrime[i] = false;
+                ans++;
+                for (int j = 2; i * j < n; j++){
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        return ans;
+    }
+}
