ca7e781a9934ac629aa945f1491febfc08178649
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-13 00:28:04 -0600

    Create 279_PerfectSquares.java

---
+++ Java/279_PerfectSquares.java
@@ -0,0 +1,21 @@
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+
+Method: dynamic programming
+class Solution {
+    public int numSquares(int n) {
+        int[] f = new int[n+1];
+        f[0] = 0;
+        for (int i = 1; i <= n; i++){
+            f[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j*j <= i; j++){
+                f[i] = Math.min(f[i], f[i-j*j] + 1);
+            }
+        }
+        return f[n];
+        
+    }
+}
