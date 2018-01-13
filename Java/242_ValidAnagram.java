def15d7904edd45c5e60d01c43642b80d740bec4
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-13 13:47:45 -0600

    Update 242_ValidAnagram.java

--- Java/242_ValidAnagram.java
+++ Java/242_ValidAnagram.java
@@ -65,3 +65,22 @@ class Solution {
         return false;
     }
 }
+
+Method 3:
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[256];
+        for (int i = 0; i < s.length(); i++){
+            alphabet[s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++){
+            alphabet[t.charAt(i)]--;
+        }
+        for (int i = 0; i < alphabet.length; i++){
+            if (alphabet[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
