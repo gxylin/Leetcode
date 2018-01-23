50ff58cfea15dedf037860caed3d90b93d6dc87a
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-23 00:22:42 -0600

    Create 116_JumpGame.java

---
+++ Java/116_JumpGame.java
@@ -0,0 +1,42 @@
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+ Notice
+This problem have two method which is Greedy and Dynamic Programming.
+
+The time complexity of Greedy method is O(n).
+
+The time complexity of Dynamic Programming method is O(n^2).
+
+We manually set the small data set to allow you pass the test in both ways. This is just to let you learn 
+how to use this problem in dynamic programming ways. If you finish it in dynamic programming ways,
+you can try greedy method to make it accept again.
+
+Method 1:
+dynamic programming
+Time complexity: O(n^2)
+public class Solution {
+    /*
+     * @param A: A list of integers
+     * @return: A boolean
+     */
+    public boolean canJump(int[] A) {
+        if (A == null || A.length == 0){
+            return false;
+        }
+        boolean[] can = new boolean[A.length];
+        can[0] = true;
+        for (int i = 1; i < A.length; i++){
+            for (int j = 0; j <i; j++){
+                if (can[j] && j + A[j] >= i){
+                    can[i] = true;
+                    break;
+                }
+            }
+        }
+        return can[A.length - 1];
+    }
+}
