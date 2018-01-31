1a3e4d2c3ac9f1a3acce230213311c799c0777a5
optimisea <34330974+optimisea@users.noreply.github.com>
2018-01-30 22:08:42 -0600

    Create 775. Global_Local_Inversions.java

---
+++ Java/775. Global_Local_Inversions.java
@@ -0,0 +1,37 @@
+We have some permutation A of [0, 1, ..., N - 1], where N is the length of A.
+
+The number of (global) inversions is the number of i < j with 0 <= i < j < N and A[i] > A[j].
+
+The number of local inversions is the number of i with 0 <= i < N and A[i] > A[i+1].
+
+Return true if and only if the number of global inversions is equal to the number of local inversions.
+
+Example 1:
+
+Input: A = [1,0,2]
+Output: true
+Explanation: There is 1 global inversion, and 1 local inversion.
+Example 2:
+
+Input: A = [1,2,0]
+Output: false
+Explanation: There are 2 global inversions, and 1 local inversion.
+
+All local inversions are global inversions.
+If the number of global inversions is equal to the number of local inversions,
+it means that all global inversions in permutations are local inversions.
+It also means that we can not find A[i] > A[j] with i+2<=j.
+In other words, max(A[i]) < A[i+2]
+
+class Solution {
+    public boolean isIdealPermutation(int[] A) {
+        int max = 0;
+        for (int i = 0; i < A.length - 2; i++){
+            max = Math.max(max, A[i]);
+            if (max > A[i+2]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
