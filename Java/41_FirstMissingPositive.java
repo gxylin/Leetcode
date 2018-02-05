1e85ca528cf98140c3f76df2b50638381723cacc
optimisea <34330974+optimisea@users.noreply.github.com>
2018-02-04 20:04:59 -0600

    Create 41_FirstMissingPositive.java

---
+++ Java/41_FirstMissingPositive.java
@@ -0,0 +1,35 @@
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+
+
+The key here is to use swapping to keep constant space and also make use of the length of the array,
+which means there can be at most n positive integers. 
+So each time we encounter an valid integer, find its correct position and swap. Otherwise we continue.
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length){
+            if (nums[i] <= 0 || nums[i] > nums.length || nums[i] == nums[nums[i] -1]){
+                i++;
+            }else if (nums[i] != nums[nums[i] - 1]){
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        i = 0;
+        while (i < nums.length && nums[i] == i + 1){
+            i++;
+        }
+        return i+1;
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
