2b32205a9789761f45c5dcfb67c3b71614842882
optimisea <34330974+optimisea@users.noreply.github.com>
2018-02-06 22:02:20 -0600

    Create 145_Postorder.java

---
+++ Java/145_Postorder.java
@@ -0,0 +1,40 @@
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+For example:
+Given binary tree [1,null,2,3],
+
+   1
+    \
+     2
+    /
+   3
+ 
+
+return [3,2,1].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+    private void dfs(TreeNode root, List<Integer> result){
+        if (root == null){
+            return;
+        }
+        dfs(root.left, result);
+        dfs(root.right, result);
+        result.add(root.val);
+    }
+}
