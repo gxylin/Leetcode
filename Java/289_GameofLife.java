54d34dde2d20fcc6c97575943869be981dadb949
optimisea <34330974+optimisea@users.noreply.github.com>
2018-02-10 22:13:57 -0600

    Update 289_GameofLife.java

--- Java/289_GameofLife.java
+++ Java/289_GameofLife.java
@@ -68,7 +68,20 @@ class Solution {
 
 Method 2:
 https://leetcode.com/problems/game-of-life/discuss/73223/Easiest-JAVA-solution-with-explanation
+[2nd bit, 1st bit] = [next state, current state]
 
+- 00  dead (next) <- dead (current)
+- 01  dead (next) <- live (current)  
+- 10  live (next) <- dead (current)  
+- 11  live (next) <- live (current) 
+    
+ To get the current state, simply do
+
+board[i][j] & 1
+To get the next state, simply do
+
+board[i][j] >> 1
+    
 public void gameOfLife(int[][] board) {
     if (board == null || board.length == 0) return;
     int m = board.length, n = board[0].length;
