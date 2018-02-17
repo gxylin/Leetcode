76ea33749df4f29783b1d1eb2cb85bea0a014d78
optimisea <34330974+optimisea@users.noreply.github.com>
2018-02-16 21:58:16 -0600

    Update 547_FriendCircles.java

--- Java/547_FriendCircles.java
+++ Java/547_FriendCircles.java
@@ -80,3 +80,55 @@ class Solution {
     }
 }
 
+Method 3: Union Find
+class Solution {
+    class UF{
+        int[] id; //parent link
+        int[] size; //size of component for each root id;
+        int count; // number of components
+        public UF(int N){
+            count = N;
+            id = new int[N];
+            size = new int[N];
+            for (int i = 0; i < N; i++){
+                id[i] = i;
+                size[i] = 1;
+            }
+        }
+        public int size(){
+            return count;
+        }
+        public int root(int p){
+            while (p != id[p]){
+                p = id[p];
+            }
+            return p;
+        }
+        public boolean find(int p, int q){
+            return root(p) == root(q);
+        }
+        public void union(int p, int q){
+            int rp = root(p);
+            int rq = root(q);
+            if (size[rp] < size[rq]){
+                id[rp] = rq;
+                size[rq] += size[rp];
+            }else{
+                id[rq] = rp;
+                size[rp] += size[rq];
+            }
+            count--;
+        }
+    }
+    public int findCircleNum(int[][] M) {
+        UF uf = new UF(M.length);
+        for (int i = 0; i < M.length; i++){
+            for (int j = i + 1; j < M[0].length; j++){
+                if (M[i][j] == 1 && !uf.find(i, j)){
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.size();
+    }
+}
