8c2ef5ed343af94f93f1166d97789480a7ea818e
optimisea <34330974+optimisea@users.noreply.github.com>
2018-02-24 13:03:18 -0600

    Update 169_MajorityElement.java

--- Java/169_MajorityElement.java
+++ Java/169_MajorityElement.java
@@ -27,16 +27,19 @@ class Solution {
 }
 
 Method 3:
-public class Solution {
-    public int majorityElement(int[] num) {
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
+class Solution {
+    public int majorityElement(int[] nums) {
+        int major = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == major){
                 count++;
-                major=num[i];
-            }else if(major==num[i]){
+            }else if (count == 0){
+                major = nums[i];
                 count++;
-            }else count--; 
+            }else{
+                count--;
+            }
         }
         return major;
     }
