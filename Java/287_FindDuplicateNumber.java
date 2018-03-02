379575ba34977aad7651e676b32f1766ede38f1c
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-02 00:08:06 -0600

    Update 287_FindDuplicateNumber.java

--- Java/287_FindDuplicateNumber.java
+++ Java/287_FindDuplicateNumber.java
@@ -21,3 +21,32 @@ class Solution {
     }
 }
 
+Method 2:
+Time complexity: O(nlogn)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int start = 1;
+        int end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (check_smaller_number(mid, nums) <= mid){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        if (check_smaller_number(start, nums) <= start){
+            return end;
+        }
+        return start;
+    }
+    private int check_smaller_number(int mid, int[] nums){
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] <= mid){
+                count++;
+            }
+        }
+        return count;
+    }
+}
