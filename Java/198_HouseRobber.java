87470a68089db3b2f636c5b710cdbb5757a11950
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-04 00:24:39 -0600

    Update 198_HouseRobber.java

--- Java/198_HouseRobber.java
+++ Java/198_HouseRobber.java
@@ -19,3 +19,20 @@ class Solution {
         return Math.max(a, b);
     }
 }
+
+Method 2: dynamic programming (序列型,接龙型）
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        int n = nums.length;
+        int[] f = new int[n+1];
+        f[0] = 0;
+        f[1] = nums[0];
+        for (int i = 2; i <= n; i++){
+            f[i] = Math.max(f[i-1], f[i-2] + nums[i-1]);
+        }
+        return f[n];
+    }
+}
