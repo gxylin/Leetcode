3ccd037fc316b3a644d0e92d36acd569822ddcf6
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-03 22:58:23 -0600

    Update 253_MeetingRoomsII.java

--- Java/253_MeetingRoomsII.java
+++ Java/253_MeetingRoomsII.java
@@ -50,3 +50,55 @@ class Solution {
         return max;
     }
 }
+
+
+Method 2: 
+Sweep Line: O(nlogn)
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    class Point{
+        int time;
+        int flag;
+        public Point(int time, int flag){
+            this.time = time;
+            this.flag = flag;
+        }
+    }
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0){
+            return 0;
+        }
+        List<Point> list = new ArrayList<>();
+        for(Interval i : intervals){
+            list.add(new Point(i.start, 1));
+            list.add(new Point(i.end, 0));
+        }
+        Collections.sort(list, new Comparator<Point>(){
+            public int compare(Point a, Point b){
+                if (a.time == b.time){
+                    return a.flag - b.flag;
+                }
+                return a.time - b.time;
+            }
+        });
+        int count = 0;
+        int ans = 0;
+        for(Point p : list){
+            if (p.flag == 1){
+                count++;
+            }else{
+                count--;
+            }
+            ans = Math.max(ans, count);
+        }
+        return ans;
+    }
+}
