a82ea18f4164bad1413215c9d42ad5ca38134f06
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-04 11:35:59 -0600

    Update 213_HouseRobberII.java

--- Java/213_HouseRobberII.java
+++ Java/213_HouseRobberII.java
@@ -37,3 +37,35 @@ class Solution {
     }
 }
 
+Method 2: 循环数组处理方法一： 分裂
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        int n = nums.length;
+        if (n == 1){
+            return nums[0];
+        }
+        int[] A = new int[n - 1];
+        for (int i = 0; i < n - 1; i++){
+            A[i] = nums[i];
+        }
+        int[] B = new int[n - 1];
+        for (int i = 1; i < n; i++){
+            B[i-1] = nums[i];
+        }
+        return Math.max(robI(A), robI(B));
+    }
+    private int robI(int[] nums){
+        int n = nums.length;
+        int[] f = new int[n+1];
+        f[0] = 0;
+        f[1] = nums[0];
+        for (int i = 2; i <= n; i++){
+            f[i] = Math.max(f[i-1], f[i-2] + nums[i-1]);
+        }
+        return f[n];
+    }
+}
+
