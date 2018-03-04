2103ebc01eb532e753be331c1d09aa3778111482
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-03 22:51:25 -0600

    Update 252_MeetingRooms.java

--- Java/252_MeetingRooms.java
+++ Java/252_MeetingRooms.java
@@ -32,3 +32,55 @@ class Solution {
         return true;
     }
 }
+
+Method 2:
+Sweep Line O(nlogn), n is the number of points
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    class Point{
+        int time;
+        int flag;
+        public Point(int time, int flag){
+            this.time = time;
+            this.flag = flag;
+        }
+    }
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0){
+            return true;
+        }
+        List<Point> list = new ArrayList<>();
+        for (Interval i : intervals){
+            list.add(new Point(i.start, 1));
+            list.add(new Point(i.end, 0));
+        }
+        Collections.sort(list, new Comparator<Point>(){
+            public int compare(Point a, Point b){
+                if (a.time == b.time){
+                    return a.flag - b.flag;
+                }
+                return a.time - b.time;
+            }
+        });
+        int count = 0;
+        for (Point p : list){
+            if (p.flag == 1){
+                count++;
+            }else{
+                count--;
+            }
+            if (count > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
