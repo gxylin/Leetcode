274dda87ef694f3be8b5abcb3b1c8c513bd0c7ba
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-04 22:21:44 -0600

    Create 394_CoinsLine.java

---
+++ Java/394_CoinsLine.java
@@ -0,0 +1,48 @@
+There are n coins in a line. Two players take turns to take one or two coins from right side 
+until there are no more coins left. The player who take the last coin wins.
+
+Could you please decide the first play will win or lose?
+
+Have you met this question in a real interview? 
+Example
+n = 1, return true.
+
+n = 2, return true.
+
+n = 3, return false.
+
+n = 4, return true.
+
+n = 5, return true.
+
+Challenge 
+O(n) time and O(1) memory
+
+http://www.jiuzhang.com/solutions/coins-in-a-line/
+
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: A boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int n) {
+       boolean[] dp = new boolean[n+1];
+       if (n <= 0){
+           return false;
+       }else if (n == 1){
+           return true;
+       }else if (n ==2){
+           return true;
+       }else if (n == 3){
+           return false;
+       }
+       dp[0] = false;
+       dp[1] = true;
+       dp[2] = true;
+       dp[3] = false;
+       for (int i = 4; i <= n; i++){
+           dp[i] = (dp[i-4] && dp[i-3]) || (dp[i-3] && dp[i-2]);
+       }
+       return dp[n];
+    }
+}
