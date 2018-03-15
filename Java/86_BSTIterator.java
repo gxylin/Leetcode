939ae3b4cb88e956b977701030eb8de2406a0674
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-14 20:22:56 -0500

    Create 86_BSTIterator.java

---
+++ Java/86_BSTIterator.java
@@ -0,0 +1,80 @@
+Design an iterator over a binary search tree with the following rules:
+
+Elements are visited in ascending order (i.e. an in-order traversal)
+next() and hasNext() queries run in O(1) time in average.
+Have you met this question in a real interview? 
+Example
+For the following binary search tree, in-order traversal by using iterator is [1, 6, 10, 11, 12]
+
+   10
+ /    \
+1      11
+ \       \
+  6       12
+Challenge 
+Extra memory usage O(h), h is the height of the tree.
+
+Super Star: Extra memory usage O(1)
+
+
+Method: Use stack to store infomation
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * BSTIterator iterator = new BSTIterator(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode node = iterator.next();
+ *    do something for node
+ * } 
+ */
+
+
+public class BSTIterator {
+    Stack<TreeNode> stack;
+    TreeNode next; //next treeNode to visit
+    /*
+    * @param root: The root of binary tree.
+    */public BSTIterator(TreeNode root) {
+        // do intialization if necessary
+        stack = new Stack<TreeNode>();
+        next = root;
+        addToStack(root);
+    }
+
+    /*
+     * @return: True if there has next node, or false
+     */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /*
+     * @return: return next node
+     */
+    public TreeNode next() {
+        if (!hasNext()){
+            return null;
+        }
+        TreeNode cur = stack.pop();
+        next = cur.right;
+        if (next != null){
+            addToStack(next);
+        }
+        return cur;
+    }
+    private void addToStack(TreeNode node){
+        while (node != null){
+            stack.push(node);
+            node = node.left;
+        }
+        next = null;
+    }
+}
