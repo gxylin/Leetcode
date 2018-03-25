e82a117975a0b277c1887ff6d5b432f77f3d1836
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-25 15:55:38 -0500

    Update 322_CoinChange.java

--- Java/322_CoinChange.java
+++ Java/322_CoinChange.java
@@ -74,3 +74,21 @@ class Solution {
         return count;
     }
 }
+
+Method 2: DP like LIS
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++){
+            for (int j = 0; j < coins.length; j++){
+                if (i - coins[j] >= 0){
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                } 
+            }
+        }
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+}
