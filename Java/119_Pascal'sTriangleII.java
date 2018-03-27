1486058e2a5f940ec7ef35eba796d12c207f10d5
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-27 00:23:09 -0500

    Update 119_Pascal'sTriangleII.java

--- Java/119_Pascal'sTriangleII.java
+++ Java/119_Pascal'sTriangleII.java
@@ -6,7 +6,9 @@ Return [1,3,3,1].
 Note:
 Could you optimize your algorithm to use only O(k) extra space?
 
-Time complexity: O(n)
+Method 1:
+Time complexity: O(k^2)
+Space complexity: O(1)
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<>();
@@ -18,6 +20,31 @@ class Solution {
             for (int j = i - 1; j > 0; j--){
                 result.set(j, result.get(j) + result.get(j-1));
             }
+        }
+        return result;
+    }
+}
+
+Method 2:
+Time complexity: O(k^2)
+Space complexity: O(2*k);
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        int[][] f = new int[2][rowIndex+1];
+        f[0][0] = 1;
+        for (int i = 1; i <= rowIndex ; i++){
+            for (int j = 0; j <= rowIndex; j++){
+                if (j == 0 || j == rowIndex){
+                    f[i%2][j] = 1;
+                }else{
+                    f[i%2][j] = f[(i-1)%2][j-1] + f[(i-1)%2][j];
+                }
+                
+            }
+        }
+        for (int i = 0; i <= rowIndex; i++){
+            result.add(f[rowIndex%2][i]);
         }
         return result;
     }
