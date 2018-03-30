4996402b26bccefb278c5bc101c4f801b7bb7e2d
optimisea <34330974+optimisea@users.noreply.github.com>
2018-03-29 20:49:29 -0500

    Update 662_MaximumWidthofBinaryTree.java

--- Java/662_MaximumWidthofBinaryTree.java
+++ Java/662_MaximumWidthofBinaryTree.java
@@ -96,3 +96,51 @@ class Solution {
         return max;
     }
 }
+
+Method 2:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> count = new LinkedList<>();
+        int max = 1;
+        queue.offer(root);
+        count.offer(1);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            int left = 0;
+            int right = 0;
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                Integer index = count.poll();
+                if (i == 0){
+                    left = index;
+                }
+                if (i == size - 1){
+                    right = index;
+                }
+                if (node.left != null){
+                    queue.offer(node.left);
+                    count.offer(index*2);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                    count.offer(index*2+1);
+                }
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
