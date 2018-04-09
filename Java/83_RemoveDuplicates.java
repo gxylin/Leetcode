93fab67c587deba2e95636a1b67cc7e25b202411
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-08 21:04:02 -0500

    Update 83_RemoveDuplicates.java

--- Java/83_RemoveDuplicates.java
+++ Java/83_RemoveDuplicates.java
@@ -12,6 +12,8 @@ Given 1->1->2->3->3, return 1->2->3.
  *     ListNode(int x) { val = x; }
  * }
  */
+    
+Method 1:
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null){
@@ -28,5 +30,28 @@ class Solution {
         }
         slow.next = null;
         return head;      
+    }
+}
+
+Method 2:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null){
+            if (cur.val == cur.next.val){
+                cur.next = cur.next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+        return head;
     }
 }
