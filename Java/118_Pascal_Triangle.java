6cff9d6e82c1199201e609f3ebd2882b7d57c264
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-10 16:06:00 -0500

    Create 118_Pascal_Triangle.java

---
+++ Java/118_Pascal_Triangle.java
@@ -0,0 +1,60 @@
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+Method 1:
+DP
+Time complexity: O(n^2)
+Space complexity: O(n^2)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[][] dp = new int[numRows+1][numRows+1];
+        for (int i = 1; i <= numRows; i++){
+            List<Integer> item = new ArrayList<>();
+            for (int j = 1; j <= i; j++){
+                if (j == 1 || j == i){
+                    dp[i][j] = 1;
+                }else{
+                   dp[i][j] =  dp[i-1][j-1] + dp[i-1][j];
+                }
+                item.add(dp[i][j]);
+            }
+            result.add(item);
+        }
+        return result;
+    }
+}
+Method 2:
+DP + rolling array
+Time complexity: O(n^2)
+Space complexity: O(n)
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[][] dp = new int[2][numRows+1];
+        for (int i = 1; i <= numRows; i++){
+            List<Integer> item = new ArrayList<>();
+            for (int j = 1; j <= i; j++){
+                if (j == 1 || j == i){
+                    dp[i%2][j] = 1;
+                }else{
+                   dp[i%2][j] =  dp[(i-1)%2][j-1] + dp[(i-1)%2][j];
+                }
+                item.add(dp[i%2][j]);
+            }
+            result.add(item);
+        }
+        return result;
+    }
+}
