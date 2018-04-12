ca1d7527b2a9d83549808ecde460a006079a7c33
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-12 17:52:20 -0500

    Create 214_Shortest.java

---
+++ Java/214_Shortest.java
@@ -0,0 +1,28 @@
+Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. 
+Find and return the shortest palindrome you can find by performing this transformation.
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+
+Given "abcd", return "dcbabcd"
+
+class Solution {
+    public String shortestPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        for (int i = len - 1; i >= 0; i--){
+            sb.append(s.charAt(i));
+        }
+        String rev = sb.toString();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < len; i++){
+            if (s.substring(0, len - i).equals(rev.substring(i))){
+                ans.append(rev.substring(0, i));
+                ans.append(s);
+                break;
+            }
+        }
+        return ans.toString();
+    }
+}
