722071b741fb76d097047660152f67acf4460d61
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-13 15:23:58 -0500

    Update 131_Skyline.java

--- Java/131_Skyline.java
+++ Java/131_Skyline.java
@@ -1,7 +1,81 @@
 http://www.lintcode.com/en/problem/building-outline/
 https://leetcode.com/problems/the-skyline-problem/description/
 
+
 Sweep line + TreeSet
+
+Leetcode:
+class Solution {
+    class Point{
+        int height;
+        int position;
+        int flag;
+        int start;
+        public Point(int height, int position, int flag, int start){
+            this.height = height;
+            this.position = position;
+            this.flag = flag;
+            this.start = start;
+        }
+    }
+    public List<int[]> getSkyline(int[][] buildings) {
+         List<int[]> result = new ArrayList<>();
+        List<Point> list = new ArrayList<>();
+        for(int[] b : buildings){
+            list.add(new Point(b[2], b[0], 1, b[0]));
+            list.add(new Point(b[2], b[1], 0, b[0]));
+        }
+        Collections.sort(list, new Comparator<Point>(){
+            public int compare(Point a, Point b){
+                if (a.position == b.position){
+                    return b.flag - a.flag;
+                }
+                return a.position - b.position;
+            }
+        });
+        TreeSet<Point> pointTree = new TreeSet<>(new Comparator<Point>(){
+            public int compare(Point a, Point b){
+                if (a.height == b.height){
+                    return a.position - b.position;
+                }
+                return a.height - b.height;
+            }
+        });
+        int prevHeight = 0;
+        int prevPos = -1;
+        int currHeight = 0;
+        
+        for (Point p : list){
+            if (p.flag == 1){
+                pointTree.add(p);
+                currHeight = pointTree.last().height;
+            }else{
+                pointTree.remove(new Point(p.height, p.start, 1, p.start));
+                currHeight = pointTree.isEmpty() ? 0 : pointTree.last().height;
+            }
+            if (currHeight != prevHeight){
+                if (prevHeight != 0 && prevPos != -1 && prevPos != p.position){
+                    int[] item = new int[2];
+                    item[0] = prevPos;
+                    item[1] = prevHeight;
+                    result.add(item);  
+                }
+                prevHeight = currHeight;
+                prevPos = p.position;
+                if (currHeight == 0){
+                    int[] ground = new int[2];
+                    ground[0] = p.position;
+                    ground[1] = currHeight;
+                    result.add(ground);
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
+Lintcode:
 public class Solution {
     /**
      * @param buildings: A list of lists of integers
