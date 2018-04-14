9aa668690bf515855eb808687a8c082d3e6ecad0
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-14 12:59:17 -0500

    Update 227_Basic.java

--- Java/227_Basic.java
+++ Java/227_Basic.java
@@ -16,7 +16,7 @@ class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
         int ans = 0;
-        int number = 0; //denote the previous number
+        int number = 0; //denote the current number
         char sign = '+'; //denote the previous sign
         for (int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
@@ -40,6 +40,74 @@ class Solution {
         while (!stack.isEmpty()){
             ans += stack.pop();
         }
+        return ans;
+    }
+}
+
+class Solution {
+    public int calculate(String s) {
+       Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+        int number = 0; //denote the current number
+        char sign = '+'; //denote the previous sign
+        stack.push(number);
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)){
+                number = 10 * number + (int) (c - '0');
+            }
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1){
+                if (sign == '+'){
+                    ans += stack.peek();
+                    stack.push(number);
+                }else if (sign == '-'){
+                    ans += stack.peek();
+                    stack.push(-number);
+                }else if (sign == '*'){
+                    stack.push(stack.pop() * number);
+                }else if (sign == '/'){
+                    stack.push(stack.pop() / number);
+                }
+                sign = c;
+                number = 0;
+            }
+        }
+        ans += stack.peek();
+        return ans;
+    }
+}
+
+
+
+Method 2: No stack
+class Solution {
+    public int calculate(String s) {
+        int ans = 0;
+        int number = 0; //denote the current number
+        int preNum = 0; //denote the previous number
+        char preSign = '+'; //denote the previous sign
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)){
+                number = 10 * number + (int) (c - '0');
+            }
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1){
+                if (preSign == '+'){
+                    ans += preNum;
+                    preNum = number;
+                }else if (preSign == '-'){
+                    ans += preNum;
+                    preNum = -number;
+                }else if (preSign == '*'){
+                    preNum = preNum * number;
+                }else if (preSign == '/'){
+                    preNum = preNum / number;
+                }
+                preSign = c;
+                number = 0;
+            }
+        }
+        ans += preNum;
         return ans;
     }
 }
