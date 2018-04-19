00d0dcb3a6b88d0f224e1e06793244ecd6241108
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-19 13:48:14 -0500

    Create 315_Count.java

---
+++ Java/315_Count.java
@@ -0,0 +1,31 @@
+You are given an integer array nums and you have to return a new counts array. The counts array has the property
+where counts[i] is the number of smaller elements to the right of nums[i].
+
+Example:
+
+Given nums = [5, 2, 6, 1]
+
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
+Return the array [2, 1, 1, 0].
+
+Method 1: Brute force O(n^2) TLE
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int count = 0;
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[j] < nums[i]){
+                    count++;
+                }
+            }
+            result.add(count);
+        }
+        return result;
+    }
+}
+
+Method 2:
