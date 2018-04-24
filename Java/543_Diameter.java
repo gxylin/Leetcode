a983fd7268077cd8b7a8b8e158f505011c493c9d
optimisea <34330974+optimisea@users.noreply.github.com>
2018-04-23 21:59:31 -0500

    Create 543_Diameter.java

---
+++ Java/543_Diameter.java
@@ -0,0 +1,71 @@
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a 
+binary tree is the length of the longest path between any two nodes in a tree. This path may or
+may not pass through the root.
+
+Example:
+Given a binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Note: The length of path between two nodes is represented by the number of edges between them.
+
+https://leetcode.com/articles/diameter-of-binary-tree/
+Method 1:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+    private int depth(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        int L = depth(root.left);
+        int R = depth(root.right);
+        ans = Math.max(ans, L+R+1);
+        return Math.max(L, R) + 1;
+    }
+}
+
+Method 2: without using global variable
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] ans = new int[1];
+        depth(root, ans);
+        return ans[0] - 1 < 0 ? 0 : ans[0] - 1;
+    }
+    private int depth(TreeNode root, int[] ans){
+        if (root == null){
+            return 0;
+        }
+        int L = depth(root.left, ans);
+        int R = depth(root.right, ans);
+        ans[0] = Math.max(ans[0], L+R+1);
+        return Math.max(L, R) + 1;
+    }
+}
+
