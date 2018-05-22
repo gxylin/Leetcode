fe305943bc636fce8eb7ec545bad6596ed515cbd
optimisea <34330974+optimisea@users.noreply.github.com>
2018-05-22 09:58:22 -0500

    Update 366_Find.java

--- 366_Find.java
+++ 366_Find.java
@@ -30,7 +30,7 @@ Explanation:
 Returns [4, 5, 3], [2], [1]. 
 
 Method 1:
-O(n^2)
+Time complexity: O(n^2)
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -65,3 +65,64 @@ class Solution {
     }
 }
 
+The following method is wrong because:
+Java does manipulate objects by reference, and all object variables are references. 
+          However, Java doesn't pass method arguments by reference; it passes them by value. 
+          Java copies and passes the reference by value, not the object.
+Hence, your just modified the copy of root and set it to null instead of the original root. So your code will run forever and TLE. 
+          The workaround is to return the root so it can be modified as shown in method 1
+
+ WRONG SOLUTION!!!
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        while (root != null){
+            List<Integer> item = new ArrayList<>();
+            dfs(item, root);
+            result.add(item);
+        }
+        return result;
+    }
+    private void dfs(List<Integer> item, TreeNode root){
+        if (root == null){
+            return;
+        }
+        if (root.left == null && root.right == null){
+            item.add(root.val);
+            root = null;
+            return;
+        }
+        dfs(item, root.left);
+        dfs(item, root.right);
+    }
+}
+
+Method 2: 
+Time complexity: O(n)
+bottom-up approach. The key is to find the height of each node. Here the definition of height is:
+The height of a node is the number of edges from the node to the deepest leaf. --CMU 15-121 Binary Trees
+According to the definition, the height of leaf is 0. h(node) = 1 + max(h(node.left), h(node.right)).
+The height of a node is also the its index in the result list (res). For example, leaves, whose heights are 0, are stored in res[0]. 
+Once we find the height of a node, we can put it directly into the result.
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        height(result, root);
+        return result;
+    }
+    private int height(List<List<Integer>> result, TreeNode root){
+        if (root == null){
+           return -1;
+        }
+        int left = height(result, root.left);
+        int right = height(result, root.right);
+        int currHeight = 1 + Math.max(left, right);
+        if (currHeight == result.size()){
+            result.add(new ArrayList<Integer>());
+        }
+        result.get(currHeight).add(root.val);
+        root = null;
+        return currHeight;
+        
+    }
+}
