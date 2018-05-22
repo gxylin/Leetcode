1d91af2f8e53bd2859e146cc18c9c3442f62ce7b
optimisea <34330974+optimisea@users.noreply.github.com>
2018-05-22 08:30:05 -0500

    Update 169_MajorityElement.java

--- Java/169_MajorityElement.java
+++ Java/169_MajorityElement.java
@@ -17,6 +17,18 @@ class Solution {
         return Integer.MIN_VALUE;
     }
 }
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if (map.get(nums[i]) > nums.length / 2){
+                return nums[i];
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+}
 
 Method 2:
 class Solution {
