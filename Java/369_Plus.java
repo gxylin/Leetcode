c063e348869b9fc4e40cbe9a0c40c321142cc8d5
optimisea <34330974+optimisea@users.noreply.github.com>
2018-05-23 00:26:11 -0500

    Update 369_Plus.java

--- Java/369_Plus.java
+++ Java/369_Plus.java
@@ -53,3 +53,79 @@ class Solution {
     }
 }
 
+Method 2:
+how to add value reversely: Use DFS
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode plusOne(ListNode head) {
+        if (head == null){
+            return head;
+        }
+        if (dfs(head) == 0){
+            return head;
+        }else{
+            ListNode newHead = new ListNode(1);
+            newHead.next = head;
+            return newHead;
+        }
+    }
+    private int dfs(ListNode node){
+        if (node == null){
+            return 1;
+        }
+        int carry = dfs(node.next);
+        if (carry == 0){
+            return 0;
+        }
+        int val = node.val + 1;
+        node.val = val % 10;
+        return val / 10;
+    }
+}
+
+Method 3:
+Iterative Two-Pointers with dummy node Java O(n) time, O(1) space
+
+    i stands for the most significant digit that is going to be incremented if there exists a carry
+    dummy node can handle cases such as "9->9>-9" automatically
+
+
+public class Solution {
+    public ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode i = dummy;
+        ListNode j = dummy;
+        
+        while (j.next != null) {
+            j = j.next;
+            if (j.val != 9) {
+                i = j;
+            }
+        }
+        
+        if (j.val != 9) {
+            j.val++;
+        } else {
+            i.val++;
+            i = i.next;
+            while (i != null) {
+                i.val = 0;
+                i = i.next;
+            }
+        }
+        
+        if (dummy.val == 0) {
+            return dummy.next;
+        }
+        
+        return dummy;
+    }
+}
