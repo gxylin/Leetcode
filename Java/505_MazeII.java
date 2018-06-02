343e0f4218b376310513831b36ecb139662cc795
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-02 11:39:07 -0500

    Update 505_MazeII.java

--- Java/505_MazeII.java
+++ Java/505_MazeII.java
@@ -198,3 +198,98 @@ Time complexity : O(m∗n∗max(m,n)). Complete traversal of maze will be done i
 
  Space complexity : O(mn). distancedistancedistance array of size m∗nm*nm∗n is used.
 
+Method 3: Dijkstra Algorithm
+
+
+Time complexity : O((mn)^2). Complete traversal of maze will be done in the worst case and function minDistance takes O(mn) time.
+
+Space complexity : O(mn). distancedistancedistance array of size m∗n is used.
+
+public class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        for (int[] row: distance)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        distance[start[0]][start[1]] = 0;
+        dijkstra(maze, distance, visited);
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
+    public int[] minDistance(int[][] distance, boolean[][] visited) {
+        int[] min={-1,-1};
+        int min_val = Integer.MAX_VALUE;
+        for (int i = 0; i < distance.length; i++) {
+            for (int j = 0; j < distance[0].length; j++) {
+                if (!visited[i][j] && distance[i][j] < min_val) {
+                    min = new int[] {i, j};
+                    min_val = distance[i][j];
+                }
+            }
+        }
+        return min;
+    }
+    public void dijkstra(int[][] maze, int[][] distance, boolean[][] visited) {
+        int[][] dirs={{0,1},{0,-1},{-1,0},{1,0}};
+        while (true) {
+            int[] s = minDistance(distance, visited);
+            if (s[0] < 0)
+                break;
+            visited[s[0]][s[1]] = true;
+            for (int[] dir: dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                int count = 0;
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                    count++;
+                }
+                if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                    distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
+                }
+            }
+        }
+    }
+}
+
+Method 4: Dijkstra Algorithm and Priority Queue
+
+
+Time complexity : O(mn∗log(mn)). Complete traversal of maze will be done in the worst case giving a factor of mn. Further, poll method is a combination of heapifying(O(log(n)) and removing the top elementO(1) from the priority queue, and it takes O(n) time for nnn elements. In the current case, poll introduces a factor of log(mn)log(mn)log(mn).
+
+Space complexity : O(mn). distancedistancedistance array of size m∗n is used and queuequeuequeue size can grow upto m∗nm*nm∗n in worst case.
+public class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        for (int[] row: distance)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        distance[start[0]][start[1]] = 0;
+        dijkstra(maze, start, distance);
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
+
+    public void dijkstra(int[][] maze, int[] start, int[][] distance) {
+        int[][] dirs={{0,1},{0,-1},{-1,0},{1,0}};
+        PriorityQueue < int[] > queue = new PriorityQueue < > ((a, b) -> a[2] - b[2]);
+        queue.offer(new int[]{start[0],start[1],0});
+        while (!queue.isEmpty()) {
+            int[] s = queue.poll();
+            if(distance[s[0]][s[1]] < s[2])
+                continue;
+            for (int[] dir: dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                int count = 0;
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                    count++;
+                }
+                if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                    distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
+                    queue.offer(new int[]{x - dir[0], y - dir[1], distance[x - dir[0]][y - dir[1]]});
+                }
+            }
+        }
+    }
+}
