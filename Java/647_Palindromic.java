9982d404c24d0e9de3837163fdf3ba8ca050912e
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-06 00:13:31 -0500

    Update 647_Palindromic.java

--- Java/647_Palindromic.java
+++ Java/647_Palindromic.java
@@ -36,5 +36,21 @@ class Solution {
     }
 }
 
-Method 2: DP
+Method 2: DP Best solution
 O(n^2)
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n-1; i >= 0; i--){
+            for (int j = i; j < n; j++){
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i+1][j-1]);
+                if (dp[i][j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
