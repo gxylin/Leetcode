5a38b678f70d8b7ef6ddf05fb75cc473a28bce78
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-15 00:14:17 -0500

    Create 695_Max.java

---
+++ Java/695_Max.java
@@ -0,0 +1,92 @@
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+Example 1:
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+
+Method 1: BFS Similar as Number of Island
+Time complexity: O(mn)
+Space complexity: O(mn)
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 1){
+                    int count = bfs(grid, i, j);
+                    max = Math.max(max, count);
+                }
+            }
+        }
+        return max;
+    }
+    private int bfs(int[][] grid, int i, int j){
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        grid[i][j] = 0;
+        while (!queue.isEmpty()){
+            int[] p = queue.poll();
+            int cx = p[0];
+            int cy = p[1];
+            count++;
+            for (int k = 0; k < dx.length; k++){
+                int x = cx + dx[k];
+                int y = cy + dy[k];
+                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1){
+                    queue.offer(new int[]{x, y});
+                    grid[x][y] = 0;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+Method 2: DFS recursion
+Time complexity: O(mn)
+Space complexity: O(mn)
+class Solution {
+    int[][] grid;
+    boolean[][] seen;
+
+    public int area(int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c) + area(r-1, c)
+                  + area(r, c-1) + area(r, c+1));
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
+        seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c));
+            }
+        }
+        return ans;
+    }
+}
