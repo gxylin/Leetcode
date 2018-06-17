7f2f0f2472d14d8627a75ddc61c72ea4e88b9ffd
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-17 15:44:13 -0500

    Update 731_My.java

--- Java/731_My.java
+++ Java/731_My.java
@@ -27,6 +27,7 @@ The number of calls to MyCalendar.book per test case will be at most 1000.
 In calls to MyCalendar.book(start, end), start and end are integers in the range [0, 10^9].
 
 Method 1: brute force
+Time complexity: O(n)
 class MyCalendarTwo {
     class Interval {
         int start;
@@ -64,3 +65,35 @@ class MyCalendarTwo {
  * MyCalendarTwo obj = new MyCalendarTwo();
  * boolean param_1 = obj.book(start,end);
  */
+
+ Method 2: sweep line (best)
+ Time complexity: O(n)
+ class MyCalendarTwo {
+    TreeMap<Integer, Integer> map;
+    public MyCalendarTwo() {
+        map = new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        int active = 0;
+        for (int d : map.values()){
+            active += d;
+            if (active >= 3){
+                map.put(start, map.get(start) - 1);
+                map.put(end, map.get(end) + 1);
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
+                                                       
+                                                       
