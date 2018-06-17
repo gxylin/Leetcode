b29fe7f5205c9c5b6e64c40c8a8f2776abd1f5a1
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-17 16:43:10 -0500

    Update 253_MeetingRoomsII.java

--- Java/253_MeetingRoomsII.java
+++ Java/253_MeetingRoomsII.java
@@ -102,3 +102,34 @@ class Solution {
         return ans;
     }
 }
+
+Method 3: Best solution (inspired by My calendar II)
+Time complexity: O(nlogn)
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0){
+            return 0;
+        }
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(Interval interval : intervals){
+            map.put(interval.start, map.getOrDefault(interval.start, 0) + 1);
+            map.put(interval.end, map.getOrDefault(interval.end, 0) - 1);
+        }
+        int active = 0;
+        int max = Integer.MIN_VALUE;
+        for (int val : map.values()){
+            active += val;
+            max = Math.max(max, active);
+        }
+        return max;
+    }
+}
