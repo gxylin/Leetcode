ad5021d378ce7ab2574c39b9b142e6e6403bef22
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-18 22:33:24 -0500

    Update 740_Delete.java

--- Java/740_Delete.java
+++ Java/740_Delete.java
@@ -32,7 +32,7 @@ class Solution {
             count[i]++;
         }
         int[] dp = new int[10001];
-        dp[1] = count[1];
+        dp[1] = 1 * count[1];
         for (int i = 2; i <= 10000; i++){
             dp[i] = Math.max(dp[i-1], dp[i-2] + i * count[i]);
         }
