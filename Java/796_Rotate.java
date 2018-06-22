52be461f5b04a99bca737e335e3af4ca8484448c
optimisea <34330974+optimisea@users.noreply.github.com>
2018-06-21 21:41:21 -0500

    Create 796_Rotate.java

---
+++ Java/796_Rotate.java
@@ -0,0 +1,46 @@
+
+We are given two strings, A and B.
+
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+
+Example 1:
+Input: A = 'abcde', B = 'cdeab'
+Output: true
+
+Example 2:
+Input: A = 'abcde', B = 'abced'
+Output: false
+Note:
+
+A and B will have length at most 100.
+
+Method 1:
+class Solution {
+    public boolean rotateString(String A, String B) {
+        return A.length() == B.length() && (A+A).contains(B);
+    }
+}
+
+Method 2:
+class Solution {
+    public boolean rotateString(String A, String B) {
+        if (A.length() == 0 && B.length() == 0){
+            return true;
+        }
+        if (A.length() != B.length()){
+            return false;
+        }
+        int idx = 0;
+        while (idx < A.length()){
+            while (idx < A.length() && A.charAt(idx) != B.charAt(0)){
+                idx++;
+            }
+            String rotated = A.substring(idx) + A.substring(0, idx);
+            if (rotated.equals(B)){
+                return true;
+            }
+            idx++;
+        }
+        return false;
+    }
+}
