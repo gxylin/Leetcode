4ff709fb6335878cedb6382fce43841b33c550ad
optimisea <34330974+optimisea@users.noreply.github.com>
2018-07-11 00:28:36 -0500

    Update 752_Open.java

--- Java/752_Open.java
+++ Java/752_Open.java
@@ -164,5 +164,17 @@ class Solution {
     }
 }
 
-
+  for(int i = 0; i < 4; i ++) {
+     char c = sb.charAt(i);
+     String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+     String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
+     if(!visited.contains(s1) && !deads.contains(s1)) {
+         q.offer(s1);
+         visited.add(s1);
+     }
+     if(!visited.contains(s2) && !deads.contains(s2)) {
+         q.offer(s2);
+         visited.add(s2);
+      }
+}
  
