f38156eefff75489c2ca7a26a9f9c7f0a13e62bf
optimisea <34330974+optimisea@users.noreply.github.com>
2018-07-15 15:33:27 -0500

    Create 845_Longest.java

---
+++ Java/845_Longest.java
@@ -0,0 +1,58 @@
+Let's call any (contiguous) subarray B (of A) a mountain if the following properties hold:
+
+B.length >= 3
+There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
+(Note that B could be any subarray of A, including the entire array A.)
+
+Given an array A of integers, return the length of the longest mountain. 
+
+Return 0 if there is no mountain.
+
+Example 1:
+
+Input: [2,1,4,7,3,2,5]
+Output: 5
+Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+Example 2:
+
+Input: [2,2,2]
+Output: 0
+Explanation: There is no mountain.
+Note:
+
+0 <= A.length <= 10000
+0 <= A[i] <= 10000
+Follow up:
+
+Can you solve it using only one pass?
+Can you solve it in O(1) space?
+
+Method 1:
+class Solution {
+    public int longestMountain(int[] A) {
+        if (A.length < 3){
+            return 0;
+        }
+        int longest = 0;
+        int i = 0;
+        while (i < A.length - 1){
+            while (i < A.length - 1 && A[i+1] <= A[i]){
+                i++;
+            }
+            int start = i;
+            while (i < A.length - 1 && A[i+1] > A[i]){
+                i++;
+            }
+            int peak = i;
+            while (i < A.length - 1 && A[i+1] < A[i]){
+                i++;
+            }
+            int end = i;
+            if (start < peak && peak < end){
+                longest = Math.max(longest, i - start + 1);
+            }
+        }
+        return longest;
+    }
+}
+
