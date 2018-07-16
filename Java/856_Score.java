4306e238a4f0f3ebfa1e187f81566d8506af98ac
optimisea <34330974+optimisea@users.noreply.github.com>
2018-07-16 14:14:55 -0500

    Create 856_Score.java

---
+++ Java/856_Score.java
@@ -0,0 +1,100 @@
+Given a balanced parentheses string S, compute the score of the string based on the following rule:
+
+() has score 1
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+ 
+
+Example 1:
+
+Input: "()"
+Output: 1
+Example 2:
+
+Input: "(())"
+Output: 2
+Example 3:
+
+Input: "()()"
+Output: 2
+Example 4:
+
+Input: "(()(()))"
+Output: 6
+ 
+
+Note:
+
+S is a balanced parentheses string, containing only ( and ).
+2 <= S.length <= 50
+
+Method 1: Better
+class Solution {
+    public int scoreOfParentheses(String S) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < S.length(); i++){
+            char c = S.charAt(i);
+            if (c == '('){
+                stack.push(-1);
+            }else{
+                int cur = 0; //store the sum between ( and ), if there is no value, push 1 otherwise push value * 2
+                while (stack.peek() != -1){
+                    cur += stack.pop();
+                }
+                stack.pop();
+                stack.push(cur == 0 ? 1 : cur * 2);
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()){
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
+
+Method 2:
+class Solution {
+    public int scoreOfParentheses(String S) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        int cur = 0;//store 
+        for (int i = 0; i < S.length(); i++){
+            char c = S.charAt(i);
+            if (c == '('){
+                stack.push(cur);
+                cur = 0;
+            }else{
+                cur = stack.pop();
+                if (S.charAt(i-1) == '('){
+                    cur += 1;
+                }else{
+                    cur += res * 2;
+                }
+                res = cur;
+            }
+        }
+        return res;
+    }
+}
+
+Method 3: Best
+Our goal is to maintain the score at the current depth we are on. When we see an opening bracket, we increase our depth, 
+and our score at the new depth is 0. When we see a closing bracket,
+we add twice the score of the previous deeper part - except when counting (), which has a score of 1.
+class Solution {
+    public int scoreOfParentheses(String S) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (char c : S.toCharArray()){
+            if (c == '('){
+                stack.push(0);
+            }else{
+                int v = stack.pop();
+                int w = stack.pop();
+                stack.push(w + Math.max(v * 2, 1));
+            }
+        }
+        return stack.pop();
+    }
+}
