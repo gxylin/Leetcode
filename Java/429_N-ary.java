14b11af42dccca1c455f5de6278c867da4e0c164
optimisea <34330974+optimisea@users.noreply.github.com>
2018-07-25 09:55:17 -0500

    Create 429_N-ary.java

---
+++ Java/429_N-ary.java
@@ -0,0 +1,41 @@
+Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example, given a 3-ary tree:
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++){
+                Node node = queue.poll();
+                list.add(node.val);
+                for (Node n : node.children){
+                    queue.offer(n);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
