cd42750775dea70c9b4f5f7e7c36b6a6119b2dbc
optimisea <34330974+optimisea@users.noreply.github.com>
2018-07-26 17:29:31 -0500

    Create 559_Maximum.java

---
+++ Java/559_Maximum.java
@@ -0,0 +1,49 @@
+Given a n-ary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+For example, given a 3-ary tree:
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public int maxDepth(Node root) {
+        return dfs(root);
+    }
+    private int dfs(Node root){
+        if (root == null){
+            return 0;
+        }
+        int max = 0;
+        for (Node node : root.children){
+            max = Math.max(max, dfs(node));
+        }
+        return max + 1;
+    }
+}
+
+
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null){
+            return 0;
+        }
+        int max = 0;
+        for (Node node : root.children){
+            max = Math.max(max, dfs(node));
+        }
+        return max + 1;
+    }
+}
