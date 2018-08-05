2ce45d920c958666b14e84a0a5c960c94da67760
optimisea <34330974+optimisea@users.noreply.github.com>
2018-08-05 17:56:51 -0500

    Create 727_Minimum.java

---
+++ Java/727_Minimum.java
@@ -0,0 +1,91 @@
+Given strings S and T, find the minimum (contiguous) substring W of S, so that T is a subsequence of W.
+
+If there is no such window in S that covers all characters in T, return the empty string "". If there are multiple such minimum-length windows, return the one with the left-most starting index.
+
+Example 1:
+
+Input: 
+S = "abcdebdde", T = "bde"
+Output: "bcde"
+Explanation: 
+"bcde" is the answer because it occurs before "bdde" which has the same length.
+"deb" is not a smaller window because the elements of T in the window must occur in order.
+
+ 
+
+Note:
+
+    All the strings in the input will only contain lowercase letters.
+    The length of S will be in the range [1, 20000].
+    The length of T will be in the range [1, 100].
+
+
+Method 1: DP
+Time complexity: O(mn)
+Space complexity: O(mn)
+class Solution {
+    public String minWindow(String S, String T) {
+        int m = T.length();
+        int n = S.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int j = 0; j <= n; j++){
+            dp[0][j] = j+1;
+        }
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (T.charAt(i-1) == S.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+        for (int j = 1; j <= n; j++){
+            if (dp[m][j] != 0){
+                int cand = j - (dp[m][j] - 1);
+                if (cand < len){
+                    head = dp[m][j] - 1;
+                    len = cand;
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : S.substring(head, head + len);
+    }
+}
+
+class Solution {
+    public String minWindow(String S, String T) {
+        int m = T.length();
+        int n = S.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        for (int j = 0; j <= n; j++){
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (T.charAt(i-1) == S.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+        for (int j = 1; j <= n; j++){
+            if (dp[m][j] != -1){
+                int cand = j - dp[m][j];
+                if (cand < len){
+                    head = dp[m][j];
+                    len = cand;
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : S.substring(head, head + len);
+    }
+}
