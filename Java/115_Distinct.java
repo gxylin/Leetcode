22f35facc445b85034e6587ad7d58fe20c3961c9
optimisea <34330974+optimisea@users.noreply.github.com>
2018-08-05 20:52:15 -0500

    Create 115_Distinct.java

---
+++ Java/115_Distinct.java
@@ -0,0 +1,60 @@
+Given a string S and a string T, count the number of distinct subsequences of S which equals T.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+Example 1:
+
+Input: S = "rabbbit", T = "rabbit"
+Output: 3
+Explanation:
+
+As shown below, there are 3 ways you can generate "rabbit" from S.
+(The caret symbol ^ means the chosen letters)
+
+rabbbit
+^^^^ ^^
+rabbbit
+^^ ^^^^
+rabbbit
+^^^ ^^^
+Example 2:
+
+Input: S = "babgbag", T = "bag"
+Output: 5
+Explanation:
+
+As shown below, there are 5 ways you can generate "bag" from S.
+(The caret symbol ^ means the chosen letters)
+
+babgbag
+^^ ^
+babgbag
+^^    ^
+babgbag
+^    ^^
+babgbag
+  ^  ^^
+babgbag
+    ^^^
+    
+Method: Dynamic Programming
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = t.length();
+        int n = s.length();
+        int[][] dp = new int[m+1][n+1];//the number of cases before and including index j of String t for index i of String t
+        for (int j = 0; j <= n; j++){
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (t.charAt(i-1) == s.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
