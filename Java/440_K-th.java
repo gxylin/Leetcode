df4096c8ce908f6f45ecff0a4879afc81b37b0b6
optimisea <34330974+optimisea@users.noreply.github.com>
2018-08-12 18:27:39 -0500

    Create 440_K-th.java

---
+++ Java/440_K-th.java
@@ -0,0 +1,41 @@
+Given integers n and k, find the lexicographically k-th smallest integer in the range from 1 to n.
+
+Note: 1 ≤ k ≤ n ≤ 109.
+
+Example:
+
+Input:
+n: 13   k: 2
+
+Output:
+10
+
+Explanation:
+The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
+
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k = k -1;
+        while (k > 0){
+            int steps = calStep(n, curr, curr+1);
+            if (steps <= k){
+                curr += 1;
+                k -= steps;
+            }else{
+                curr *= 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+    private int calStep(int n, long n1, long n2){
+        int steps = 0;
+        while (n1 <= n){
+            steps += Math.min(n2 - n1, n - n1 + 1);
+            n1 *= 10;
+            n2 *= 10;
+        }
+        return steps;
+    }
+}
