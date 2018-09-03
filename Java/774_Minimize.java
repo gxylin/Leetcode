0fd1a7e663f1b16acf9e80bfa995ff9fd69b09d7
optimisea <34330974+optimisea@users.noreply.github.com>
2018-09-03 00:53:48 -0500

    Update 774_Minimize.java

--- Java/774_Minimize.java
+++ Java/774_Minimize.java
@@ -50,6 +50,22 @@ class Solution {
     }
 }
 
+Why did I use s binary search?
+In fact there are some similar problems on Leetcode so that is part of experience.
+Secondly, I got a hint from "Answers within 10^-6 of the true value will be accepted as correct.". The first solution I tried was binary search.
+Because binary search may not find exact value but it can approach the true answer.
+
+Explanation of solution
+Now we are using binary search to find the smallest possible value of D.
+I initilze left = 0 and right = the distance between the first and the last station
+count is the number of gas station we need to make it possible.
+if count > K, it means mid is too small to realize using only K more stations.
+if count <= K, it means mid is possible and we can continue to find a bigger one.
+When left + 1e-6 >= right, it means the answer within 10^-6 of the true value and it will be accepted.
+
+Time complexity:
+O(NlogM), where N is station length and M is st[N - 1] - st[0]
+
 Intuition
 
 Let's ask possible(D): with K (or less) gas stations, can we make every adjacent distance between gas stations at most D? This function is monotone, so we can apply a binary search to find D*D^{\text{*}}D​*​​.
