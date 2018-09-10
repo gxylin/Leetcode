b1666b9f18d7867a08185f2884f75e8db1d1d765
optimisea <34330974+optimisea@users.noreply.github.com>
2018-09-10 15:04:39 -0500

    Update 106_Construct.java

--- Java/106_Construct.java
+++ Java/106_Construct.java
@@ -24,6 +24,8 @@ Return the following binary tree:
  *     TreeNode(int x) { val = x; }
  * }
  */
+Method 1: Recursion
+Time complexity: O(nlogn)
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
@@ -45,5 +47,27 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+
+Method 2: Recursion + HashMap
+Time complexity: O(n)
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        return dfs(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, map);
+    }
+    private TreeNode dfs(int[] inorder, int iStart, int iEnd, int[] postorder, int pStart, int pEnd, Map<Integer, Integer> map){
+        if (iStart > iEnd || pStart > pEnd){
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[pEnd]);
+        int deltaIndex = map.get(postorder[pEnd]) - iStart;
+        root.left = dfs(inorder, iStart, iStart + deltaIndex - 1, postorder, pStart, pStart + deltaIndex -1, map);
+        root.right = dfs(inorder, iStart + deltaIndex + 1, iEnd, postorder, pStart + deltaIndex, pEnd - 1, map);
+        return root;
     }
 }
