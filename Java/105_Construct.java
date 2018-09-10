188aceb82c8deaa8758a85a87dcaaac22224ffd9
optimisea <34330974+optimisea@users.noreply.github.com>
2018-09-10 14:53:19 -0500

    Update 105_Construct.java

--- Java/105_Construct.java
+++ Java/105_Construct.java
@@ -14,7 +14,8 @@ Return the following binary tree:
   9  20
     /  \
    15   7
-   
+
+  
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -24,6 +25,8 @@ Return the following binary tree:
  *     TreeNode(int x) { val = x; }
  * }
  */
+Method 1: Recursion
+Time complexity: O(nlogn)
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);        
@@ -46,5 +49,27 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+
+Method 2: Recursion + HashMap
+Time complexity: O(n)
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        return dfs(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+    }
+    private TreeNode dfs(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd, Map<Integer, Integer> map){
+        if (pStart > pEnd || iStart > iEnd){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[pStart]);
+        int deltaIndex = map.get(preorder[pStart]) - iStart;
+        root.left = dfs(preorder, pStart + 1, pStart + deltaIndex, inorder, iStart, iStart + deltaIndex - 1, map);
+        root.right = dfs(preorder, pStart + deltaIndex + 1, pEnd, inorder, iStart + deltaIndex + 1, iEnd, map);
+        return root;
     }
 }
