b36e07e40fd83434af72c73164af33964b29b3e2
optimisea <34330974+optimisea@users.noreply.github.com>
2018-09-21 01:07:54 -0500

    Update 904_Fruit.java

--- Java/904_Fruit.java
+++ Java/904_Fruit.java
@@ -111,4 +111,26 @@ class Solution {
     }
 }
 
-
+Method 3: Slidnig window with hashMap
+class Solution {
+    public int totalFruit(int[] tree) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int N = tree.length;
+        int res = 0;
+        int start = 0;
+        int end = 0;
+        while (end < N){
+            map.put(tree[end], map.getOrDefault(tree[end], 0) + 1);
+            while (map.size() > 2){
+                map.put(tree[start], map.get(tree[start]) - 1);
+                if (map.get(tree[start]) == 0){
+                    map.remove(tree[start]);
+                }
+                start++;
+            }
+            res = Math.max(res, end - start + 1);
+            end++;
+        }
+        return res;
+    }
+}
