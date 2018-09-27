a1a227519c24e5eeac8c3269bf0d4da009dc07b2
optimisea <34330974+optimisea@users.noreply.github.com>
2018-09-26 23:09:13 -0500

    Update 907_Sum.java

--- Java/907_Sum.java
+++ Java/907_Sum.java
@@ -47,14 +47,14 @@ class Solution {
         int n = A.length;
         int res = 0;
         Stack<Integer> stack = new Stack<>();//monotonic stack and  store index, but the value of index in ascending
-        int[] dp = new int[n+1];
-        stack.push(0);
+        int[] dp = new int[n+1]; //dp[i] indicates the sum of subarray minimum ending at i-1
+        stack.push(0); //dummy index is set to 0
         for (int i = 1; i <= n; i++){
             while (stack.peek() != 0 && A[stack.peek()-1] >= A[i-1]){
                 stack.pop();
             }
             dp[i] = (dp[stack.peek()] + (i - stack.peek()) * A[i-1]) % mod;
-            stack.push(i);
+            stack.push(i);//stack stores the index + 1
             res = (res + dp[i]) % mod;
         }
         return res;
