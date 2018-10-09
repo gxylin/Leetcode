8e28062a3cb5e03852d1e3b4f80053c0d987b841
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-08 20:20:08 -0500

    Create 914_gcd.java

---
+++ Java/914_gcd.java
@@ -0,0 +1,62 @@
+In a deck of cards, each card has an integer written on it.
+
+Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
+
+Each group has exactly X cards.
+All the cards in each group have the same integer.
+ 
+
+Example 1:
+
+Input: [1,2,3,4,4,3,2,1]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[3,3],[4,4]
+Example 2:
+
+Input: [1,1,1,2,2,2,3,3]
+Output: false
+Explanation: No possible partition.
+Example 3:
+
+Input: [1]
+Output: false
+Explanation: No possible partition.
+Example 4:
+
+Input: [1,1]
+Output: true
+Explanation: Possible partition [1,1]
+Example 5:
+
+Input: [1,1,2,2,2,2]
+Output: true
+Explanation: Possible partition [1,1],[2,2],[2,2]
+
+Note:
+
+1 <= deck.length <= 10000
+0 <= deck[i] < 10000
+ 
+ 
+ class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : deck){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        int res = 0;
+        for (int val : map.values()){
+            res = gcd(res, val);
+        }
+        return res > 1;
+    }
+    private int gcd(int a, int b){
+        return b == 0 ? a : gcd(b, a%b);
+    }
+}
+
+
+How to calculate greatest common divisor
+private int gcd (int a, int b){
+  return b == 0 ? a : gcd (b, a%b);
+}
