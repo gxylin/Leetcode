f482a6271982dbde8947f768ad825fc730041a2a
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-18 15:23:52 -0500

    Update 11_ContainerWater.java

--- Java/11_ContainerWater.java
+++ Java/11_ContainerWater.java
@@ -28,3 +28,21 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+        while (left < right){
+            if (height[left] < height[right]){
+                max = Math.max(max, (right - left) * height[left]);
+                left++;
+            }else{
+                max = Math.max(max, (right - left) * height[right]);
+                right--;
+            }
+        }
+        return max;
+    }
+}
