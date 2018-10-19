7caba2b749dd4682f37ce064605ea98c18d9d545
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-18 20:39:13 -0500

    Update 364_TrappingRainWaterII.java

--- Java/364_TrappingRainWaterII.java
+++ Java/364_TrappingRainWaterII.java
@@ -67,3 +67,62 @@ public class Solution {
         return ans;
     }
 }
+
+
+
+class Solution {
+    class Node {
+        int x;
+        int y;
+        int h;
+        public Node (int x, int y, int h){
+            this.x = x;
+            this.y = y;
+            this.h = h;
+        }
+    }
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap == null || heightMap.length == 0){
+            return 0;
+        }
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        Queue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>(){
+            public int compare (Node n1, Node n2){
+                return n1.h - n2.h;
+            }
+        });
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++){
+            pq.offer(new Node(i, 0, heightMap[i][0]));
+            visited[i][0] = true;
+            pq.offer(new Node(i, n-1, heightMap[i][n-1]));
+            visited[i][n-1] = true;
+        }
+        for (int j = 1; j < n-1; j++){
+            pq.offer(new Node(0, j, heightMap[0][j]));
+            visited[0][j] = true;
+            pq.offer(new Node(m-1, j, heightMap[m-1][j]));
+            visited[m-1][j] = true;
+        }
+        int[][] dirs = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        int res = 0;
+        while (!pq.isEmpty()){
+            Node node = pq.poll();
+            for (int[] dir : dirs){
+                int nx = node.x + dir[0];
+                int ny = node.y + dir[1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]){
+                    if (heightMap[nx][ny] < node.h){
+                        res += node.h - heightMap[nx][ny];
+                        pq.offer(new Node(nx, ny, node.h));//note that this node must be pushed in queue and set height as node.h not heightMap[nx][ny]
+                    }else{
+                        pq.offer(new Node(nx, ny, heightMap[nx][ny]));
+                    }
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
