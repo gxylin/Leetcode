c970d8bb93560c8746c74e4ec4840107d42e5fcd
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-19 17:31:56 -0500

    Update 156_MergeIntervals.java

--- Java/156_MergeIntervals.java
+++ Java/156_MergeIntervals.java
@@ -55,3 +55,33 @@ public class Solution {
         
     }
 }
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> res = new ArrayList<>();
+        Collections.sort(intervals, new Comparator<Interval>(){
+            public int compare (Interval i1, Interval i2){
+                return i1.start - i2.start;
+            }
+        });
+        Interval last = null;
+        for (Interval interval : intervals){
+            if (last == null || last.end < interval.start){
+                last = interval;
+                res.add(last);
+            }else{
+                last.end = Math.max(last.end, interval.end);
+            }
+        }
+        return res;
+    }
+}
