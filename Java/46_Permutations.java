d15ffd82677e8c9562adceb9817fd84c0dfec802
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-20 10:36:58 -0500

    Update 46_Permutations.java

--- Java/46_Permutations.java
+++ Java/46_Permutations.java
@@ -26,11 +26,37 @@ class Solution {
             return;
         }
         for (int i = 0; i < nums.length ; i++){
-            if (item.contains(nums[i])){
+            if (item.contains(nums[i])){ //arraylist contains time complexity O(n)
                 continue;
             }
             item.add(nums[i]);
             dfs(result, item, nums);
+            item.remove(item.size() - 1);
+        }
+    }
+}
+
+Better
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+        dfs(res, new ArrayList<Integer>(), seen, nums);
+        return res;
+    }
+    private void dfs(List<List<Integer>> res, List<Integer> item, Set<Integer> seen, int[] nums){
+        if(item.size() == nums.length){
+            res.add(new ArrayList<Integer>(item));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++){
+            if (seen.contains(nums[i])){
+                continue;
+            }
+            item.add(nums[i]);
+            seen.add(nums[i]);
+            dfs(res, item, seen, nums);
+            seen.remove(nums[i]);
             item.remove(item.size() - 1);
         }
     }
