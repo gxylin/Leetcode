0012ad448084c30d19c2dddcb5d38a845824d851
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-19 22:30:12 -0500

    Update 141_LinkedListCycle.java

--- Java/141_LinkedListCycle.java
+++ Java/141_LinkedListCycle.java
@@ -32,3 +32,31 @@ public class Solution {
     }
 }
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null){
+            return false;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null && fast != slow){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast == null || fast.next == null){
+            return false;
+        }
+        return true;
+    }
+}
