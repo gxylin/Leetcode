1601babbc76096596e9219ba3c4c72b23d633b56
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-19 22:42:04 -0500

    Update 142_LinkedListCycleII.java

--- Java/142_LinkedListCycleII.java
+++ Java/142_LinkedListCycleII.java
@@ -1,4 +1,4 @@
-iven a linked list, return the node where the cycle begins. If there is no cycle, return null.
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 Note: Do not modify the linked list.
 
@@ -36,5 +36,44 @@ public class Solution {
             }
         }
         return null;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCylce = false;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                hasCylce = true;
+                break;
+            }
+        }
+        if (!hasCylce){
+            return null;
+        }
+        ListNode node = head;
+        while (node != slow){
+            node = node.next;
+            slow = slow.next;
+        }
+        return node;
     }
 }
