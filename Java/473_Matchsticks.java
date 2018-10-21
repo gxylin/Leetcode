58e26ac7eb8f6f08b3b3ff2076888d65e0b76b9a
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-21 11:41:31 -0500

    Update 473_Matchsticks.java

--- Java/473_Matchsticks.java
+++ Java/473_Matchsticks.java
@@ -1,6 +1,9 @@
-Remember the story of Little Match Girl? By now, you know exactly what matchsticks the little match girl has, please find out a way you can make one square by using up all those matchsticks. You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
+Remember the story of Little Match Girl? By now, you know exactly what matchsticks the little match girl has, please find out a way 
+you can make one square by using up all those matchsticks. You should not break any stick, but you can link them up, and each 
+matchstick must be used exactly one time.
 
-Your input will be several matchsticks the girl has, represented with their stick length. Your output will either be true or false, to represent whether you could make one square using all the matchsticks the little match girl has.
+Your input will be several matchsticks the girl has, represented with their stick length. Your output will either be true or false,
+to represent whether you could make one square using all the matchsticks the little match girl has.
 
 Example 1:
 Input: [1,1,2,2,2]
