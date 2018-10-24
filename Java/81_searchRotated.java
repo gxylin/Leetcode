c5ec2888f11bb6cb612ad3a856d86d03ae76f804
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-23 23:04:39 -0500

    Update 81_searchRotated.java

--- Java/81_searchRotated.java
+++ Java/81_searchRotated.java
@@ -53,3 +53,50 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0){
+            return false;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target){
+                return true;
+            }
+            int last = nums[end];
+            if (target == last){
+                return true;
+            }else if (target > last){
+                if (nums[mid] == last){
+                    end--;
+                    if (nums[mid] == nums[start]){
+                        start++;
+                    }
+                }else if (nums[mid] > last && nums[mid] < target){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+            }else {
+                if (nums[mid] == last){
+                    end--;
+                    if (nums[mid] == nums[start]){
+                        start++;
+                    }
+                }else if (nums[mid] < last && nums[mid] > target){
+                    end = mid;
+                }else{
+                    start = mid;
+                }
+            }
+        }
+        if (nums[start] == target || nums[end] == target){
+            return true;
+        }
+        return false;
+    }
+}
