f81f18b25239f2b8a557fa817420887524208228
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-23 22:10:46 -0500

    Update 33_SearchinRotatedSortedArray.java

--- Java/33_SearchinRotatedSortedArray.java
+++ Java/33_SearchinRotatedSortedArray.java
@@ -128,3 +128,41 @@ class Solution {
         return end;        
     }
 }
+
+
+Best solution:
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int last = nums[end];
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (target > last){
+                if (nums[mid] < target && nums[mid] > last){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+            }else{
+                if (nums[mid] > target && nums[mid] < last){
+                    end = mid;
+                }else{
+                    start = mid;
+                }
+            }
+        }
+        if (nums[start] == target){
+            return start;
+        }else if (nums[end] == target){
+            return end;
+        }
+        return -1;
+    }
+}
