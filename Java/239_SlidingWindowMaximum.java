6ac2df0de1ab2ab97df80970d0afb04e97827322
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-24 21:29:07 -0500

    Update 239_SlidingWindowMaximum.java

--- Java/239_SlidingWindowMaximum.java
+++ Java/239_SlidingWindowMaximum.java
@@ -39,7 +39,7 @@ class Solution {
         return result;
     }
 }
-Method 2: use deque
+Method 2: use deque O(n)
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0){
@@ -63,4 +63,58 @@ class Solution {
         return result;   
     }
     
+}
+
+Method 3: O(nk) use priority queue
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0){
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        Queue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>(){
+           public int compare (Integer i1, Integer i2){
+               return i2 - i1;
+           } 
+        });
+        for (int i = 0; i < k; i++){
+            pq.offer(nums[i]);
+        }
+        res[0] = pq.peek();
+        for (int i = k; i < n; i++){
+            pq.remove(nums[i-k]); // take O(k)
+            pq.offer(nums[i]);
+            res[i-k+1] = pq.peek();
+            
+        }
+        return res;
+    }
+}
+
+
+Best solution:
+https://leetcode.com/problems/sliding-window-maximum/discuss/65881/O(n)-solution-in-Java-with-two-simple-pass-in-the-array?page=3
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0){
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] maxLeft = new int[n];
+        int[] maxRight = new int[n];
+        maxLeft[0] = nums[0];
+        maxRight[n-1] = nums[n-1];
+        for (int i = 1; i < nums.length; i++){
+            maxLeft[i] = i % k == 0 ? nums[i] : Math.max(maxLeft[i-1], nums[i]);
+            int j = n - 1 - i;
+            maxRight[j] = j % k == 0 ? nums[j] : Math.max(maxRight[j+1], nums[j]);
+        }
+        int[] res = new int[n-k+1];
+        for (int i = 0; i < n-k+1; i++){
+            res[i] = Math.max(maxRight[i], maxLeft[i+k-1]);
+        }
+        return res;
+    }
 }
