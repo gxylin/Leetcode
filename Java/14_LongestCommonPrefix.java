b7bd5ba0fd443f95fe021b3ea2107b5ca1bc8c17
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-31 15:09:10 -0500

    Update 14_LongestCommonPrefix.java

--- Java/14_LongestCommonPrefix.java
+++ Java/14_LongestCommonPrefix.java
@@ -1,5 +1,17 @@
 Write a function to find the longest common prefix string amongst an array of strings.
 
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0){
@@ -12,5 +24,51 @@ class Solution {
             }
         }
         return pre;
+    }
+}
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Boolean found = false;
+        for (int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++){
+                if (i == strs[j].length() || c != strs[j].charAt(i)){
+                    found = true;
+                    break;
+                }
+            }
+            if (!found){
+                sb.append(c);
+            }else{
+                break;
+            }   
+        }
+        return sb.toString();
+    }
+}
+
+Better: after sorting, only compare first and last
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        for (int i = 0; i < first.length(); i++){
+            if (first.charAt(i) != last.charAt(i)){
+                break;
+            }else{
+                sb.append(first.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
