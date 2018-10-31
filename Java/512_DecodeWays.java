83f44733beef520ae8bb2b041fc25568800be5cd
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-31 15:34:26 -0500

    Update 512_DecodeWays.java

--- Java/512_DecodeWays.java
+++ Java/512_DecodeWays.java
@@ -69,3 +69,28 @@ public class Solution {
         return f[s.length()];
     }
 }
+
+Better:
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0'){
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++){
+            int curr = (int)(s.charAt(i-1) - '0');
+            int prev = (int)(s.charAt(i-2) - '0');
+            int num = prev * 10 + curr;
+            if (curr != 0){
+                dp[i] += dp[i-1];
+            }
+            if (num >= 10 && num <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
