aea51df2deb364709464697621abb364a60702e6
optimisea <34330974+optimisea@users.noreply.github.com>
2018-10-30 20:12:32 -0500

    Update 54_SpiralMatrix.java

--- Java/54_SpiralMatrix.java
+++ Java/54_SpiralMatrix.java
@@ -47,3 +47,46 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0){
+            return res;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0;
+        int bottom = m - 1;
+        int left = 0;
+        int right = n - 1;
+        while (top <= bottom && left <= right){
+            for (int j = left; j <= right; j++){
+                res.add(matrix[top][j]);
+            }
+            top++;
+            if (top > bottom){
+                break;
+            }
+            for (int i = top; i <= bottom; i++){
+                res.add(matrix[i][right]);
+            }
+            right--;
+            if (left > right){
+                break;
+            }
+            for (int j = right; j >= left; j--){
+                res.add(matrix[bottom][j]);
+            }
+            bottom--;
+            if (top > bottom){
+                break;
+            }
+            for (int i = bottom; i >= top; i--){
+                res.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return res;
+    }
+}
