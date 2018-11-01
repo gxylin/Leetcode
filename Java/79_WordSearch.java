820d922fa9a71bc1e377753deaead0d489913c6c
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-01 15:58:23 -0500

    Update 79_WordSearch.java

--- Java/79_WordSearch.java
+++ Java/79_WordSearch.java
@@ -55,3 +55,47 @@ class Solution {
         return false;
     }
 }
+
+Better:
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        StringBuilder sb = new StringBuilder();
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (word.charAt(0) != board[i][j]){
+                    continue;
+                }
+                if (search(board, i, j, word, dirs, 0, visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean search(char[][] board, int x, int y, String word, int[][] dirs, int pos, boolean[][] visited){
+        if (board[x][y] != word.charAt(pos)){
+            return false;
+        }
+        if (pos == word.length() - 1){
+            return true;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++){
+            int nx = x + dirs[i][0];
+            int ny = y + dirs[i][1];
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]){
+                if (search(board, nx, ny, word, dirs, pos + 1, visited)){
+                    return true;
+                }
+            }
+        }
+        visited[x][y] = false;
+        return false;
+    }
+}
