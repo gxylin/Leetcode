0b4c99cc03c9e08b5063ecebb1ba3b33b57504da
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-02 00:00:02 -0500

    Update 141_Sqrt.java

--- Java/141_Sqrt.java
+++ Java/141_Sqrt.java
@@ -40,3 +40,21 @@ public class Solution {
         return (int) start;
     }
 }
+
+class Solution {
+    public int mySqrt(int x) {
+        long start = 1;
+        long end = x;
+        while (start <= end){
+            long mid = start + (end - start) / 2;
+            if (mid * mid == x){
+                return (int)mid;
+            }else if (mid * mid < x){
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+        }
+        return (int) Math.min(start, end);
+    }
+}
