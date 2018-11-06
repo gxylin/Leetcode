dd517481f0a9e768ecb9c366354d5dbcf66b20a1
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-05 21:57:54 -0600

    Update 236_LowestCommonAncestorBinaryTree.java

--- Java/236_LowestCommonAncestorBinaryTree.java
+++ Java/236_LowestCommonAncestorBinaryTree.java
@@ -47,3 +47,38 @@ class Solution {
         return null;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return lca(root, p, q);
+    }
+    private TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
+        if (root == null){
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val){
+            return root;
+        }
+        TreeNode left = lca(root.left, p, q);
+        TreeNode right = lca(root.right, p, q);
+        if (left == null && right == null){
+            return null;
+        }
+        if (left == null){
+            return right;
+        }
+        if (right == null){
+            return left;
+        }
+        return root;
+    }
+}
