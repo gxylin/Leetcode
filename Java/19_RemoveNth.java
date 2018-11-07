a4a37ac3c69c2d62311611d2b7362512c005c25c
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-06 23:01:13 -0600

    Update 19_RemoveNth.java

--- Java/19_RemoveNth.java
+++ Java/19_RemoveNth.java
@@ -36,3 +36,31 @@ class Solution {
         return dummy.next;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        int count = -1;
+        while (count < n){
+            fast = fast.next;
+            count++;
+        }
+        while (fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
