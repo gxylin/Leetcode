b30ebe48f28d763cf2e4e0a308835c8ab3b8aa39
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-07 11:41:01 -0600

    Update 74_Search2DMatrix.java

--- Java/74_Search2DMatrix.java
+++ Java/74_Search2DMatrix.java
@@ -96,3 +96,35 @@ Time complexity O(log(mn) = logm + logn)
         
     }
 }
+
+
+Better version:
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            int num = converter(matrix, mid);
+            if (num == target){
+                return true;
+            }else if (num < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+    private int converter(int[][] matrix, int index){
+        int n = matrix[0].length;
+        int row = index / n;
+        int col = index % n;
+        return matrix[row][col];
+    }
+}
