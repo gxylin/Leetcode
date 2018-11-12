9c1cf54a9f9ecc8d44bb88ebc0b955385db180c3
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-12 14:27:15 -0600

    Update 136_SingleNumber.java

--- Java/136_SingleNumber.java
+++ Java/136_SingleNumber.java
@@ -17,3 +17,24 @@ class Solution {
         return ans;
     }
 }
+
+
+Generalization format as Single II 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++){
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++){
+                if (((nums[j] >> i) & 1) == 1){
+                    sum++;
+                    sum %= 2;
+                }
+            }
+            if (sum != 0){
+                res |= (sum << i);
+            }
+        }
+        return res;
+    }
+}
