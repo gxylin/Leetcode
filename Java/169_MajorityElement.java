a7379a3c52f1941c23e915a47a82b63764c28b6b
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-12 15:12:02 -0600

    Update 169_MajorityElement.java

--- Java/169_MajorityElement.java
+++ Java/169_MajorityElement.java
@@ -56,3 +56,45 @@ class Solution {
         return major;
     }
 }
+
+Best solution:
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++){
+            if (count == 0){
+                res = nums[i];
+                count = 1;
+            }else if (nums[i] == res){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return res;
+    }
+}
+
+Method 4:
+bit manipulation:
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++){
+            int ones = 0;
+            int zeros = 0;
+            for (int j = 0; j < nums.length; j++){
+                if ((nums[j] & (1 << i)) != 0){
+                    ones++;
+                }else{
+                    zeros++;
+                }
+            }
+            if (ones > zeros){
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+}
