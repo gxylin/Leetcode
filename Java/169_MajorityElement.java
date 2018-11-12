443acca8cdee42c5e6331d629e00496ce6f0c7f8
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-12 15:44:27 -0600

    Update 169_MajorityElement.java

--- Java/169_MajorityElement.java
+++ Java/169_MajorityElement.java
@@ -43,11 +43,11 @@ class Solution {
     public int majorityElement(int[] nums) {
         int count = 0;
         int res = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (count == 0){
-                res = nums[i];
+        for (int num : nums){
+            if (num == res){
                 count++;
-            }else if (res == nums[i]){
+            }else if (count == 0){
+                res = num;
                 count++;
             }else{
                 count--;
