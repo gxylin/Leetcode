0e6577500bf62452c2f9f5a7e3f2aec629bf70ce
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-14 15:30:14 -0600

    Update 269_AlienDictionary.java

--- Java/269_AlienDictionary.java
+++ Java/269_AlienDictionary.java
@@ -55,7 +55,8 @@ t:0
 f:0
 e:0
 
-Then build the hashmap by comparing the adjacent words, the first character that is different between two adjacent words reflect the lexicographical order. For example:
+Then build the hashmap by comparing the adjacent words, the first character that is different between two adjacent words 
+reflect the lexicographical order. For example:
 
  "wrt",
  "wrf",
@@ -65,7 +66,8 @@ Then build the hashmap by comparing the adjacent words, the first character that
  "er",
     first different character is 1rd letter, so w comes before e
 
-The characters in set come after the key. x->y means letter x comes before letter y. x -> set: y,z,t,w means x comes before all the letters in the set. The final HashMap “map” looks like.
+The characters in set come after the key. x->y means letter x comes before letter y. x -> set: y,z,t,w means x comes 
+before all the letters in the set. The final HashMap “map” looks like.
 
 t -> set: f    
 w -> set: e
