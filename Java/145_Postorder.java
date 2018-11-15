986688f36e2d4e9cc73470038fa3dafc17d8d4df
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-14 21:59:12 -0600

    Update 145_Postorder.java

--- Java/145_Postorder.java
+++ Java/145_Postorder.java
@@ -38,3 +38,35 @@ class Solution {
         result.add(root.val);
     }
 }
+
+Method 2: iteration: similar logic compared to preOrder iteration
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            res.add(0, node.val);
+            if (node.left != null){
+                stack.push(node.left);
+            }
+            if (node.right != null){
+                stack.push(node.right);
+            }
+        }
+        return res;
+    }
+}
