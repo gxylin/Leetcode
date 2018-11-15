598d4577b62f4c2fe4584084ea61f080722c7364
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-14 21:27:40 -0600

    Update 94_BinaryTreeInorderTraversal.java

--- Java/94_BinaryTreeInorderTraversal.java
+++ Java/94_BinaryTreeInorderTraversal.java
@@ -63,3 +63,31 @@ class Solution {
         helper(result, node.right);
     }
 }
+
+Method 3: iteration:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()){
+            while (curr != null){
+                stack.add(curr);
+                curr = curr.left;
+            }
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            curr = node.right;
+        }
+        return res;
+    }
+}
