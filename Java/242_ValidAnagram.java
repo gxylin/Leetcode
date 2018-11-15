913116142ab37ac140171d064726c08424cb9d8b
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-14 20:48:19 -0600

    Update 242_ValidAnagram.java

--- Java/242_ValidAnagram.java
+++ Java/242_ValidAnagram.java
@@ -84,3 +84,26 @@ class Solution {
         return true;
     }
 }
+
+Better version:
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()){
+            if (!map.containsKey(c)){
+                return false;
+            }else{
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        for (int val : map.values()){
+            if (val != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
