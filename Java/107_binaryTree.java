6ddb6ba356b75fc31fc2880c7d488e2439bcf574
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-15 13:00:54 -0600

    Update 107_binaryTree.java

--- Java/107_binaryTree.java
+++ Java/107_binaryTree.java
@@ -49,3 +49,34 @@ class Solution {
         return result;
     }
 }
+
+
+Method 2: dfs
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, res, 0);
+        return res;
+    }
+    private void dfs(TreeNode root, List<List<Integer>> res, int height){
+        if (root == null){
+            return;
+        }
+        if (height == res.size()){
+            List<Integer> list = new ArrayList<>();;
+            res.add(list);
+        }
+        res.get(height).add(root.val);
+        dfs(root.left, res, height+1);
+        dfs(root.right, res, height+1);
+    }
+}
