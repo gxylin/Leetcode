cc8b5678ed573a59519390e0038d0e40d5a0a86b
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-15 19:23:50 -0600

    Update 235_LowestCommonAncestorBinarySearchTree.java

--- Java/235_LowestCommonAncestorBinarySearchTree.java
+++ Java/235_LowestCommonAncestorBinarySearchTree.java
@@ -41,6 +41,22 @@ class Solution {
     }
 }
 
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;
+        }
+        if (root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
+
+
 Method 2: iteration
 /**
  * Definition for a binary tree node.
