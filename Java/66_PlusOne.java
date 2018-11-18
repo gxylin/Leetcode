5a8607876a4ab4491caf41f1c843b83c935b193d
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-18 16:30:31 -0600

    Update 66_PlusOne.java

--- Java/66_PlusOne.java
+++ Java/66_PlusOne.java
@@ -18,3 +18,23 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--){
+            int num = digits[i] + carry;
+            digits[i] = num % 10;
+            carry = num / 10;
+        }
+        if (carry == 1){
+            int[] res = new int[digits.length+1];
+            res[0] = 1;
+            for (int i = 1; i < res.length; i++){
+                res[i] = digits[i-1];
+            }
+            return res;
+        }
+        return digits;
+    }
+}
