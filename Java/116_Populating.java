add9a2a58f82ab240d1da05525a46aa55d664fc4
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-19 17:09:04 -0600

    Update 116_Populating.java

--- Java/116_Populating.java
+++ Java/116_Populating.java
@@ -79,3 +79,40 @@ public class Solution {
         }
     }
 }
+
+
+Method 3: BFS
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if (root == null){
+            return;
+        }
+        Queue<TreeLinkNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeLinkNode node = queue.poll();
+                if (i == size - 1){
+                    node.next = null;
+                }else{
+                    node.next = queue.peek();
+                }
+                if (node.left != null){
+                    queue.offer(node.left);
+                }
+                if (node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+    }
+}
