4530975f68d4d9fc9040fdb297fd91d9abca02fd
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-23 14:50:21 -0600

    Update 279_PerfectSquares.java

--- Java/279_PerfectSquares.java
+++ Java/279_PerfectSquares.java
@@ -5,17 +5,16 @@ For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return
 Method: dynamic programming
 class Solution {
     public int numSquares(int n) {
-        int[] f = new int[n+1];
-        f[0] = 0;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         for (int i = 1; i <= n; i++){
-            f[i] = Integer.MAX_VALUE;
-        }
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j*j <= i; j++){
-                f[i] = Math.min(f[i], f[i-j*j] + 1);
+            for (int j = 0; j * j <= i; j++){
+                if (dp[i - j * j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
             }
         }
-        return f[n];
-        
+        return dp[n];
     }
 }
