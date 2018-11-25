114fa307bb06c6a31d613fd3392e568068deebcb
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-25 12:48:44 -0600

    Update 131_PalindromePartitioning.java

--- Java/131_PalindromePartitioning.java
+++ Java/131_PalindromePartitioning.java
@@ -10,6 +10,7 @@ Return
   ["a","a","b"]
 ]
 
+Method 1: DFS
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
@@ -33,6 +34,54 @@ class Solution {
                 item.remove(item.size() - 1);
             }
         }
+    }
+    private boolean isValid(String s){
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right){
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+
+Method 2: Recursion + memo
+class Solution {
+    Map<String, List<List<String>>> map = new HashMap<>();
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        if (s == null || s.length() == 0){
+            return res;
+        }
+        if (map.containsKey(s)){
+            return map.get(s);
+        }
+        for (int i = 1; i <= s.length(); i++){
+            String sub = s.substring(0, i);
+            if (isValid(sub)){
+                List<List<String>> temp = partition(s.substring(i));
+                if (temp.size() == 0){
+                    List<String> list = new ArrayList<>();
+                    list.add(sub);
+                    res.add(list);
+                }else{
+                    for (List<String> item: temp){
+                        List<String> list = new ArrayList<>();
+                        list.add(sub);
+                        list.addAll(item);
+                        res.add(list);
+                    }
+                }
+                
+            }
+        }
+        map.put(s, res);
+        return res;
     }
     private boolean isValid(String s){
         int left = 0;
