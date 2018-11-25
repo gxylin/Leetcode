3cf49e0ef6c7cd6bd30b6d5c9911f427722bca15
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-25 01:11:48 -0600

    Update 32_Longest.java

--- Java/32_Longest.java
+++ Java/32_Longest.java
@@ -46,20 +46,23 @@ Time complexity: O(n)
 Space complexity: O(n)
 class Solution {
     public int longestValidParentheses(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
         int n = s.length();
-        int ans = 0;
-        int[] dp = new int[n+1];
-        for (int i = 1; i < s.length(); i++){
+        int max = 0;
+        int[] dp = new int[n]; // denotes the longest valid parentheses ending at index i (but include index i)
+        for (int i = 1; i < n; i++){
             if (s.charAt(i) == ')'){
                 if (s.charAt(i-1) == '('){
-                     dp[i] = (i -2 >= 0 ? dp[i-2] : 0)+ 2;
+                    dp[i] = (i - 2 >= 0 ? dp[i-2] : 0) + 2;
                 }else if (i - dp[i-1] - 1 >= 0 && s.charAt(i-dp[i-1]-1) == '('){
-                    dp[i] = (i - dp[i-1] - 2 >= 0 ? dp[i-dp[i-1]-2] : 0) + dp[i-1] + 2;
+                    dp[i] = (i-dp[i-1]-2 >= 0 ? dp[i-dp[i-1]-2]: 0) + dp[i-1] + 2;
                 }
             }
-            ans = Math.max(ans, dp[i]);
+            max = Math.max(max, dp[i]);
         }
-        return ans;
+        return max;
     }
 }
 
