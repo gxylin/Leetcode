3ba64d5f4f655f2e95c93bf22e1b7f2c8c0a6f3c
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-25 22:17:12 -0600

    Create 945_Minimum.java

---
+++ Java/945_Minimum.java
@@ -0,0 +1,57 @@
+Given an array of integers A, a move consists of choosing any A[i], and incrementing it by 1.
+
+Return the least number of moves to make every value in A unique.
+
+ 
+
+Example 1:
+
+Input: [1,2,2]
+Output: 1
+Explanation:  After 1 move, the array could be [1, 2, 3].
+Example 2:
+
+Input: [3,2,1,2,1,7]
+Output: 6
+Explanation:  After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+It can be shown with 5 or less moves that it is impossible for the array to have all unique values.
+ 
+
+Note:
+
+0 <= A.length <= 40000
+0 <= A[i] < 40000
+
+Method:
+Sort the array.
+Compared with previous number,
+the current number need to be at least prev + 1.
+Time Complexity: O(NlogN)
+
+class Solution {
+    public int minIncrementForUnique(int[] A) {
+        Arrays.sort(A);
+        int res = 0;
+        for (int i = 0; i < A.length - 1; i++){
+            int val = Math.max(A[i] + 1, A[i+1]);
+            res += val - A[i+1];
+            A[i+1] = val;
+        }
+        return res;
+    }
+}
+
+Better version:
+class Solution {
+    public int minIncrementForUnique(int[] A) {
+        Arrays.sort(A);
+        int res = 0;
+        int need = 0;
+        for (int a : A){
+            res += Math.max(need - a, 0);
+            need = Math.max(need, a) + 1;
+        }
+        return res;
+    }
+}
+
