b3fce5d89ca61d9880d15db7cc20d9251b364e92
optimisea <34330974+optimisea@users.noreply.github.com>
2018-11-26 23:05:22 -0600

    Update 771_JewelsandStones.java

--- Java/771_JewelsandStones.java
+++ Java/771_JewelsandStones.java
@@ -33,3 +33,19 @@ class Solution {
         return count;
     }
 }
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<>();
+        for (char c : J.toCharArray()){
+            set.add(c);
+        }
+        int res = 0;
+        for(char c : S.toCharArray()){
+            if (set.contains(c)){
+                res++;
+            }
+        }
+        return res;
+    }
+}
