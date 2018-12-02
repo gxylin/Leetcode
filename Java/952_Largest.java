2e5a6b210c1e7e7ec020138bc0fb033e8a31f4a4
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-02 16:27:40 -0600

    Update 952_Largest.java

--- Java/952_Largest.java
+++ Java/952_Largest.java
@@ -5,6 +5,73 @@ There is an edge between A[i] and A[j] if and only if A[i] and A[j] share a comm
 Return the size of the largest connected component in the graph.
 
 
+    
+Best Solution:
+
+Time complexity: O(N*sqrt(Max val of A[i]))
+Union Find template. The only additional stuff is one hashmap which is used to convert factor to the node index 
+in A for union.
+    
+class Solution {
+    class UF {
+        int[] parent;
+        int[] size;
+        int max;
+        public UF (int N){
+            parent = new int[N];
+            size = new int[N];
+            max = 1;
+            for (int i = 0; i < N; i++){
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        public int find(int x){
+            if (x == parent[x]){
+                return x;
+            }
+            return find(parent[x]);
+        }
+        public void union(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY){
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+                max = Math.max(max, size[rootY]);
+            }
+        }
+    }
+    public int largestComponentSize(int[] A) {
+        int N = A.length;
+        Map<Integer, Integer> map = new HashMap<>();// index is the factor, val is the node index
+        UF uf = new UF(N);
+        for (int i = 0; i < N; i++){
+            int a = A[i];
+            for (int j = 2; j * j <= a; j++){
+                if (a % j == 0){
+                    if (!map.containsKey(j)){
+                        map.put(j, i);
+                    }else{
+                        uf.union(i, map.get(j));
+                    }
+                    if (!map.containsKey(a/j)){
+                        map.put(a/j, i);
+                    }else{
+                        uf.union(i, map.get(a/j));
+                    }
+                }
+            }
+            if (!map.containsKey(a)){//a could be factor too. Don't miss this
+                map.put(a, i);
+            }else{
+                uf.union(i, map.get(a));
+            }
+        }
+        return uf.max;
+    }
+}
+    
 Method 1: 
 Time complexity: O(N*sqrt(Max val of A[i]))
 Union Find template. The only additional stuff is one hashmap factorToNode which is used to convert factor to the node index 
