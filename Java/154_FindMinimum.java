cc630f7d044ba40e9c0339b9d444d51257cc6fc3
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-03 10:23:58 -0600

    Update 154_FindMinimum.java

--- Java/154_FindMinimum.java
+++ Java/154_FindMinimum.java
@@ -36,3 +36,24 @@ class Solution {
         return nums[end];
     }
 }
+
+class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int target = nums[end];
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == nums[start]){
+                start++;
+            }else if (nums[mid] == nums[end]){
+                end--;
+            }else if (nums[mid] <= target){
+                end = mid;
+            }else{
+                start = mid;
+            }
+        }
+        return Math.min(nums[start], nums[end]);
+    }
+}
