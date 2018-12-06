138b61a852c433cf0f69cbbe17e1a56d0b665df3
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-06 15:09:12 -0600

    Update trieTemplate.java

--- Java/trieTemplate.java
+++ Java/trieTemplate.java
@@ -42,3 +42,53 @@ class Trie {
     }
     
     Refer to Leetcode 212 word search II for Trie + backtracking
+
+
+Better version:
+class Trie {
+        class TrieNode {
+            TrieNode[] children;
+            boolean isEnd;
+            public TrieNode () {
+                children = new TrieNode[26];
+                isEnd = false;
+            }
+        }
+        TrieNode root;
+        public Trie () {
+            root = new TrieNode();
+        }
+        public void insert(String word){
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                if (node.children[c - 'a'] == null){
+                    node.children[c - 'a'] = new TrieNode();
+                }
+                node = node.children[c - 'a'];
+            }
+            node.isEnd = true;
+        }
+        public boolean startWith(String word){
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                if (node.children[c - 'a'] == null){
+                    return false;
+                }
+                node = node.children[c - 'a'];
+            }
+            return true;
+        }
+        public boolean search(String word){
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++){
+                char c = word.charAt(i);
+                if (node.children[c - 'a'] == null){
+                    return false;
+                }
+                node = node.children[c - 'a'];
+            }
+            return node.isEnd;
+        }
+    }
