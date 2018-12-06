1187fe9a62e51ba6d98ab22ceea976478196c9f7
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-06 10:43:37 -0600

    Update 76_LongestIncreasingSubsequence.java

--- Java/76_LongestIncreasingSubsequence.java
+++ Java/76_LongestIncreasingSubsequence.java
@@ -41,3 +41,22 @@ public class Solution {
         return result;
     }
 }
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            dp[i] = 1;
+            for (int j = 0; j < i; j++){
+                if (nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i : dp){
+            max = Math.max(max, i);
+        }
+        return max;
+    }
+}
