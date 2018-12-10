27d8a416386b78bc5c7a1b9113c5609b642d48df
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-09 22:00:40 -0600

    Update 651_Keys.java

--- Java/651_Keys.java
+++ Java/651_Keys.java
@@ -64,8 +64,8 @@ class Solution {
         int[] dp = new int[N+1];
         for (int i = 1; i <= N; i++){
             dp[i] = i;
-            for (int j = 1; j <= N - 3; j++){
-                dp[i] = Math.max(dp[i], dp[j] * (i-j-1));
+            for (int j = 1; j <= i - 3; j++){
+                dp[i] = Math.max(dp[i], dp[j] * (i-2-j+1));
             }
         }
         return dp[N];
