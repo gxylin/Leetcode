9a3413ba3ef89e3140a3854486d777e27409ccab
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-09 21:25:12 -0600

    Update 650_Keys.java

--- Java/650_Keys.java
+++ Java/650_Keys.java
@@ -50,3 +50,16 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int minSteps(int n) {
+        int ans = 0;
+        for (int i = 2; i <= n; i++){
+            while (n % i == 0){
+                ans += i;
+                n /= i;
+            }
+        }
+        return ans;
+    }
+}
