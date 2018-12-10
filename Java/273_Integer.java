399b5bef26bc4925dcd29ca101314a530ed3dc8e
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-10 17:31:06 -0600

    Update 273_Integer.java

--- Java/273_Integer.java
+++ Java/273_Integer.java
@@ -18,12 +18,8 @@ Input: 1234567891
 Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
 
 class Solution {
-    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
-                                                       "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
-                                                        "Eighty", "Ninety"};
-    
+    private final String[] lessTwenty = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy","Eighty", "Ninety"};
     public String numberToWords(int num) {
         if (num == 0){
             return "Zero";
@@ -31,21 +27,19 @@ class Solution {
         return helper(num);
     }
     private String helper(int num){
-        String result = new String();
-        if (num < 10){
-            result = belowTen[num];
-        }else if (num < 20){
-            result = belowTwenty[num - 10];
+        String result = "";
+        if (num < 20){
+            result = lessTwenty[num];
         }else if (num < 100){
-            result =  belowHundred[num/10] + " " + helper(num%10);
+            result = tens[num/10] + " " + helper(num%10);
         }else if (num < 1000){
-            result =  helper(num/100) + " Hundred " + helper(num%100);
+            result = helper(num/100) + " Hundred " + helper(num%100);
         }else if (num < 1000000){
             result = helper(num/1000) + " Thousand " + helper(num%1000);
         }else if (num < 1000000000){
             result = helper(num/1000000) + " Million " + helper(num%1000000);
         }else{
-            result =  helper(num/1000000000) + " Billion " + helper(num%1000000000);
+            result = helper(num/1000000000) + " Billion " + helper(num%1000000000);
         }
         return result.trim();
     }
