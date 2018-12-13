cf239c5069d79d9779ac69df7da1a79eeb590380
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-12 18:25:07 -0600

    Update 10_RegularExpressionMatching.java

--- Java/10_RegularExpressionMatching.java
+++ Java/10_RegularExpressionMatching.java
@@ -129,3 +129,36 @@ class Solution {
         return dp[m][n];
     }
 }
+
+
+Best version to match the rationale
+dp[i][j] denotes match or not when take first i-th p from and first j-th from s
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = p.length();
+        int n = s.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++){
+            if (p.charAt(i-1) == '*'){
+                dp[i][0] = dp[i-1][0] || dp[i-2][0];
+            }
+        }
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (p.charAt(i-1) == s.charAt(j-1) || p.charAt(i-1) == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else if (p.charAt(i-1) == '*'){
+                    if (i > 1 && p.charAt(i-2) != s.charAt(j-1) && p.charAt(i-2) != '.'){
+                        dp[i][j] = dp[i-2][j];
+                    }else if (i > 1 && (p.charAt(i-2) == s.charAt(j-1) || p.charAt(i-2) == '.')){
+                        dp[i][j] = dp[i-2][j] || dp[i-2][j-1] || dp[i][j-1];
+                    }else{
+                        dp[i][j] = dp[i][j-1];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
