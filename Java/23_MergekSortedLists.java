e47244eb1ef60d8ae1371d3375b15b417c935db0
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-13 23:48:42 -0600

    Update 23_MergekSortedLists.java

--- Java/23_MergekSortedLists.java
+++ Java/23_MergekSortedLists.java
@@ -15,74 +15,70 @@ class Solution {
         if (lists == null || lists.length == 0){
             return null;
         }
-        return helper(lists, 0 , lists.length - 1);
+        return mergeSort(lists, 0, lists.length - 1);
     }
-    private ListNode helper(ListNode[] lists, int start, int end){
-        if (start >= end){
+    private ListNode mergeSort(ListNode[] lists, int start, int end){
+        if (start > end){
+            return null;
+        }
+        if (start == end){
             return lists[start];
         }
         int mid = start + (end - start) / 2;
-        ListNode left = helper(lists, start, mid);
-        ListNode right = helper(lists, mid + 1, end);
+        ListNode left = mergeSort(lists, start, mid);
+        ListNode right = mergeSort(lists, mid + 1, end);
         return merge(left, right);
     }
     private ListNode merge(ListNode l1, ListNode l2){
-        ListNode dummy = new ListNode(0);
-        ListNode lastNode = dummy;
+        if (l1 == null){
+            return l2;
+        }
+        if (l2 == null){
+            return l1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode node  = dummy;
         while (l1 != null && l2 != null){
-            if (l1.val > l2.val){
-                lastNode.next = l2;
-                l2 = l2.next;
-            }else{
-                lastNode.next = l1;
+            if (l1.val < l2.val){
+                node.next = l1;
                 l1 = l1.next;
+            }else{
+                node.next = l2;
+                l2 = l2.next;
             }
-            lastNode = lastNode.next;
+            node = node.next;
         }
-        if (l1 != null){
-            lastNode.next = l1;
+        if (l1 == null){
+            node.next = l2;
         }
-        if (l2 != null){
-            lastNode.next = l2;
+        if (l2 == null){
+            node.next = l1;
         }
         return dummy.next;
     }
-    
 }
 
 Method 2: heap priority queue
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0){
-            return null;
-        }
-        Queue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>(){
-            public int compare(ListNode a, ListNode b){
-                return a.val - b.val;
+        ListNode dummy = new ListNode(-1);
+        ListNode node = dummy;
+        Queue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>(){
+            public int compare (ListNode n1, ListNode n2){
+                return n1.val - n2.val;
             }
         });
-
-        for (int i = 0; i < lists.length; i++){
-            if (lists[i] != null){
-                pq.offer(lists[i]);
+        for (ListNode n : lists){
+            if (n != null){
+                pq.offer(n);
             }
         }
-        ListNode dummy = new ListNode(0);
-        ListNode lastNode = dummy;
         while (!pq.isEmpty()){
-            ListNode temp = pq.poll();
-            lastNode.next = temp;
-            lastNode = temp;
-            if (temp.next != null){
-               pq.offer(temp.next); 
+            ListNode curr = pq.poll();
+            node.next = curr;
+            node = node.next;
+            if (curr.next != null){
+                pq.offer(curr.next);
             }
         }
         return dummy.next;
