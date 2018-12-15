a332e536b8dd54305feda530379a1dbec5c39195
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-14 19:22:57 -0600

    Update 43_MultiplyStrings.java

--- Java/43_MultiplyStrings.java
+++ Java/43_MultiplyStrings.java
@@ -34,3 +34,45 @@ class Solution {
         return sb.toString();
     }
 }
+
+Better version:
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = 0;
+        int j = 0;
+        int prev = 0;
+        int len1 = num1.length();
+        int len2 = num2.length();
+        for (i = 0; i < len1; i++){
+            int c1 = (int)(num1.charAt(len1-1-i) - '0');
+            int digits = 0;
+            int num = 0;
+            carry = 0;
+            for (j = 0; j < len2; j++){
+                int c2 = (int)(num2.charAt(len2-1-j) - '0');
+                if (sb.length() == i+j){
+                    prev = 0;
+                }else{
+                    prev = (int)(sb.charAt(i+j) - '0');
+                }
+                digits = c1 * c2 + carry + prev;
+                num = digits%10;
+                carry = digits/10;
+                if (sb.length() == i+j){
+                    sb.append(num);
+                }else{
+                    sb.setCharAt(i+j, (char)(num + '0'));
+                }
+            }
+            if (carry != 0){
+                sb.append(carry);
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
