e7c2e9c99abce528bbaa13deda8ed710d964e65a
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-14 20:57:25 -0600

    Update 94_BinaryTreeInorderTraversal.java

--- Java/94_BinaryTreeInorderTraversal.java
+++ Java/94_BinaryTreeInorderTraversal.java
@@ -104,3 +104,4 @@ class Solution {
         return res;
     }
 }
+https://leetcode.com/problems/validate-binary-search-tree/discuss/32112/Learn-one-iterative-inorder-traversal-apply-it-to-multiple-tree-questions-(Java-Solution)
