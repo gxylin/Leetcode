e1854d5abb8a4267a4a8d9e3494063f978120dca
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-14 19:59:10 -0600

    Update 5_KthLargestElement.java

--- Java/5_KthLargestElement.java
+++ Java/5_KthLargestElement.java
@@ -150,3 +150,50 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int index = partition(nums, start, end);
+            if (index == k-1){
+                return nums[index]; 
+            }else if (index > k-1){
+                end = index-1;
+            }else{
+                start = index+1;
+            }
+        }
+        return nums[start];
+    }
+    private int partition(int[] nums, int start, int end){
+        Random random = new Random();
+        int index = start + random.nextInt(end - start + 1);
+        int pivot = nums[index];
+        swap(nums, index, start);
+        int head = start;
+        start++;
+        while (start <= end){
+            while (start <= end && nums[start] >= pivot){
+                start++;
+            }
+            while (start <= end && nums[end] < pivot){
+                end--;
+            }
+            if (start < end){
+                swap(nums, start, end);
+                start++;
+                end--;
+            }  
+        }
+        swap(nums, head, end);
+        return end;
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
