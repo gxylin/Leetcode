dd75e14959bf12478ee5a49db51ff2fad8b75cc0
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-17 22:02:52 -0600

    Update 523_Continuous.java

--- Java/523_Continuous.java
+++ Java/523_Continuous.java
@@ -37,3 +37,26 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < n; i++){
+            sum += nums[i];
+            if (k != 0){
+                sum %= k;
+            }
+            if (map.containsKey(sum)){
+                if (i - map.get(sum) > 1){
+                    return true;
+                }
+            }else{
+                map.put(sum, i);
+            }
+        }
+        return false;
+    }
+}
