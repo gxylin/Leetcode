d2b4970e778ee19386d7bc929b9196c5be9cb31d
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-19 00:34:23 -0600

    Update 416_Partition.java

--- Java/416_Partition.java
+++ Java/416_Partition.java
@@ -58,6 +58,32 @@ class Solution {
         return dp[nums.length][target];
     }
 }
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int i : nums){
+            sum += i;
+        }
+        if (sum%2 != 0){
+            return false;
+        }
+        int target =sum / 2;
+        int n = nums.length;
+        boolean[][] dp = new boolean[n+1][target+1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= target; j++){
+                if (j >= nums[i-1]){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+            }
+        }
+        return dp[n][target];
+    }
+}
 
 Method 2: 滚动数组优化
 class Solution {
