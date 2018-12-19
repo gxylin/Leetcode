08d0467c7c2aa0d3907f20c680e6f63debf267a8
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-19 00:31:17 -0600

    Update 416_Partition.java

--- Java/416_Partition.java
+++ Java/416_Partition.java
@@ -87,6 +87,30 @@ class Solution {
     }
 }
 
+Best solution: Reduce space complexity
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int i : nums){
+            sum += i;
+        }
+        if (sum%2 != 0){
+            return false;
+        }
+        int target =sum / 2;
+        int n = nums.length;
+        boolean[] dp = new boolean[target+1];
+        dp[0] = true;
+        for (int i = 0; i < nums.length; i++){
+            for (int j = target; j >= nums[i]; j--){
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[target];
+    }
+}
+
+
 
 Method 2: backtracking 
 
