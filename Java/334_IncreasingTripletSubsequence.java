f7ad7cc31b807fc49763832e95a16961e8667ea9
optimisea <34330974+optimisea@users.noreply.github.com>
2018-12-27 13:41:06 +0800

    Update 334_IncreasingTripletSubsequence.java

--- Java/334_IncreasingTripletSubsequence.java
+++ Java/334_IncreasingTripletSubsequence.java
@@ -32,3 +32,20 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        for (int num : nums){
+            if (num < first){
+                first = num;
+            }else if (num != first && num < second){
+                second = num;
+            }else if (num != first && num != second){
+                return true;
+            }
+        }
+        return false;
+    }
+}
