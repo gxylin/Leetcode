7a7a698ff6d9c75938005f0ae9cd24a9858c8692
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-14 13:19:32 -0600

    Update 178_GraphValidTree.java

--- Java/178_GraphValidTree.java
+++ Java/178_GraphValidTree.java
@@ -5,6 +5,9 @@ these edges make up a valid tree.
 You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is 
 the same as [1, 0] and thus will not appear together in edges.
 
+这道题给了我们一个无向图，让我们来判断其是否为一棵树，我们知道如果是树的话，所有的节点必须是连接的，也就是说必须是连通图，而且不能有环，
+所以我们的焦点就变成了验证是否是连通图和是否含有环。
+
 public class Solution {
     /*
      * @param n: An integer
