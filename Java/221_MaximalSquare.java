ec23ba3c3354da9fc1c66bfa111f533034f85bb7
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-14 15:48:20 -0600

    Update 221_MaximalSquare.java

--- Java/221_MaximalSquare.java
+++ Java/221_MaximalSquare.java
@@ -11,31 +11,37 @@ Return 4.
 Method 1: 2D dynamic programming
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0){
+        if (matrix == null || matrix.length == 0){
             return 0;
         }
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] f = new int[m][n];
-        int maxSide = 0;
+        int[][] dp = new int[m][n];//denote the max square side ending at i, j
+        int max = 0;
         for (int i = 0; i < m; i++){
-            f[i][0] = matrix[i][0] - '0';
-            maxSide = Math.max(maxSide, f[i][0]);
+            dp[i][0] = (int)(matrix[i][0] - '0');
+            max = Math.max(max, dp[i][0]);
         }
         for (int j = 0; j < n; j++){
-            f[0][j] = matrix[0][j] - '0';
-            maxSide = Math.max(maxSide, f[0][j]);
+            dp[0][j] = (int)(matrix[0][j] - '0');
+            max = Math.max(max, dp[0][j]);
         }
+       
         for (int i = 1; i < m; i++){
             for (int j = 1; j < n; j++){
                 if (matrix[i][j] == '1'){
-                    f[i][j] = Math.min(f[i-1][j-1], Math.min(f[i-1][j], f[i][j-1])) + 1;
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    max = Math.max(max, dp[i][j]);
                 }else{
-                    f[i][j] = 0;
+                    dp[i][j] = 0;
                 }
-                maxSide = Math.max(maxSide, f[i][j]);
+                
             }
         }
-        return maxSide * maxSide;
+        return max * max;
     }
 }
+
+Why pick Math.min
+https://www.youtube.com/watch?v=aYnEO53H4lw&t=247s
+
