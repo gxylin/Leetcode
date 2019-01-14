f137256d2dabac24b665674aa42794fe7b908661
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-14 12:43:54 -0600

    Update 178_GraphValidTree.java

--- Java/178_GraphValidTree.java
+++ Java/178_GraphValidTree.java
@@ -1,5 +1,5 @@
-Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these 
-edges make up a valid tree.
+Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether
+these edges make up a valid tree.
 
  Notice
 You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is 
