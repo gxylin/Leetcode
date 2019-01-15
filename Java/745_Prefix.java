d6270ad3a395ce575f78bf6f690d4d7b7cbbeab4
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-15 11:51:02 -0600

    Update 745_Prefix.java

--- Java/745_Prefix.java
+++ Java/745_Prefix.java
@@ -15,9 +15,11 @@ prefix, suffix have lengths in range [0, 10].
 words[i] and prefix, suffix queries consist of lowercase letters only.
 
 Method 1: Trie of Suffix Wrapped Words
-Consider the word 'apple'. For each suffix of the word, we could insert that suffix, followed by '#', followed by the word, all into the trie.
+Consider the word 'apple'. For each suffix of the word, we could insert that suffix, followed by '#', followed by the word,
+all into the trie.
 
-For example, we will insert '#apple', 'e#apple', 'le#apple', 'ple#apple', 'pple#apple', 'apple#apple' into the trie. Then for a query like prefix = "ap", suffix = "le", we can find it by querying our trie for le#ap.
+For example, we will insert '#apple', 'e#apple', 'le#apple', 'ple#apple', 'pple#apple', 'apple#apple' into the trie.
+Then for a query like prefix = "ap", suffix = "le", we can find it by querying our trie for le#ap.
 
 Time Complexity: O(NK^2 + QK) where N is the number of words, K is the maximum length of a word, and Q is the number of queries.
 
