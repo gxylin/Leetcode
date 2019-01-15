99c48cb6788c400394dfdf117a2682936584e1ca
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-14 18:48:25 -0600

    Update 404_Sum.java

--- Java/404_Sum.java
+++ Java/404_Sum.java
@@ -62,3 +62,37 @@ class Solution {
         return sum;
     }
 }
+
+Method 3:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int sum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            TreeNode curr = queue.poll();
+            if (curr.left != null){
+                queue.offer(curr.left);
+                if (curr.left.left == null && curr.left.right == null){
+                    sum += curr.left.val;
+                }
+            }
+            if (curr.right != null){
+                queue.offer(curr.right);
+            }
+        }
+        return sum;
+    }
+}
