0b1b075955b4ef2ff05515826d75eb0666205196
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-17 23:00:22 -0600

    Update 6_ZigZagConversion.java

--- Java/6_ZigZagConversion.java
+++ Java/6_ZigZagConversion.java
@@ -33,3 +33,24 @@ class Solution {
         return ans;
     }
 }
+
+Best solution:
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1){
+            return s;
+        }
+        StringBuilder res = new StringBuilder();
+        int steps = 2 * numRows - 2;
+        int n = s.length();
+        for (int i = 0; i < numRows; i++){
+            for (int j = 0; j + i < n; j += steps){
+                res.append(s.charAt(j+i));
+                if (i!= 0 && i != numRows - 1 && j + steps - i < n){
+                    res.append(s.charAt(j + steps - i));
+                }
+            }
+        }
+        return res.toString();
+    }
+}
