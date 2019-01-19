845b5e38db403026d23b69f14fcb5b0095e59c7e
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-18 21:49:39 -0600

    Update 113_PathSumII.java

--- Java/113_PathSumII.java
+++ Java/113_PathSumII.java
@@ -86,3 +86,39 @@ class Solution {
         item.remove(item.size() - 1);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        dfs(res, new ArrayList<Integer>(), root, sum);
+        return res;
+    }
+    private void dfs(List<List<Integer>> res, List<Integer> item, TreeNode root, int sum){
+        if (root.val == sum && root.left == null && root.right == null){
+            item.add(root.val);
+            res.add(new ArrayList<>(item));
+            item.remove(item.size() - 1);
+            return;
+        }
+        item.add(root.val);
+        if (root.left != null){
+            dfs(res, item, root.left, sum - root.val);
+        }
+        if (root.right != null){
+            dfs(res, item, root.right, sum - root.val);
+        }
+        item.remove(item.size() - 1);
+    }
+}
