1ee366c25395e160e34d6d07662dd337bcc1f47e
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-20 09:31:22 -0600

    Create 979_Distribute.java

---
+++ Java/979_Distribute.java
@@ -0,0 +1,23 @@
+Given the root of a binary tree with N nodes, each node in the tree has node.val coins, and there are N coins total.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another.  (The move may be from parent to child, or from child to parent.)
+
+Return the number of moves required to make every node have exactly one coin.
+
+
+class Solution {
+    int res = 0;
+    public int distributeCoins(TreeNode root) {
+        coinGivenToParent(root);
+        return res;
+    }
+    private int coinGivenToParent(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        int left = coinGivenToParent(root.left);
+        int right = coinGivenToParent(root.right);
+        res += Math.abs(left) + Math.abs(right);
+        return left + right + root.val - 1;
+    }
+}
