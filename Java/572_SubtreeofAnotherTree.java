fccb527e22bbd0a006b6510b7b4311d5e9eb0ee1
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-21 14:07:53 -0600

    Update 572_SubtreeofAnotherTree.java

--- Java/572_SubtreeofAnotherTree.java
+++ Java/572_SubtreeofAnotherTree.java
@@ -66,3 +66,37 @@ class Solution {
         return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
     }
 }
+
+
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null){
+            return true;
+        }
+        if (s == null && t != null){
+            return false;
+        }
+        if (s != null && t == null){
+            return false;
+        }
+        if (s.val != t.val){
+            return isSubtree(s.left, t) || isSubtree(s.right, t);
+        }
+        return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    private boolean isSame(TreeNode s, TreeNode t){
+        if (s == null && t == null){
+            return true;
+        }
+        if (s == null && t != null){
+            return false;
+        }
+        if (s != null && t == null){
+            return false;
+        }
+        if (s.val != t.val){
+            return false;
+        }
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
