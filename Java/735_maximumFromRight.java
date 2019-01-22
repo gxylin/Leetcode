49fe00565fb730f376ca048f328a3e6b0477e060
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-22 16:03:48 -0600

    Update 735_maximumFromRight.java

--- Java/735_maximumFromRight.java
+++ Java/735_maximumFromRight.java
@@ -1,5 +1,6 @@
 Given an array of integers, replace every element with the next greatest element
-(greatest element on the right side) in the array. Since there is no element next to the last element, replace it with -1. For example, if the array is [16, 17, 4, 3, 5, 2], then it should be modified to [17, 5, 5, 5, 2, -1].
+(greatest element on the right side) in the array. Since there is no element next to the last element, replace it with -1. For example, 
+if the array is [16, 17, 4, 3, 5, 2], then it should be modified to [17, 5, 5, 5, 2, -1].
 
 Have you met this question in a real interview? Yes
 Example
