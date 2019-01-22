8bcc3da55b649d63f209d69d3a34839ac317bad3
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-22 13:46:21 -0600

    Update 279_PerfectSquares.java

--- Java/279_PerfectSquares.java
+++ Java/279_PerfectSquares.java
@@ -18,3 +18,19 @@ class Solution {
         return dp[n];
     }
 }
+
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j * j <= i; j++){
+                if (dp[i-j*j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i-j*j] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
