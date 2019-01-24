88370e03ae1d63e6ebf1c84899000691cb0e9653
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-24 09:57:52 -0600

    Update 646_MaximumLengthofPairChain.java

--- Java/646_MaximumLengthofPairChain.java
+++ Java/646_MaximumLengthofPairChain.java
@@ -43,3 +43,26 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, new Comparator<int[]>(){
+            public int compare (int[] p1, int[] p2){
+                return p1[0] - p2[0];
+            }
+        });
+        int n = pairs.length;
+        int[] dp = new int[n];
+        int max = 1;
+        for (int i = 0; i < n; i++){
+            dp[i] = 1;
+            for (int j = 0; j < i; j++){
+                if (pairs[j][1] < pairs[i][0]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
