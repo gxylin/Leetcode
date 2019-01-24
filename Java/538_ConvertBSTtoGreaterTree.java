5c983eb5a78427137e99d09ee6d2608422407e9c
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-23 22:50:32 -0600

    Update 538_ConvertBSTtoGreaterTree.java

--- Java/538_ConvertBSTtoGreaterTree.java
+++ Java/538_ConvertBSTtoGreaterTree.java
@@ -39,3 +39,29 @@ class Solution {
         return left;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+    private void dfs(TreeNode root){
+        if (root == null){
+            return;
+        }
+        dfs(root.right);
+        sum += root.val;
+        root.val = sum;
+        dfs(root.left);
+    }
+}
