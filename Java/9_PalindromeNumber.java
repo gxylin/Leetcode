f13047b514c71676ef955ed6b48305a1b4c9b446
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-27 17:11:14 -0600

    Update 9_PalindromeNumber.java

--- Java/9_PalindromeNumber.java
+++ Java/9_PalindromeNumber.java
@@ -30,3 +30,19 @@ class Solution {
         return x == reverse || x == reverse / 10;
     }
 }
+
+Better version:
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        int reverse = 0;
+        int orig = x;
+        while (x > 0){
+            reverse = reverse * 10 + x%10;
+            x /= 10;
+        }
+        return reverse == orig;
+    }
+}
