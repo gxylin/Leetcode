dbf6654bd599c9fd9648a1b9d3e967c2c040cb09
optimisea <34330974+optimisea@users.noreply.github.com>
2019-01-29 19:22:35 -0600

    Update 511_SwapTwoNodesLinkedList.java

--- Java/511_SwapTwoNodesLinkedList.java
+++ Java/511_SwapTwoNodesLinkedList.java
@@ -61,3 +61,30 @@ public class Solution {
        return dummy.next;
     }
 }
+
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = head;
+        ListNode next = head.next;
+       
+        while (cur != null && next != null){
+            ListNode temp = next.next;
+            next.next = cur;
+            cur.next = temp;
+            prev.next = next;
+            prev = cur;
+            cur = temp;
+            if (temp != null){
+                next= temp.next;
+            }
+        }
+        return dummy.next;
+    }
+}
