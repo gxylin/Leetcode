8f225fb7fddb6e3635879a1294bdefb830212b57
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-04 23:28:45 -0600

    Update 417_Pacific.java

--- Java/417_Pacific.java
+++ Java/417_Pacific.java
@@ -23,7 +23,8 @@ Return:
 
 [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
 
- Method 1: time complexity: O(N^2 * 2^N)
+ Method 1: Forward Travel
+ Time complexity: O(N^2 * 2^N)
 class Solution {
     public List<int[]> pacificAtlantic(int[][] matrix) {
         List<int[]> result = new ArrayList<>();
@@ -127,7 +128,9 @@ class Solution {
     }
 }
 
-Method 2: O(N^2 + 2^N) Better solution (Good)
+Method 2: Best solution: Backward travel
+
+Time complexity: O(N^2 + 2^N) 
 class Solution {
     public List<int[]> pacificAtlantic(int[][] matrix) {
         List<int[]> res = new ArrayList<>();
