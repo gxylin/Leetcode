7245d401bf182befc1991201e6400470161707f4
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-04 23:25:02 -0600

    Update 417_Pacific.java

--- Java/417_Pacific.java
+++ Java/417_Pacific.java
@@ -23,6 +23,7 @@ Return:
 
 [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
 
+ Method 1: time complexity: O(N^2 * 2^N)
 class Solution {
     public List<int[]> pacificAtlantic(int[][] matrix) {
         List<int[]> result = new ArrayList<>();
@@ -125,3 +126,59 @@ class Solution {
         return false;
     }
 }
+
+Method 2: O(N^2 + 2^N) Better solution (Good)
+class Solution {
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0){
+            return res;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        Queue<int[]> paQueue = new LinkedList<>();
+        Queue<int[]> atQueue = new LinkedList<>();
+        for (int i = 0; i < m; i++){
+            pacific[i][0] = true;
+            paQueue.offer(new int[]{i, 0});
+            atlantic[i][n-1] = true;
+            atQueue.offer(new int[]{i, n-1});
+        }
+        for (int j = 0; j < n; j++){
+            pacific[0][j] = true;
+            paQueue.offer(new int[]{0, j});
+            atlantic[m-1][j] = true;
+            atQueue.offer(new int[]{m-1, j});
+        }
+        bfs(matrix, paQueue, pacific);
+        bfs(matrix, atQueue, atlantic);
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (pacific[i][j] && atlantic[i][j]){
+                    res.add(new int[]{i, j});
+                }
+            }
+        }
+        return res;
+    }
+    private void bfs(int[][] matrix, Queue<int[]> queue, boolean[][] visited){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        while (!queue.isEmpty()){
+            int[] curr = queue.poll();
+            for (int[] dir : dirs){
+                int nx = curr[0] + dir[0];
+                int ny = curr[1] + dir[1];
+                if (nx >= 0 && nx < m && ny >=0 && ny < n && !visited[nx][ny] && matrix[nx][ny] >= matrix[curr[0]][curr[1]]){ 
+                  //note that here is >= , not < because we are doing backward travel not like method 1 which is forward travel
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+}
+  
