4e6b16c775eae54bcaf580326a7ce0dfdb53f638
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-05 16:43:47 -0600

    Update 447_Number.java

--- Java/447_Number.java
+++ Java/447_Number.java
@@ -36,3 +36,24 @@ class Solution {
         return count;
     }
 }
+
+
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        int count = 0;
+        for (int[] p : points){
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int[] q : points){
+                if (p[0] == q[0] && p[1] == q[1]){
+                    continue;
+                }
+                int dist = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                map.put(dist, map.getOrDefault(dist, 0) + 1);
+            }
+            for (int val : map.values()){
+                count += val * (val - 1);
+            }
+        }
+        return count;
+    }
+}
