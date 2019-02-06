44e5dde96fe38b64dc1519cc526939dbe9a8b275
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-06 14:03:57 -0600

    Update 443_String.java

--- Java/443_String.java
+++ Java/443_String.java
@@ -161,7 +161,7 @@ class Solution {
 }
 
 
-Method 4:
+Method 4: 
 class Solution {
     public int compress(char[] chars) {
         boolean newchar = true;
@@ -186,6 +186,32 @@ class Solution {
                     chars[index++] = str.charAt(j);
                 }
                 newchar = true;
+            }
+        }
+        return index;
+    }
+}
+
+
+
+Best solution: in place
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;
+        int i = 0;
+        while (i < chars.length){
+            char curr = chars[i];
+            int count = 0;
+            while (i < chars.length && chars[i] == curr){
+                count++;
+                i++;
+            }
+            chars[index++] = curr;
+            if (count > 1){
+                String str = String.valueOf(count);
+                for (char c : str.toCharArray()){
+                    chars[index++] = c;
+                }
             }
         }
         return index;
