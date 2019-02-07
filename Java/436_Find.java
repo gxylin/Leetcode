cae5b522bf42876479cbd1d69da75efc4af33abb
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-06 23:21:04 -0600

    Update 436_Find.java

--- Java/436_Find.java
+++ Java/436_Find.java
@@ -144,3 +144,34 @@ class Solution {
         return result;
     }
 }
+
+
+Best solution:
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int[] findRightInterval(Interval[] intervals) {
+        int n = intervals.length;
+        int[] res = new int[n];
+        TreeMap<Integer, Integer> treemap = new TreeMap<>();
+        for (int i = 0; i < n; i++){
+            treemap.put(intervals[i].start, i);
+        }
+        for (int i = 0; i < n; i++){
+            Integer key = treemap.ceilingKey(intervals[i].end);
+            if (key == null){
+                res[i] = -1;
+            }else{
+                res[i] = treemap.get(key);
+            }
+        }
+        return res;
+    }
+}
