0daac574ae9a3d757bfa412fbb3588ac85575613
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-06 21:29:43 -0600

    Update 540_Single.java

--- Java/540_Single.java
+++ Java/540_Single.java
@@ -68,5 +68,37 @@ class Solution {
         return nums[start];
     }
 }
+
+Better solution:
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end){
+            int mid = start + (end - start) / 2;
+            int left = nums[mid-1];
+            int right = nums[mid+1];
+            if (nums[mid] != left && nums[mid] != right){
+                return nums[mid];
+            }
+            if (mid % 2 == 0){
+                if (nums[mid] == nums[mid-1]){
+                    end = mid -1;
+                }else if (nums[mid] == nums[mid+1]){
+                    start = mid + 1;
+                }
+            }else{
+                if (nums[mid] == nums[mid-1]){
+                    start = mid + 1;
+                }else if (nums[mid] == nums[mid+1]){
+                    end = mid - 1;
+                }
+            }
+        }
+        return nums[start];
+    }
+}
+
+
 Method 3
 https://leetcode.com/problems/single-element-in-a-sorted-array/discuss/100754/Java-Binary-Search-short-(7l)-O(log(n))-w-explanations
