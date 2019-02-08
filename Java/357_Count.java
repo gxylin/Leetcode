039bb5abe6223e3a9734210be3dff0e5f4761c30
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-08 16:39:29 -0600

    Update 357_Count.java

--- Java/357_Count.java
+++ Java/357_Count.java
@@ -7,9 +7,11 @@ Following the hint. Let f(n) = count of number with unique digits of length n.
 
 f(1) = 10. (0, 1, 2, 3, ...., 9)
 
-f(2) = 9 * 9. Because for each number i from 1, ..., 9, we can pick j to form a 2-digit number ij and there are 9 numbers that are different from i for j to choose from.
+f(2) = 9 * 9. Because for each number i from 1, ..., 9, we can pick j to form a 2-digit number ij and there are 9 numbers that
+are different from i for j to choose from.
 
-f(3) = f(2) * 8 = 9 * 9 * 8. Because for each number with unique digits of length 2, say ij, we can pick k to form a 3 digit number ijk and there are 8 numbers that are different from i and j for k to choose from.
+f(3) = f(2) * 8 = 9 * 9 * 8. Because for each number with unique digits of length 2, say ij, we can pick k to form a 3 digit number
+ijk and there are 8 numbers that are different from i and j for k to choose from.
 
 Similarly f(4) = f(3) * 7 = 9 * 9 * 8 * 7....
 
