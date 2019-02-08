b7bb4f7555b14aea67e140565d0fe606ac0359a4
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-07 22:07:24 -0600

    Update 132_Palindrome.java

--- Java/132_Palindrome.java
+++ Java/132_Palindrome.java
@@ -11,6 +11,8 @@ https://leetcode.com/problems/palindrome-partitioning-ii/discuss/42213/Easiest-J
 
 
 Similar as Longest Increasing Sequence
+Time complexity: O(n^2)
+Space complexity: O(n^2)
 class Solution {
     public int minCut(String s) {
         int n = s.length();
@@ -26,5 +28,93 @@ class Solution {
             }
         }
         return cut[n-1];
+    }
+}
+
+https://leetcode.com/problems/palindrome-partitioning-ii/discuss/42213/Easiest-Java-DP-Solution-(97.36)
+This can be solved by two points:
+
+cut[i] is the minimum of cut[j - 1] + 1 (j <= i), if [j, i] is palindrome.
+If [j, i] is palindrome, [j + 1, i - 1] is palindrome, and c[j] == c[i].
+The 2nd point reminds us of using dp (caching).
+
+a   b   a   |   c  c
+                j  i
+       j-1  |  [j, i] is palindrome
+   cut(j-1) +  1
+    
+    
+Best solution:
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        boolean[][] cache = new boolean[n][n];
+        for (int i = 1; i <= n; i++){
+            dp[i] = i-1;
+            for (int j = 0; j < i; j++){
+                if ((j + 1 >= i - 2 || cache[j+1][i-2]) && s.charAt(i-1) == s.charAt(j)){
+                    cache[j][i-1] = true;
+                    dp[i] = j == 0 ? 0 : Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+
+
+Method 2:
+Time complexity: O(n^3)
+Space complexity: O(n) 
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++){
+            dp[i] = i;
+            for (int j = 0; j <= i; j++){
+                if (isValid(s, j, i)){
+                    dp[i] = j == 0 ? 0 : Math.min(dp[i], dp[j-1] + 1);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+    private boolean isValid(String s, int j, int i){
+        while (j < i){
+            if (s.charAt(j) != s.charAt(i)){
+                return false;
+            }
+            j++;
+            i--;
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= n; i++){
+            dp[i] = i-1;
+            for (int j = 0; j < i; j++){
+                if (isValid(s, j, i-1)){
+                    dp[i] = j == 0 ? 0 : Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+    private boolean isValid(String s, int j, int i){
+        while (j < i){
+            if (s.charAt(j) != s.charAt(i)){
+                return false;
+            }
+            j++;
+            i--;
+        }
+        return true;
     }
 }
