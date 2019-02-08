ec1b4e3fa2a8f15342ae48c054d6d70d1b275b91
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-08 17:08:36 -0600

    Update 360_Sort.java

--- Java/360_Sort.java
+++ Java/360_Sort.java
@@ -1,4 +1,5 @@
- Given a sorted array of integers nums and integer values a, b and c. Apply a quadratic function of the form f(x) = ax2 + bx + c to each element x in the array.
+ Given a sorted array of integers nums and integer values a, b and c. Apply a quadratic function of the form f(x) = ax2 + bx + c to 
+ each element x in the array.
 
 The returned array must be in sorted order.
 
