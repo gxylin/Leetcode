7b524bcd39053e48e214021bb0ff453ba4835894
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-08 23:47:31 -0600

    Update 408_Valid.java

--- Java/408_Valid.java
+++ Java/408_Valid.java
@@ -66,3 +66,29 @@ class Solution {
         return i == word.length() && j == abbr.length();
     }
     
+
+Better solution:
+ public boolean validWordAbbreviation(String word, String abbr){
+     int i = 0;
+     int j = 0;
+     while (i < word.length() && j < abbr.length()){
+        if (Character.isDigit(abbr.charAt(j)){
+           if (abbr.charAt(j) == '0'){
+              return false;
+           }
+           int num = 0;
+           while (j < abbr.length() && Character.isDigit(abbr.charAt(j))){
+              num = 10 * num + (int)(abbr.charAt(j) - '0');
+              j++;
+           }
+           i += num;
+        }else{
+           if (word.charAt(i) != abbr.charAt(j)){
+              return false;
+           }
+           i++;
+           j++;
+        }
+     }
+     return i == word.length() && j == abbr.length();
+ }
