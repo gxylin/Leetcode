c395d5d0d2370eb3a58404b0bdbe49bad0b871ce
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-08 19:46:34 -0600

    Update 359_Logger.java

--- Java/359_Logger.java
+++ Java/359_Logger.java
@@ -28,7 +28,7 @@ logger.shouldPrintMessage(10,"foo"); returns false;
 // logging string "foo" at timestamp 11
 logger.shouldPrintMessage(11,"foo"); returns true;
 
-
+Method 1: for small data
 class Logger {
     Map<String, Integer> map;
     /** Initialize your data structure here. */
@@ -45,6 +45,41 @@ class Logger {
             return true;
         }
         return false;
+        
+    }
+}
+
+Method 2: for large data
+class Logger {
+    private int N;
+    private int[] times;
+    private Map<String, Integer> map;
+    /** Initialize your data structure here. */
+    public Logger() {
+        N = 10;
+        times = new int[N];
+        map = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+       int index = timestamp % 10;
+       if (!map.containsKey(message)){
+          map.put(message, index);
+          time[index] = timestamp;
+          return true;
+       }else{
+          int oldIndex = map.get(message);
+          if (timestamp - time[oldIndex] <= 10){
+              return false;
+          }else{//replace the index and don't care about the oldIndex content
+              time[index] = timestamp;
+              map.put(message, index);
+              return true;
+          }  
+       }
         
     }
 }
