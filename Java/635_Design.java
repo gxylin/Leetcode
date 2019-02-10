5f3a5e59df939146a4646e94e18877098d91b493
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-10 17:49:25 -0600

    Update 635_Design.java

--- Java/635_Design.java
+++ Java/635_Design.java
@@ -1,10 +1,14 @@
-You are given several logs that each log contains a unique id and timestamp. Timestamp is a string that has the following format: Year:Month:Day:Hour:Minute:Second, for example, 2017:01:01:23:59:59. All domains are zero-padded decimal numbers.
+You are given several logs that each log contains a unique id and timestamp. Timestamp is a string that has the following format: 
+Year:Month:Day:Hour:Minute:Second, for example, 2017:01:01:23:59:59. All domains are zero-padded decimal numbers.
 
 Design a log storage system to implement the following functions:
 
 void Put(int id, string timestamp): Given a log's unique id and timestamp, store the log in your storage system.
 
-int[] Retrieve(String start, String end, String granularity): Return the id of logs whose timestamps are within the range from start to end. Start and end all have the same format as timestamp. However, granularity means the time level for consideration. For example, start = "2017:01:01:23:59:59", end = "2017:01:02:23:59:59", granularity = "Day", it means that we need to find the logs within the range from Jan. 1st 2017 to Jan. 2nd 2017.
+int[] Retrieve(String start, String end, String granularity): Return the id of logs whose timestamps are within the range 
+from start to end. Start and end all have the same format as timestamp. However, granularity means the time level for consideration. 
+    For example, start = "2017:01:01:23:59:59", end = "2017:01:02:23:59:59", granularity = "Day", it means that we need to find the 
+    logs within the range from Jan. 1st 2017 to Jan. 2nd 2017.
 
 Example 1:
 
@@ -44,7 +48,8 @@ class LogSystem {
         int index = units.indexOf(gra);
         int pos = indices[index];
         for (String[] strArray : log){
-            if (strArray[1].substring(0, pos).compareTo(s.substring(0, pos)) >= 0 && strArray[1].substring(0, pos).compareTo(e.substring(0, pos)) <= 0){
+            String str = strArray[1].substring(0, pos);
+            if (str.compareTo(s.substring(0, pos)) >= 0 && str.compareTo(e.substring(0, pos)) <= 0){
                 res.add(Integer.parseInt(strArray[0]));
             }
         }
