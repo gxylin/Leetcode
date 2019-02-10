dcfba7f9821b4cf0951f1fd7c926a3b935f8f0d2
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-10 13:50:25 -0600

    Update 544_Output.java

--- Java/544_Output.java
+++ Java/544_Output.java
@@ -1,6 +1,11 @@
- During the NBA playoffs, we always arrange the rather strong team to play with the rather weak team, like make the rank 1 team play with the rank nth team, which is a good strategy to make the contest more interesting. Now, you're given n teams, you need to output their final contest matches in the form of a string.
+ During the NBA playoffs, we always arrange the rather strong team to play with the rather weak team, like make the rank 1 team play 
+ with the rank nth team, which is a good strategy to make the contest more interesting. Now, you're given n teams, you need to output 
+  their final contest matches in the form of a string.
 
-The n teams are given in the form of positive integers from 1 to n, which represents their initial rank. (Rank 1 is the strongest team and Rank n is the weakest team.) We'll use parentheses('(', ')') and commas(',') to represent the contest team pairing - parentheses('(' , ')') for pairing and commas(',') for partition. During the pairing process in each round, you always need to follow the strategy of making the rather strong one pair with the rather weak one.
+The n teams are given in the form of positive integers from 1 to n, which represents their initial rank. (Rank 1 is the strongest 
+team and Rank n is the weakest team.) We'll use parentheses('(', ')') and commas(',') to represent the contest team pairing - 
+  parentheses('(' , ')') for pairing and commas(',') for partition. During the pairing process in each round, you always need to 
+  follow the strategy of making the rather strong one pair with the rather weak one.
 
 Example 1:
 
@@ -60,23 +65,27 @@ class Solution {
 class Solution {
     public String findContestMatch(int n) {
         String[] team = new String[n];
-        for (int i = 1; i <= n; ++i)
-            team[i-1] = "" + i;
-
-        for (; n > 1; n /= 2)
-            for (int i = 0; i < n / 2; ++i)
-                team[i] = "(" + team[i] + "," + team[n-1-i] + ")";
-
+        for (int i = 0; i < n; ++i)
+            team[i] = "" + i+1;
+        int k = n;
+        while (k > 1){
+           for (int j = 0; j < k/2; j++){
+              team[j] = "(" + team[j] + "," + team[k-1-j] + ")";
+           }
+        }
         return team[0];
     }
 }
 
 Method 2:
-Let's try to solve the problem in linear time. We can treat this problem as two separate problems: outputting the correct sequence of parentheses and commas, and outputting the correct team number. With a little effort, one can be convinced that a linear time solution probably exists.
+Let's try to solve the problem in linear time. We can treat this problem as two separate problems: 
+ outputting the correct sequence of parentheses and commas, and outputting the correct team number.
+ With a little effort, one can be convinced that a linear time solution probably exists.
 
 Algorithm
 
-Let's focus on the parentheses first. We can use recursion to find the answer. For example, when N = 8, let R = log_2(N) = 3 be the number of rounds. The parentheses and commas look like this:
+Let's focus on the parentheses first. We can use recursion to find the answer. For example, when N = 8, let R = log_2(N) = 3 
+ be the number of rounds. The parentheses and commas look like this:
 
 (((x,x),(x,x)),((x,x),(x,x)))
 
