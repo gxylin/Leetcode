ff3f256585ab5e615b83eeadc0b6cb0d41eb2023
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-11 11:38:36 -0600

    Update 599_Minimum.java

--- Java/599_Minimum.java
+++ Java/599_Minimum.java
@@ -1,6 +1,7 @@
 Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
 
-You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output
+all of them with no order requirement. You could assume there always exists an answer.
 
 Example 1:
 Input:
@@ -60,4 +61,31 @@ public String[] findRestaurant(String[] list1, String[] list2) {
         }
     }
     return res.toArray(new String[res.size()]);
+}
+
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < list1.length; i++){
+            map.put(list1[i], i);
+        }
+        int min = Integer.MAX_VALUE;
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < list2.length; i++){
+            if (map.containsKey(list2[i])){
+                if (map.get(list2[i]) + i < min){
+                    min = map.get(list2[i]) + i;
+                    res.clear();
+                    res.add(list2[i]);
+                }else if (map.get(list2[i]) + i == min){
+                    res.add(list2[i]);
+                }
+            }
+        }
+        String[] result = new String[res.size()];
+        for (int i = 0; i < res.size(); i++){
+            result[i] = res.get(i);
+        }
+        return result;
+    }
 }
