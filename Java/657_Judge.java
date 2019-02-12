8ead8297399200824fe17fdf9d43229334bf804e
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-11 23:28:41 -0600

    Update 657_Judge.java

--- Java/657_Judge.java
+++ Java/657_Judge.java
@@ -28,3 +28,22 @@ class Solution {
         return x == 0 && y == 0;
     }
 }
+
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char c : moves.toCharArray()){
+            if (c == 'U'){
+                x--;
+            }else if (c == 'D'){
+                x++;
+            }else if (c == 'R'){
+                y++;
+            }else{
+                y--;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
