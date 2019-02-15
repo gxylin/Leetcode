012df5e69e1ff1c6ab3e9f5b2ba9862bbb3bb1ea
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-15 12:46:26 -0600

    Update 814_Binary.java

--- Java/814_Binary.java
+++ Java/814_Binary.java
@@ -72,3 +72,17 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null){
+            return root;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0){
+            root = null;
+        }
+        return root;
+    }
+}
