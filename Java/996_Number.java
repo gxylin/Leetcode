a0b63e7f0f8a848683597892487c11208568838a
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-16 22:53:18 -0600

    Create 996_Number.java

---
+++ Java/996_Number.java
@@ -0,0 +1,84 @@
+Given an array A of non-negative integers, the array is squareful if for every pair of adjacent elements, their sum is a perfect square.
+
+Return the number of permutations of A that are squareful.  Two permutations A1 and A2 differ if and only if there is some index i such that A1[i] != A2[i].
+
+ 
+
+Example 1:
+
+Input: [1,17,8]
+Output: 2
+Explanation: 
+[1,8,17] and [17,8,1] are the valid permutations.
+Example 2:
+
+Input: [2,2,2]
+Output: 1
+ 
+
+Note:
+
+1 <= A.length <= 12
+0 <= A[i] <= 1e9
+
+class Solution {
+    public int numSquarefulPerms(int[] A) {
+        List<List<Long>> res = new ArrayList<>();
+        long[] B = new long[A.length];
+        for (int i = 0; i < A.length; i++){
+            B[i] = (long)A[i];
+        }
+        boolean[] visited = new boolean[A.length];
+        Arrays.sort(B);
+        backtrack(res, new ArrayList<>(), B, visited);
+        return res.size();
+    }
+    private void backtrack(List<List<Long>> res, List<Long> item, long[] A, boolean[] visited){
+        if (item.size() == A.length){
+            res.add(new ArrayList<>(item));
+            return;
+        }
+        for (int i = 0; i < A.length; i++){
+            if (i > 0 && A[i] == A[i-1] && !visited[i-1]){
+                continue;
+            }
+            if (visited[i]){
+                continue;
+            }
+            if (item.size() == 0){
+                visited[i] = true;
+                item.add(A[i]);
+                backtrack(res, item, A, visited);
+                item.remove(item.size() - 1);
+                visited[i] = false;
+            }else{
+                long prev = item.get(item.size() - 1);
+                long cand = A[i]  + prev ;
+                if (isPerfectSquare(cand)){
+                    visited[i] = true;
+                    item.add(A[i]);
+                    backtrack(res, item, A, visited);
+                    item.remove(item.size() - 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+    private boolean isPerfectSquare(long num) {
+        long start = 0;
+        long end = num;
+        long target = num;
+        while (start <= end){
+            long mid = start + (end - start) / 2;
+            if (mid * mid == target){
+                return true;
+            }else if (mid * mid < target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        
+        return false;
+    }
+}
