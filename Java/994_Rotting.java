56fb414825b642d1156ba672c825dabb17321865
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-17 00:39:25 -0600

    Update 994_Rotting.java

--- Java/994_Rotting.java
+++ Java/994_Rotting.java
@@ -58,3 +58,48 @@ class Solution {
         return count;
     }
 }
+
+
+Better version:
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int freshNum = 0;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 2){
+                    queue.offer(new int[]{i, j});
+                }
+                if (grid[i][j] == 1){
+                    freshNum++;
+                }
+            }
+        }
+        if (freshNum == 0){
+            return 0;
+        }
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        int count = -1;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            count++;
+            for (int i = 0; i < size; i++){
+                int[] curr = queue.poll();
+                int x = curr[0];
+                int y = curr[1];
+                for (int[] dir : dirs){
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1){
+                        queue.offer(new int[]{nx, ny});
+                        grid[nx][ny] = 2;
+                        freshNum--;
+                    }
+                }
+            }
+        }
+        return freshNum == 0 ? count : -1;
+    }
+}
