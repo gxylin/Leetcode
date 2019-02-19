7b70a6ebc2271b742dbe930cbe06c8e100122ba4
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-19 16:18:38 -0600

    Update 852_Peak.java

--- Java/852_Peak.java
+++ Java/852_Peak.java
@@ -33,6 +33,28 @@ class Solution {
     }
 }
 
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if (A[mid] > A[mid-1] && A[mid] > A[mid+1]){
+                return mid;
+            }else if (A[mid] > A[mid-1] && A[mid] < A[mid+1]){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        if (A[start] > A[end]){
+            return start;
+        }
+        return end;
+        
+    }
+}
+
 Method 2:
 class Solution {
     public int peakIndexInMountainArray(int[] A) {
