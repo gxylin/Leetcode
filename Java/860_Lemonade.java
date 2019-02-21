43ac23ac70f8280ae6b7316be729d085e0da3faa
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-20 19:47:15 -0600

    Update 860_Lemonade.java

--- Java/860_Lemonade.java
+++ Java/860_Lemonade.java
@@ -65,3 +65,35 @@ class Solution {
         return true;
     }
 }
+
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int bill : bills){
+            if (bill == 5){
+                map.put(5, map.getOrDefault(5, 0) + 1);
+            }else if (bill == 10){
+                if (map.getOrDefault(5, 0) == 0){
+                    return false;
+                }
+                map.put(5, map.get(5) - 1);
+                map.put(10, map.getOrDefault(10, 0) + 1);
+            }else{
+                if (map.getOrDefault(5, 0) == 0){
+                    return false;
+                }
+                if (map.getOrDefault(10, 0) == 0){
+                    if (map.get(5) < 3){
+                        return false;
+                    }
+                    map.put(5, map.get(5) - 3);
+                }else{
+                    map.put(10, map.get(10) - 1);
+                    map.put(5, map.get(5) - 1);
+                }
+            }
+        }
+        return true;
+    }
+}
