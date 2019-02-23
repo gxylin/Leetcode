11a85945647c041ac023fa2311f55c0cf3b3e371
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-23 00:48:31 -0600

    Update 430_Flatten.java

--- Java/430_Flatten.java
+++ Java/430_Flatten.java
@@ -119,3 +119,50 @@ class Solution {
         return head;
     }
 }
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+    public Node() {}
+
+    public Node(int _val,Node _prev,Node _next,Node _child) {
+        val = _val;
+        prev = _prev;
+        next = _next;
+        child = _child;
+    }
+};
+*/
+class Solution {
+    public Node flatten(Node head) {
+        if (head == null){
+            return head;
+        }
+        Node curr = head;
+        while (curr != null){
+            if (curr.child == null){
+                curr = curr.next;
+                continue;
+            }
+            Node origin = curr;
+            Node temp = curr.next;
+            curr.next = curr.child;
+            curr.child.prev = curr;
+            curr.child = null;
+            while (curr.next != null){
+                curr = curr.next;
+            }
+            curr.next = temp;
+            if (temp != null){
+                temp.prev = curr;
+            }
+            curr = origin.next;
+        }
+        return head;
+    }
+}
