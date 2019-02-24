7c26056711b9d3aaa20f75fab11ae59988dd456b
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-24 00:24:02 -0600

    Create 998_Maximum.java

---
+++ Java/998_Maximum.java
@@ -0,0 +1,80 @@
+We are given the root node of a maximum tree: a tree where every node has a value greater than any other value in its subtree.
+
+Just as in the previous problem, the given tree was constructed from an list A (root = Construct(A)) recursively with the following Construct(A) routine:
+
+If A is empty, return null.
+Otherwise, let A[i] be the largest element of A.  Create a root node with value A[i].
+The left child of root will be Construct([A[0], A[1], ..., A[i-1]])
+The right child of root will be Construct([A[i+1], A[i+2], ..., A[A.length - 1]])
+Return root.
+Note that we were not given A directly, only a root node root = Construct(A).
+
+Suppose B is a copy of A with the value val appended to it.  It is guaranteed that B has unique values.
+
+Return Construct(B).
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null){
+            return new TreeNode(val);
+        }
+        return dfs(root, null, val);
+    }
+    private TreeNode dfs(TreeNode root, TreeNode parent, int val){
+        if (root == null){
+            return new TreeNode(val);
+        }
+        if (val > root.val){
+            TreeNode newRoot = new TreeNode(val);
+            if (parent == null){
+                newRoot.left = root;
+                return newRoot;
+            }
+            if (root == parent.left){
+                newRoot.left = root;
+                parent.left = newRoot;
+            }else {
+                newRoot.left = root;
+                parent.right = newRoot;
+            }
+            return newRoot;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+}
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null){
+            return new TreeNode(val);
+        }
+        if (root.val < val){
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+}
