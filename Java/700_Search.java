0a536d9e104b754a8c3b332354c734439f0b1d95
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-24 15:26:02 -0600

    Update 700_Search.java

--- Java/700_Search.java
+++ Java/700_Search.java
@@ -63,3 +63,25 @@ class Solution {
         return root;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val){
+            return root;
+        }
+        if (root.val > val){
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+    }
+}
