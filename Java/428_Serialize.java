343017e00277a1ab51fccf58bc78d137099ffc93
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-25 15:41:54 -0600

    Update 428_Serialize.java

--- Java/428_Serialize.java
+++ Java/428_Serialize.java
@@ -1,10 +1,14 @@
-Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or 
+memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
 
-Design an algorithm to serialize and deserialize an N-ary tree. An N-ary tree is a rooted tree in which each node has no more than N children. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that an N-ary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+Design an algorithm to serialize and deserialize an N-ary tree. An N-ary tree is a rooted tree in which each node has no more than 
+N children. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that 
+an N-ary tree can be serialized to a string and this string can be deserialized to the original tree structure.
 
 For example, you may serialize the following 3-ary tree
 
-as [1 [3[5 6] 2 4]]. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+as [1 [3[5 6] 2 4]]. You do not necessarily need to follow this format, so please be creative and come up with different approaches 
+yourself.
 
  
 
@@ -86,8 +90,11 @@ class Codec {
     }
 
     private void serializePreOrder(Node root, StringBuilder sb){
-        sb.append(root.val);
-        sb.append(root.children.size());
+        if (root == null){
+          return;
+        }
+        sb.append(root.val + ",");
+        sb.append(root.children.size(), ",");
         for (Node node : root.children){
             serializePreOrder(node, sb);
         }
