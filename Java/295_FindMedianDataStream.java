6a4a60cc4c8ad7f9884550202f479ba29f6a7698
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-25 21:23:12 -0600

    Update 295_FindMedianDataStream.java

--- Java/295_FindMedianDataStream.java
+++ Java/295_FindMedianDataStream.java
@@ -121,12 +121,9 @@ class MedianFinder {
     }
 }
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
+
+
+Better version:
 
 
 class MedianFinder {
