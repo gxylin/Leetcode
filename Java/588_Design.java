b883b8915195d2fb9ad717e60bfd1ab2876f694b
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-26 15:20:22 -0600

    Update 588_Design.java

--- Java/588_Design.java
+++ Java/588_Design.java
@@ -1,10 +1,15 @@
 Design an in-memory file system to simulate the following functions:
 
-ls: Given a path in string format. If it is a file path, return a list that only contains this file's name. If it is a directory path, return the list of file and directory names in this directory. Your output (file and directory names together) should in lexicographic order.
+ls: Given a path in string format. If it is a file path, return a list that only contains this file's name. If it is a directory path, 
+return the list of file and directory names in this directory. Your output (file and directory names together) should in lexicographic 
+order.
 
-mkdir: Given a directory path that does not exist, you should make a new directory according to the path. If the middle directories in the path don't exist either, you should create them as well. This function has void return type.
+mkdir: Given a directory path that does not exist, you should make a new directory according to the path. If the middle directories
+in the path don't exist either, you should create them as well. This function has void return type.
 
-addContentToFile: Given a file path and file content in string format. If the file doesn't exist, you need to create that file containing given content. If the file already exists, you need to append given content to original content. This function has void return type.
+addContentToFile: Given a file path and file content in string format. If the file doesn't exist, you need to create that file 
+containing given content. If the file already exists, you need to append given content to original content. This function has void
+return type.
 
 readContentFromFile: Given a file path, return its content in string format.
 
@@ -126,7 +131,7 @@ class FileSystem {
     class Node{
         boolean isFile;
         String content; 
-        Map<String, Node> fileAndFolder; // key: current folder name, value: subfolder or files names
+        Map<String, Node> fileAndFolder; // key: current subfolder or file name, value: node for subfolder or files
         public Node (boolean isFile, String content, Map<String, Node> fileAndFolder){
             this.isFile = isFile;
             this.content = content;
