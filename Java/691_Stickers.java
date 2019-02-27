74a0f589adb6c9314a673b27d9163baf27b3ef5c
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-27 12:07:54 -0600

    Update 691_Stickers.java

--- Java/691_Stickers.java
+++ Java/691_Stickers.java
@@ -52,7 +52,7 @@ class Solution {
             }
         }
         Map<String, Integer> dp = new HashMap<>();
-        dp.put("", 0);
+        dp.put("", 0); // one exit condition
         return minStickers(stickerMap, target, dp);
     }
     private int minStickers(int[][] stickerMap, String target, Map<String, Integer> dp){
@@ -65,7 +65,7 @@ class Solution {
         }
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < stickerMap.length; i++){
-            if (stickerMap[i][target.charAt(0) - 'a'] == 0){
+            if (stickerMap[i][target.charAt(0) - 'a'] == 0){ //to speed up
                 continue;
             }
             StringBuilder sb = new StringBuilder();
@@ -87,5 +87,59 @@ class Solution {
         }
         dp.put(target, ans);
         return ans;
+    }
+}
+
+
+
+
+class Solution {
+    public int minStickers(String[] stickers, String target) {
+        Map<String, Map<Character, Integer>> stickerMap = new HashMap<>();
+        for (String str : stickers){
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : str.toCharArray()){
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+            stickerMap.put(str, map);
+        }
+        Map<String, Integer> memo = new HashMap<>();
+        memo.put("", 0);
+        return dfs(stickers, target, memo, stickerMap);
+    }
+    private int dfs(String[] stickers, String target, Map<String, Integer> memo, Map<String, Map<Character, Integer>> stickerMap){
+        if (memo.containsKey(target)){
+            return memo.get(target);
+        }
+        Map<Character, Integer> targetMap = new HashMap<>();
+        for (char c : target.toCharArray()){
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < stickers.length; i++){
+            String str = stickers[i];
+            Map<Character, Integer> map = stickerMap.get(str);
+            if (!map.containsKey(target.charAt(0))){
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (char c : targetMap.keySet()){
+                int t = targetMap.get(c);
+                int s = map.getOrDefault(c, 0);
+                for (int j = 0; j < Math.max(0, t - s); j++){
+                    sb.append(c);
+                }
+            }
+            String next = sb.toString();
+            int nextMin = dfs(stickers, next, memo, stickerMap);
+            if (nextMin != -1){
+                min = Math.min(min, nextMin + 1);
+            }
+        }
+        if (min == Integer.MAX_VALUE){
+            min = - 1;
+        }
+        memo.put(target, min);
+        return min;
     }
 }
