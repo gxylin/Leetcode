6cd386b55b23799287ec1ade4bc5a953ca2db3d6
optimisea <34330974+optimisea@users.noreply.github.com>
2019-02-27 20:14:10 -0600

    Update 888_Fair.java

--- Java/888_Fair.java
+++ Java/888_Fair.java
@@ -1,8 +1,11 @@
-Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th bar of candy that Alice has, and B[j] is the size of the j-th bar of candy that Bob has.
+Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th bar of candy that Alice has, and B[j] is the size of 
+the j-th bar of candy that Bob has.
 
-Since they are friends, they would like to exchange one candy bar each so that after the exchange, they both have the same total amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.)
+Since they are friends, they would like to exchange one candy bar each so that after the exchange, they both have the same total 
+amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.)
 
-Return an integer array ans where ans[0] is the size of the candy bar that Alice must exchange, and ans[1] is the size of the candy bar that Bob must exchange.
+Return an integer array ans where ans[0] is the size of the candy bar that Alice must exchange, and ans[1] is the size of the
+candy bar that Bob must exchange.
 
 If there are multiple answers, you may return any one of them.  It is guaranteed an answer exists.
 
@@ -100,5 +103,45 @@ class Solution {
             }
         }
         return new int[0];
+    }
+}
+
+class Solution {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int[] res = new int[2];
+        Set<Integer> setA = new HashSet<>();
+        int sumA = 0;
+        for (int i : A){
+            sumA += i;
+            setA.add(i);
+        }
+        int sumB = 0;
+        Set<Integer> setB = new HashSet<>();
+        for (int i : B){
+            sumB += i;
+            setB.add(i);
+        }
+        
+        int avg = (sumA + sumB) / 2;
+        if (sumA > avg){
+            int diff = sumA - avg;
+            for (int i : A){
+                if (setB.contains(i-diff)){
+                    res[0] = i ;
+                    res[1] = i - diff;
+                    return res;
+                }
+            }
+        }else{
+            int diff = sumB - avg;
+            for (int i : B){
+                if (setA.contains(i-diff)){
+                    res[0] = i - diff;
+                    res[1] = i;
+                    return res;
+                }
+            }
+        }
+        return res;
     }
 }
