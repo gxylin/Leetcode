d9a54f37ba560a924cb46cfc5b1ef3efe1fbb923
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-03 14:36:22 -0600

    Update 854_Similar.java

--- Java/854_Similar.java
+++ Java/854_Similar.java
@@ -1,4 +1,5 @@
-Strings A and B are K-similar (for some non-negative integer K) if we can swap the positions of two letters in A exactly K times so that the resulting string equals B.
+Strings A and B are K-similar (for some non-negative integer K) if we can swap the positions of two letters in A exactly K times so
+that the resulting string equals B.
 
 Given two anagrams A and B, return the smallest K for which A and B are K-similar.
 
