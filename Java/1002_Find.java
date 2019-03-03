195f3da47cfc5b3ed0da2198d7395d07b9ddbc53
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-02 22:26:40 -0600

    Update 1002_Find.java

--- Java/1002_Find.java
+++ Java/1002_Find.java
@@ -3,6 +3,22 @@ the list (including duplicates).  For example, if a character occurs 3 times in 
 character three times in the final answer.
 
 You may return the answer in any order.
+    
+Example 1:
+
+Input: ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+ 
+
+Note:
+
+1 <= A.length <= 100
+1 <= A[i].length <= 100
+A[i][j] is a lowercase letter
 
 class Solution {
     public List<String> commonChars(String[] A) {
