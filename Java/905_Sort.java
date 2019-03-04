c23a1935006acbdcc2bae57a05ead8e25df10780
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-04 13:38:12 -0600

    Update 905_Sort.java

--- Java/905_Sort.java
+++ Java/905_Sort.java
@@ -54,3 +54,29 @@ class Solution {
         return A;
     }
 }
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int i = 0;
+        int j = A.length - 1;
+        while (i < j){
+            while (i < j && A[i] % 2 == 0){
+                i++;
+            }
+            while (j > i && A[j] % 2 == 1){
+                j--;
+            }
+            if (i < j){
+                swap(A, i, j);
+                i++;
+                j--;
+            }
+        }
+        return A;
+    }
+    private void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
