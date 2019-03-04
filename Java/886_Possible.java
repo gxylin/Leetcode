dc210f3e14f70b13b637a9ed534488cec34fdb2a
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-04 15:46:15 -0600

    Update 886_Possible.java

--- Java/886_Possible.java
+++ Java/886_Possible.java
@@ -82,3 +82,39 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 1; i <= N; i++){
+            map.put(i, new HashSet<>());
+        }
+        for (int[] dislike : dislikes){           
+            map.get(dislike[0]).add(dislike[1]);
+            map.get(dislike[1]).add(dislike[0]);
+        }
+        int[] group = new int[N+1];
+        for (int i = 1; i <= N; i++){
+            if (group[i] == 0){
+                if (!possible(map, group, i, 1)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean possible(Map<Integer, Set<Integer>> map, int[] group, int node, int color){
+        group[node] = color;
+        for (int nei : map.get(node)){
+            if (group[nei] == color){
+                return false;
+            }else if (group[nei] == 0){
+                if (!possible(map, group, nei, -color)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
