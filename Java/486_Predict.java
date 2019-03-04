9444bcc1ec07bc6330f1e79139f0f420d16898fc
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-04 00:12:00 -0600

    Update 486_Predict.java

--- Java/486_Predict.java
+++ Java/486_Predict.java
@@ -1,4 +1,6 @@
-Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array followed by the player 2 and then player 1 and so on. Each time a player picks a number, that number will not be available for the next player. This continues until all the scores have been chosen. The player with the maximum score wins.
+Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array followed by 
+the player 2 and then player 1 and so on. Each time a player picks a number, that number will not be available for the next player. 
+    This continues until all the scores have been chosen. The player with the maximum score wins.
 
 Given an array of scores, predict whether player 1 is the winner. You can assume each player plays to maximize his score.
 
@@ -12,7 +14,8 @@ Hence, player 1 will never be the winner and you need to return False.
 Example 2:
 Input: [1, 5, 233, 7]
 Output: True
-Explanation: Player 1 first chooses 1. Then player 2 have to choose between 5 and 7. No matter which number player 2 choose, player 1 can choose 233.
+Explanation: Player 1 first chooses 1. Then player 2 have to choose between 5 and 7. No matter which number player 2 choose, player 
+1 can choose 233.
 Finally, player 1 has more score (234) than player 2 (12), so you need to return True representing player1 can win.
 Note:
 1 <= length of the array <= 20.
