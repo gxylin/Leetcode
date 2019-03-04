21edc39ce25017de6008dd1b564614cc7728ed6e
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-03 23:42:47 -0600

    Update 871_Minimum.java

--- Java/871_Minimum.java
+++ Java/871_Minimum.java
@@ -1,14 +1,18 @@
 A car travels from a starting position to a destination which is target miles east of the starting position.
 
-Along the way, there are gas stations.  Each station[i] represents a gas station that is station[i][0] miles east of the starting position, and has station[i][1] liters of gas.
+Along the way, there are gas stations.  Each station[i] represents a gas station that is station[i][0] miles east of the starting 
+position, and has station[i][1] liters of gas.
 
-The car starts with an infinite tank of gas, which initially has startFuel liters of fuel in it.  It uses 1 liter of gas per 1 mile that it drives.
+The car starts with an infinite tank of gas, which initially has startFuel liters of fuel in it.  It uses 1 liter of gas per 1 mile
+that it drives.
 
 When the car reaches a gas station, it may stop and refuel, transferring all the gas from the station into the car.
 
-What is the least number of refueling stops the car must make in order to reach its destination?  If it cannot reach the destination, return -1.
+What is the least number of refueling stops the car must make in order to reach its destination?  If it cannot reach the destination,
+return -1.
 
-Note that if the car reaches a gas station with 0 fuel left, the car can still refuel there.  If the car reaches the destination with 0 fuel left, it is still considered to have arrived.
+Note that if the car reaches a gas station with 0 fuel left, the car can still refuel there.  If the car reaches the destination with
+0 fuel left, it is still considered to have arrived.
 
  
 
@@ -45,7 +49,7 @@ Time complexity:O(N^2)
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         int N = stations.length;
-        int[] dp = new int[N+1]; //the farthest distance that can reach at station i
+        int[] dp = new int[N+1]; //dp[t] means the furthest distance that we can get with t times of refueling.
         dp[0] = startFuel;
         for (int i = 0; i < N; i++){
             for (int j = i; j >= 0; j--){
@@ -67,6 +71,13 @@ Method 2: Better solution
 
 PriorityQueue, always try to add gas at the largest gas station first
 https://leetcode.com/problems/minimum-number-of-refueling-stops/solution/
+
+We initial res = 0 and in every loop:
+
+We add all reachable stop to priority queue.
+We pop out the largest gas from pq and refeul once.
+If we can't refuel, means that we can not go forward and return -1
+ 
 Time complexity: O(NlogN)
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
