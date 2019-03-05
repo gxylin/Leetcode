8a772bf8e4f21be4bf33e86ec262690cf1a2d5b4
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-04 18:32:01 -0600

    Update 879_Profitable.java

--- Java/879_Profitable.java
+++ Java/879_Profitable.java
@@ -34,6 +34,11 @@ Note:
 0 <= profit[i] <= 100
 1 <= group.length = profit.length <= 100
 
+ Knapsack problem
+ The knapsack problem or rucksack problem is a problem in combinatorial optimization: 
+Given a set of items, each with a weight and a value, determine the number of each item to include in a collection so that
+the total weight is less than or equal to a given limit and the total value is as large as possible.
+ 
 Method 1: 3D DP
 class Solution {
     public int profitableSchemes(int G, int P, int[] group, int[] profit) {
