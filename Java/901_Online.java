92de398750730c848a6baa1b60ed5c255d85a5ea
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-05 14:38:39 -0600

    Update 901_Online.java

--- Java/901_Online.java
+++ Java/901_Online.java
@@ -1,8 +1,11 @@
-Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current 
+ day.
 
-The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) 
+ for which the price of the stock was less than or equal to today's price.
 
-For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be
+[1, 1, 1, 2, 1, 4, 6].
 
  
 
@@ -88,7 +91,7 @@ class StockSpanner {
     }
 }
 
-Shorter version
+Best version:
 class StockSpanner {
     Stack<int[]> stack;
     public StockSpanner() {
