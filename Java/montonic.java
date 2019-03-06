41414727cf1606c96a4f89e4d6f9e73bb73d269a
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-05 21:30:57 -0600

    Create montonic.java

---
+++ Java/montonic.java
@@ -0,0 +1,14 @@
+
+Some applications of monotone (increase/decrease) stack in leetcode:
+Next Greater Element II (a very basic one)
+Largest Rectangle in Histogram(almost the same as this problem)
+Maximal Rectangle(please do this problem after you solve the above one)
+Trapping Rain Water (challenge)
+Remove Duplicate Letters(challenge)
+Remove K Digits
+Create Maximum Number
+132 Pattern(challenge, instead of focusing on the elements in the stack, this problem focuses on the elements poped from the monotone stack)
+sliding window maximum(challenge, monotone queue)
+Max Chunks To Make Sorted II
+
+https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step
