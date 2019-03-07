98d0333cdeaa8daf6c2322d7baf9a614cc50caaa
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-07 13:12:30 -0600

    Update 666_Path.java

--- Java/666_Path.java
+++ Java/666_Path.java
@@ -3,10 +3,12 @@
 For each integer in this list:
 
     The hundreds digit represents the depth D of this node, 1 <= D <= 4.
-    The tens digit represents the position P of this node in the level it belongs to, 1 <= P <= 8. The position is the same as that in a full binary tree.
+    The tens digit represents the position P of this node in the level it belongs to, 1 <= P <= 8. The position is the same
+    as that in a full binary tree.
     The units digit represents the value V of this node, 0 <= V <= 9.
 
-Given a list of ascending three-digits integers representing a binary with the depth smaller than 5. You need to return the sum of all paths from the root towards the leaves.
+Given a list of ascending three-digits integers representing a binary with the depth smaller than 5. 
+     You need to return the sum of all paths from the root towards the leaves.
 
 Example 1:
 
