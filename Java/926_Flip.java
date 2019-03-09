075f645f8c854cda32a2019b3db0635856f9ba3f
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-09 15:05:23 -0600

    Update 926_Flip.java

--- Java/926_Flip.java
+++ Java/926_Flip.java
@@ -1,4 +1,5 @@
-A string of '0's and '1's is monotone increasing if it consists of some number of '0's (possibly 0), followed by some number of '1's (also possibly 0.)
+A string of '0's and '1's is monotone increasing if it consists of some number of '0's (possibly 0), followed by some number of '1's 
+(also possibly 0.)
 
 We are given a string S of '0's and '1's, and we may flip any '0' to a '1' or a '1' to a '0'.
 
@@ -32,15 +33,22 @@ S only consists of '0' and '1' characters.
 Time complexity: O(N)
 Space complexity: O(N)
 This is DP partition problem
-The key idea to find the cut point where the left of the point will be 0 and the right of the point will be 1. So we can use DP to find the point which yields the minimum flip. This is a top down DP solution
+
+The key idea to find the cut point where the left of the point will be 0 and the right of the point will be 1. So we can
+use DP to find the point which yields the minimum flip. This is a top down DP solution
 
 cut pointt:   0   1    2   3    4    5   6   7
 array:            1   0    0   1    1    0   1 
 Note that if the length of the array is n, then there will be n+1 cut point. So dp length will be n+1
 
-Step 1: initialization. the first cut point is at index 0. All the elements need to be 1. So the flip number should be equal to the number of 0
+Step 1: initialization. the first cut point is at index 0. All the elements need to be 1. So the flip number should be equal to 
+the number of 0
 
-Step 2: Iteration. if the element is '1', it needs to be changed to '0', and the right side of the cut point are no changes. So dp[i] = dp[i-1] + 1; If the element is '0', it does not to be changed, however, this element has to be changed to '1' in previous cut point dp[i-1] because it is '0'. So compared to previouis step dp[i-1], The number of flip is reduced by 1. Hence, dp[i] = dp[i-1] - 1;
+Step 2: Iteration. 
+ If the element is '1', it needs to be changed to '0', and the right side of the cut point are no changes. 
+ So dp[i] = dp[i-1] + 1; If the element is '0', it does not to be changed, however, this element has to be changed to '1' in
+ previous cut point dp[i-1] because it is '0'. So compared to previouis step dp[i-1], The number of flip is reduced by 1.
+ Hence, dp[i] = dp[i-1] - 1;
 
 Step 3: Top down DP to find minimum
 
