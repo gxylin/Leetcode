a0ec4749c2396861510684fe94c8cc4d163f6082
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-08 20:53:27 -0600

    Update 918_Maximum.java

--- Java/918_Maximum.java
+++ Java/918_Maximum.java
@@ -1,8 +1,10 @@
 Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
 
-Here, a circular array means the end of the array connects to the beginning of the array.  (Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+Here, a circular array means the end of the array connects to the beginning of the array.  
+ (Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
 
-Also, a subarray may only include each element of the fixed buffer A at most once.  (Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+Also, a subarray may only include each element of the fixed buffer A at most once. 
+ (Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
 
  
 Example 1:
@@ -158,17 +160,16 @@ int maxSubArraySum(int[] A){
   return max;
 }
 Method 2: DP
- static int maxSubArraySum(int a[], int size) 
-    { 
-    int max_so_far = a[0]; 
-    int curr_max = a[0]; 
-  
-    for (int i = 1; i < size; i++) 
-    { 
-           curr_max = Math.max(a[i], curr_max+a[i]); 
-        max_so_far = Math.max(max_so_far, curr_max); 
-    } 
-    return max_so_far; 
-    } 
+ class Solution {
+    public int maxSubArray(int[] nums) {
+        int local = 0;
+        int global = Integer.MIN_VALUE;
+        for (int num : nums){
+            local = Math.max(local + num, num);
+            global = Math.max(global, local);
+        }
+        return global;
+    }
+}
     
     
