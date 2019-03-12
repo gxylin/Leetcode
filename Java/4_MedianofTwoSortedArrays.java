e8333538d1b994edef092678de13fbdeb625872f
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-11 23:06:16 -0500

    Update 4_MedianofTwoSortedArrays.java

--- Java/4_MedianofTwoSortedArrays.java
+++ Java/4_MedianofTwoSortedArrays.java
@@ -13,7 +13,36 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 
-Best solution
+Best solution:  O(log(m + n)), O(log(k))
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if ((m+n) % 2  == 0){
+            return (findKth(nums1, 0, nums2, 0, (m+n)/2) + findKth(nums1, 0, nums2, 0, (m+n)/2 + 1)) / 2.0;
+        }
+        return (double)findKth(nums1, 0, nums2, 0, (m+n)/2 + 1);
+    }
+    private int findKth(int[] nums1, int start1, int[] nums2, int start2, int k){
+        if (start1 >= nums1.length){
+            return nums2[start2 + k - 1];
+        }
+        if (start2 >= nums2.length){
+            return nums1[start1 + k - 1];
+        }
+        if (k == 1){
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+        int mid1 = start1 + k/2 - 1 < nums1.length ? nums1[start1 + k/2 - 1] : Integer.MAX_VALUE;
+        int mid2 = start2 + k/2 - 1 < nums2.length ? nums2[start2 + k/2 - 1] : Integer.MAX_VALUE;
+        if (mid1 < mid2){//median must be within the 2nd half of nums1 and 1st half of nums2
+            return findKth(nums1, start1 + k/2, nums2, start2, k - k/2);
+        }else{
+            return findKth(nums1, start1, nums2, start2 + k/2, k - k /2);
+        }
+    }
+}
+
 Method 1: O(log(m + n)), O(log(k))
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
