2f6d422b6d59e0296ed51159c05b391cd1db51ab
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-11 22:32:46 -0500

    Update 30_InsertInterval.java

--- Java/30_InsertInterval.java
+++ Java/30_InsertInterval.java
@@ -131,15 +131,8 @@ class Solution {
     }
 }
 
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
+
+Best solution:
 class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> res = new ArrayList<>();
