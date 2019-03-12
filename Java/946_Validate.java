af35453fad7e9fc398129d972324e5b5d6e1edb4
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-11 19:23:01 -0500

    Update 946_Validate.java

--- Java/946_Validate.java
+++ Java/946_Validate.java
@@ -1,4 +1,5 @@
-Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
+Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence
+of push and pop operations on an initially empty stack.
 
  
 
