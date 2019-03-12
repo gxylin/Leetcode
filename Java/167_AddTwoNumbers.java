2c52bc190b76eb228573515662ac159ce5c040a7
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-12 00:29:44 -0500

    Update 167_AddTwoNumbers.java

--- Java/167_AddTwoNumbers.java
+++ Java/167_AddTwoNumbers.java
@@ -53,14 +53,7 @@ public class Solution {
 }
 
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+Best solution
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
