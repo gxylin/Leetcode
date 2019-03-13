13dbae52836a6afc38cbe3fa6640de9a7331f440
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-12 20:28:15 -0500

    Update 958_Check.java

--- Java/958_Check.java
+++ Java/958_Check.java
@@ -1,7 +1,8 @@
 Given a binary tree, determine if it is a complete binary tree.
 
 Definition of a complete binary tree from Wikipedia:
-In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as 
+far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
 
  
 
@@ -11,7 +12,8 @@ Example 1:
 
 Input: [1,2,3,4,5,6]
 Output: true
-Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last level ({4, 5, 6}) are as far left as possible.
+Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last 
+level ({4, 5, 6}) are as far left as possible.
 Example 2:
 
 
@@ -86,6 +88,7 @@ class Solution {
     }
 }
 
+Best solution:
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
         if (root == null){
