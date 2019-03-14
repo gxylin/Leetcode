880a96aeca6b1d67d3581a89c723fd0d87830aee
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-13 20:28:50 -0500

    Update 966_Vowe.java

--- Java/966_Vowe.java
+++ Java/966_Vowe.java
@@ -2,11 +2,13 @@ Given a wordlist, we want to implement a spellchecker that converts a query word
 
 For a given query word, the spell checker handles two categories of spelling mistakes:
 
-Capitalization: If the query matches a word in the wordlist (case-insensitive), then the query word is returned with the same case as the case in the wordlist.
+Capitalization: If the query matches a word in the wordlist (case-insensitive), then the query word is returned with the same case
+ as the case in the wordlist.
 Example: wordlist = ["yellow"], query = "YellOw": correct = "yellow"
 Example: wordlist = ["Yellow"], query = "yellow": correct = "Yellow"
 Example: wordlist = ["yellow"], query = "yellow": correct = "yellow"
-Vowel Errors: If after replacing the vowels ('a', 'e', 'i', 'o', 'u') of the query word with any vowel individually, it matches a word in the wordlist (case-insensitive), then the query word is returned with the same case as the match in the wordlist.
+Vowel Errors: If after replacing the vowels ('a', 'e', 'i', 'o', 'u') of the query word with any vowel individually, it matches a 
+word in the wordlist (case-insensitive), then the query word is returned with the same case as the match in the wordlist.
 Example: wordlist = ["YellOw"], query = "yollow": correct = "YellOw"
 Example: wordlist = ["YellOw"], query = "yeellow": correct = "" (no match)
 Example: wordlist = ["YellOw"], query = "yllw": correct = "" (no match)
