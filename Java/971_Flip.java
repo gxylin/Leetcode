6a9ea8552c6466095c4b262884037f312d893060
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-14 18:58:37 -0500

    Update 971_Flip.java

--- Java/971_Flip.java
+++ Java/971_Flip.java
@@ -2,9 +2,11 @@ Given a binary tree with N nodes, each node has a different value from {1, ..., 
 
 A node in this binary tree can be flipped by swapping the left child and the right child of that node.
 
-Consider the sequence of N values reported by a preorder traversal starting from the root.  Call such a sequence of N values the voyage of the tree.
+Consider the sequence of N values reported by a preorder traversal starting from the root.  Call such a sequence of N values 
+the voyage of the tree.
 
-(Recall that a preorder traversal of a node means we report the current node's value, then preorder-traverse the left child, then preorder-traverse the right child.)
+(Recall that a preorder traversal of a node means we report the current node's value, then preorder-traverse the left child,
+ then preorder-traverse the right child.)
 
 Our goal is to flip the least number of nodes in the tree so that the voyage of the tree matches the voyage we are given.
 
