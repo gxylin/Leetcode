97ec3ce068abe48ce1c22b8d75420f8028865b97
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-14 21:57:18 -0500

    Update 446_Arithmetic.java

--- Java/446_Arithmetic.java
+++ Java/446_Arithmetic.java
@@ -1,4 +1,5 @@
-A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any 
+two consecutive elements is the same.
 
 For example, these are arithmetic sequences:
 
@@ -9,13 +10,16 @@ The following sequence is not arithmetic.
 
 1, 1, 2, 5, 7
 
-A zero-indexed array A consisting of N numbers is given. A subsequence slice of that array is any sequence of integers (P0, P1, ..., Pk) such that 0 ≤ P0 < P1 < ... < Pk < N.
+A zero-indexed array A consisting of N numbers is given. A subsequence slice of that array is any sequence of integers 
+(P0, P1, ..., Pk) such that 0 ≤ P0 < P1 < ... < Pk < N.
 
-A subsequence slice (P0, P1, ..., Pk) of array A is called arithmetic if the sequence A[P0], A[P1], ..., A[Pk-1], A[Pk] is arithmetic. In particular, this means that k ≥ 2.
+A subsequence slice (P0, P1, ..., Pk) of array A is called arithmetic if the sequence A[P0], A[P1], ..., A[Pk-1], A[Pk] is 
+arithmetic. In particular, this means that k ≥ 2.
 
 The function should return the number of arithmetic subsequence slices in the array A.
 
-The input contains N integers. Every integer is in the range of -231 and 231-1 and 0 ≤ N ≤ 1000. The output is guaranteed to be less than 231-1.
+The input contains N integers. Every integer is in the range of -231 and 231-1 and 0 ≤ N ≤ 1000. The output is guaranteed 
+to be less than 231-1.
 
 
 Example:
