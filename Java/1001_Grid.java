29eb70928e4aac56dc1c0ca73efb3cd6be43c3ae
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-17 11:53:29 -0500

    Update 1001_Grid.java

--- Java/1001_Grid.java
+++ Java/1001_Grid.java
@@ -1,10 +1,12 @@
 On a N x N grid of cells, each cell (x, y) with 0 <= x < N and 0 <= y < N has a lamp.
 
-Initially, some number of lamps are on.  lamps[i] tells us the location of the i-th lamp that is on.  Each lamp that is on illuminates every square on its x-axis, y-axis, and both diagonals (similar to a Queen in chess).
+Initially, some number of lamps are on.  lamps[i] tells us the location of the i-th lamp that is on.  Each lamp that is on illuminates
+every square on its x-axis, y-axis, and both diagonals (similar to a Queen in chess).
 
 For the i-th query queries[i] = (x, y), the answer to the query is 1 if the cell (x, y) is illuminated, else 0.
 
-After each query (x, y) [in the order given by queries], we turn off any lamps that are at cell (x, y) or are adjacent 8-directionally (ie., share a corner or edge with cell (x, y).)
+After each query (x, y) [in the order given by queries], we turn off any lamps that are at cell (x, y) or are adjacent 8-directionally
+(ie., share a corner or edge with cell (x, y).)
 
 Return an array of answers.  Each value answer[i] should be equal to the answer of the i-th query queries[i].
 
@@ -22,13 +24,15 @@ The grid representing which cells are lit looks like this, where [0,0] is the to
 1 0 1 0 1
 1 0 0 1 1
 1 1 1 1 1
-Then the query at [1, 1] returns 1 because the cell is lit.  After this query, the lamp at [0, 0] turns off, and the grid now looks like this:
+Then the query at [1, 1] returns 1 because the cell is lit.  After this query, the lamp at [0, 0] turns off, and the grid now looks 
+like this:
 1 0 0 0 1
 0 1 0 0 1
 0 0 1 0 1
 0 0 0 1 1
 1 1 1 1 1
-Before performing the second query we have only the lamp [4,4] on.  Now the query at [1,0] returns 0, because the cell is no longer lit.
+Before performing the second query we have only the lamp [4,4] on.  Now the query at [1,0] returns 0, because the cell is no longer 
+lit.
  
 
 Note:
@@ -38,6 +42,7 @@ Note:
 0 <= queries.length <= 20000
 lamps[i].length == queries[i].length == 2
 
+ 
 https://leetcode.com/problems/grid-illumination/discuss/243076/Java-Clean-Code-O(N)-Time-and-O(N)-Space-Beats-100
 The basic idea is:
 
