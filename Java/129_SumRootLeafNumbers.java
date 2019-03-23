3a2589afa3766d9ea15bb4d6261e8bb09a821b86
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-23 17:21:11 -0500

    Update 129_SumRootLeafNumbers.java

--- Java/129_SumRootLeafNumbers.java
+++ Java/129_SumRootLeafNumbers.java
@@ -33,6 +33,28 @@ class Solution {
     }
 }
 
+
+Better solution:
+class Solution {
+    int res = 0;
+    public int sumNumbers(TreeNode root) {
+        preorder(root, 0);
+        return res;
+    }
+    private void preorder(TreeNode root, int sum){
+        if (root == null){
+            return;
+        }
+        if (root.left == null && root.right == null){
+            res += sum * 10 + root.val;
+            return;
+        }
+        sum = sum * 10 + root.val;
+        preorder(root.left, sum);
+        preorder(root.right, sum);
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
