a907d513a9aab55a488447657983d8d1102b3f27
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-25 22:48:10 -0500

    Update 35_Search.java

--- Java/35_Search.java
+++ Java/35_Search.java
@@ -22,6 +22,13 @@ Output: 0
     
 Check leetcode: https://leetcode.com/problems/random-pick-with-weight/discuss/154044/Java-accumulated-freq-sum-and-binary-search
 
+https://github.com/optimisea/Leetcode/blob/master/Java/binary.java
+
+Note that for this method, in most cases target should be between low (inclusive) and high (inclusive) 
+     But there are two corner cases
+     case 1: target less than low, e.g. [1,2,3] target = 0 == > low will be 1, high will be 2
+     case 2: target greater than high, e.g. [1,2,3] target = 4 == > low will be 2, high will be 3
+         
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0;
@@ -36,18 +43,18 @@ class Solution {
                 end = mid;
             }
         }
-        if (nums[start] >= target){
+        if (nums[start] >= target){//cover corner case 1 and normal case
             return start;
         }
-        if (nums[end] >= target){
+        if (nums[end] >= target){//cover normal case
             return end;
         }
-        return end+1;
+        return end+1; //cover corner case 2
         //return nums.length;
     }
 }
 
-Better version:
+Better version: no need to check corner cases since it already covered.
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0;
