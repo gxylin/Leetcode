02b4c3c42cb3c893b0f3c8c0da17ec607dcf7e34
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-27 12:08:45 -0500

    Update 857_Minimum.java

--- Java/857_Minimum.java
+++ Java/857_Minimum.java
@@ -1,6 +1,7 @@
 There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
 
-Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according to the following rules:
+Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according
+to the following rules:
 
 Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
 Every worker in the paid group must be paid at least their minimum wage expectation.
