9cbe68c8b6d25683575b88578bb682ad9feb6a7a
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-27 23:59:43 -0500

    Update 885_Spiral.java

--- Java/885_Spiral.java
+++ Java/885_Spiral.java
@@ -1,10 +1,12 @@
 On a 2 dimensional grid with R rows and C columns, we start at (r0, c0) facing east.
 
-Here, the north-west corner of the grid is at the first row and column, and the south-east corner of the grid is at the last row and column.
+Here, the north-west corner of the grid is at the first row and column, and the south-east corner of the grid is at the last
+row and column.
 
 Now, we walk in a clockwise spiral shape to visit every position in this grid. 
 
-Whenever we would move outside the boundary of the grid, we continue our walk outside the grid (but may return to the grid boundary later.) 
+Whenever we would move outside the boundary of the grid, we continue our walk outside the grid (but may return to the grid 
+boundary later.) 
 
 Eventually, we reach all R * C spaces of the grid.
 
@@ -23,7 +25,8 @@ Output: [[0,0],[0,1],[0,2],[0,3]]
 Example 2:
 
 Input: R = 5, C = 6, r0 = 1, c0 = 4
-Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
+Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],
+         [4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
 
 
  
@@ -37,11 +40,14 @@ Note:
 
 Intuition
 
-We can walk in a spiral shape from the starting square, ignoring whether we stay in the grid or not. Eventually, we must have reached every square in the grid.
+We can walk in a spiral shape from the starting square, ignoring whether we stay in the grid or not. Eventually, 
+we must have reached every square in the grid.
 
 Algorithm
 
-Examining the lengths of our walk in each direction, we find the following pattern: 1, 1, 2, 2, 3, 3, 4, 4, ... That is, we walk 1 unit east, then 1 unit south, then 2 units west, then 2 units north, then 3 units east, etc. Because our walk is self-similar, this pattern repeats in the way we expect.
+Examining the lengths of our walk in each direction, we find the following pattern: 1, 1, 2, 2, 3, 3, 4, 4, ... 
+ That is, we walk 1 unit east, then 1 unit south, then 2 units west, then 2 units north, then 3 units east, etc.
+ Because our walk is self-similar, this pattern repeats in the way we expect.
 
 
 class Solution {
