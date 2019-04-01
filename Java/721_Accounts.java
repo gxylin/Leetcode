ffa781730392fdc0c02cc3e59a300579eb994ccd
optimisea <34330974+optimisea@users.noreply.github.com>
2019-03-31 21:09:24 -0500

    Update 721_Accounts.java

--- Java/721_Accounts.java
+++ Java/721_Accounts.java
@@ -36,16 +36,15 @@ names on the side. After finding each connected component using a depth-first se
 
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        //build graph
         Map<String, Set<String>> graph = new HashMap<>();
-        Map<String, String> nameMap = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         for (List<String> account : accounts){
             String name = account.get(0);
             for (int i = 1; i < account.size(); i++){
                 if (!graph.containsKey(account.get(i))){
                     graph.put(account.get(i), new HashSet<>());
                 }
-                nameMap.put(account.get(i), name);
+                map.put(account.get(i), name);
                 if (i == 1){
                     continue;
                 }
@@ -53,27 +52,28 @@ class Solution {
                 graph.get(account.get(i-1)).add(account.get(i));
             }
         }
-        //dfs search
+        Set<String> set = new HashSet<>();
         List<List<String>> res = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
-        for (String key : nameMap.keySet()){
+        for (String key : map.keySet()){
+            if (set.contains(key)){
+                continue;
+            }
             List<String> list = new ArrayList<>();
-            if (!visited.contains(key)){
-                dfs(res, list, graph, visited, key);
-                Collections.sort(list);
-                list.add(0, nameMap.get(key));
-                res.add(list);
-            }   
+            dfs(list, set, graph, key);
+            Collections.sort(list);
+            list.add(0, map.get(key));
+            res.add(list);
         }
         return res;
     }
-    private void dfs(List<List<String>> res, List<String> list, Map<String, Set<String>> graph, Set<String> visited, String key){
-        visited.add(key);
+    private void dfs(List<String> list, Set<String> set, Map<String, Set<String>> graph , String key){
+        if (set.contains(key)){
+            return;
+        }
+        set.add(key);
         list.add(key);
         for (String nei : graph.get(key)){
-            if (!visited.contains(nei)){
-                dfs(res, list, graph, visited, nei); 
-            }
+            dfs(list, set, graph, nei);
         }
     }
 }
