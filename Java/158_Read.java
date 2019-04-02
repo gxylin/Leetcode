2393cb43aef1830bcfb5e3d758a0cb045df55847
optimisea <34330974+optimisea@users.noreply.github.com>
2019-04-01 23:35:24 -0500

    Update 158_Read.java

--- Java/158_Read.java
+++ Java/158_Read.java
@@ -18,6 +18,7 @@ Public class Solution Extends Read4{
   private tmpCnt = 0; //pointer in tmp
   private tmpPtr = 0; //how many char is read in tmp
   
+  Format 1:
   public int read(char[] buff, int n){
       int total = 0;
       while (total < n){
@@ -39,4 +40,73 @@ Public class Solution Extends Read4{
       }
       return total;
   }
+}
+
+
+Format 2:
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf destination buffer
+     * @param n maximum number of characters to read
+     * @return the number of characters read
+     */
+     
+    private char[] tmp = new char[4];
+    private int tmpCnt = 0;
+    private int tmpPtr = 0;
+    public int read(char[] buf, int n) {
+        int total = 0;
+        while (total < n){
+            if (tmpPtr == 0){
+                tmpCnt = read4(tmp);
+            }
+            while (total < n && tmpPtr < tmpCnt){
+                buf[total++] = tmp[tmpPtr++];
+            }
+            if (tmpPtr == tmpCnt){
+                tmpPtr = 0;
+            }
+            if (tmpCnt < 4){
+                break;
+            }
+        }
+        return total;
+    }
+}
+
+
+
+Format 3:
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf destination buffer
+     * @param n maximum number of characters to read
+     * @return the number of characters read
+     */
+     
+    private char[] tmp = new char[4];
+    private int tmpCnt = 0;
+    private int tmpPtr = 0;
+    public int read(char[] buf, int n) {
+        int total = 0;
+        while (total < n){
+            if (tmpPtr == 0){
+                tmpCnt = read4(tmp);
+            }
+            if (tmpCnt == 0){
+                break;
+            }
+            while (total < n && tmpPtr < tmpCnt){
+                buf[total++] = tmp[tmpPtr++];
+            }
+            if (tmpPtr == tmpCnt){
+                tmpPtr = 0;
+            }
+        }
+        return total;
+    }
 }
