329a01f88377f1f87fb704666b6e123fa36bf36e
optimisea <34330974+optimisea@users.noreply.github.com>
2019-04-02 16:54:12 -0500

    Update 404_Sum.java

--- Java/404_Sum.java
+++ Java/404_Sum.java
@@ -38,6 +38,25 @@ class Solution {
     }
 }
 
+class Solution {
+    int sum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        dfs(root);
+        return sum;
+    }
+    private void dfs(TreeNode root){
+        if (root == null){
+            return;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null){
+            sum += root.left.val;
+        }
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
+
+
 Method 2: divide and conquer
 /**
  * Definition for a binary tree node.
