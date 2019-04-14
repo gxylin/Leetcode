8be19f001fa8831781c216a49c11fadec8ad6be0
optimisea <34330974+optimisea@users.noreply.github.com>
2019-04-14 00:17:58 -0500

    Create 1026_Maximum.java

---
+++ Java/1026_Maximum.java
@@ -0,0 +1,80 @@
+Given the root of a binary tree, find the maximum value V for which there exists different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
+
+(A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.)
+
+ 
+
+Example 1:
+
+
+
+Input: [8,3,10,1,6,null,14,null,null,4,7,13]
+Output: 7
+Explanation: 
+We have various ancestor-node differences, some of which are given below :
+|8 - 3| = 5
+|3 - 7| = 4
+|8 - 1| = 7
+|10 - 13| = 3
+Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+ 
+
+Note:
+
+The number of nodes in the tree is between 2 and 5000.
+Each node will have value between 0 and 100000.
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    class Node{
+        int min;
+        int max;
+        int diff;
+        public Node(int diff, int max, int min){
+            this.diff = diff;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    int res = Integer.MIN_VALUE;
+    public int maxAncestorDiff(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        dfs(root);
+        return res;
+    }
+    private Node dfs(TreeNode root){
+        if (root == null){
+            return new Node(Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        Node left = dfs(root.left);
+        Node right = dfs(root.right);
+        int diff = Integer.MIN_VALUE;
+        
+        if (left.max == Integer.MIN_VALUE && right.max == Integer.MIN_VALUE){
+            return new Node(diff, root.val, root.val);
+        }
+        
+        if (left.max != Integer.MIN_VALUE && left.min != Integer.MAX_VALUE){
+            diff = Math.max(diff, Math.max(Math.abs(root.val - left.max), Math.abs(root.val - left.min)));
+        }
+        
+        if (right.max != Integer.MIN_VALUE && right.min != Integer.MAX_VALUE){
+            diff = Math.max(diff, Math.max(Math.abs(root.val - right.max), Math.abs(root.val - right.min)));
+        }
+        res = Math.max(res, diff);
+        int currMax = Math.max(root.val, Math.max(left.max, right.max));
+        int currMin = Math.min(root.val, Math.min(left.min, right.min));
+        return new Node(diff, currMax, currMin);
+    }
+}
