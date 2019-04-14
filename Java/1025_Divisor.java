a6a023a8a7a0f259e5e8ce57a04746e64d3d2662
optimisea <34330974+optimisea@users.noreply.github.com>
2019-04-14 00:15:42 -0500

    Create 1025_Divisor.java

---
+++ Java/1025_Divisor.java
@@ -0,0 +1,45 @@
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < N and N % x == 0.
+Replacing the number N on the chalkboard with N - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return True if and only if Alice wins the game, assuming both players play optimally.
+
+ 
+
+Example 1:
+
+Input: 2
+Output: true
+Explanation: Alice chooses 1, and Bob has no more moves.
+Example 2:
+
+Input: 3
+Output: false
+Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+ 
+
+Note:
+
+1 <= N <= 1000
+
+class Solution {
+    Map<Integer, Boolean> map = new HashMap<>();
+    public boolean divisorGame(int N) {
+        if (map.containsKey(N)){
+            return map.get(N);
+        }
+        for (int i = 1; i < N; i++){
+            if (N % i == 0){
+                if (!divisorGame(N-i)){
+                    return true;
+                }
+            }
+        }
+        map.put(N, false);
+        return false;
+    }
+}
