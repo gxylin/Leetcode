164c4af039161b22cac7abce5dbcf920ab9e0dbe
optimisea <34330974+optimisea@users.noreply.github.com>
2019-05-04 22:06:00 -0500

    Update 1014_Capacity.java

--- Java/1014_Capacity.java
+++ Java/1014_Capacity.java
@@ -85,3 +85,41 @@ class Solution {
         return count + 1;
     }
 }
+
+
+class Solution {
+    public int shipWithinDays(int[] weights, int D) {
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < weights.length; i++){
+            sum += weights[i];
+            max = Math.max(max, weights[i]);
+        }
+        int start = max;
+        int end = sum;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            if (lessThanNum(weights, mid, D)){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    private boolean lessThanNum(int[] weights, int mid, int D){
+        int count = 0;
+        int sum = 0;
+        for (int i : weights){
+            sum += i;
+            if (sum > mid){
+                count++;
+                sum = i;
+                if (count >= D){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
